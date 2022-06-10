a8a814bd7dac40de077c050879b990511516a838
Conrad Gustafson <c.gustafson@cgi.com>
2022-06-10 15:44:26 -0700

    chore: add rds db to terraform

--- server/main.tf
+++ server/main.tf
@@ -272,3 +272,87 @@ module "cors" {
 
 
 }
+
+data "aws_vpc" "selected" {
+  state = "available"
+}
+
+data "aws_subnet" "a" {
+  filter {
+    name   = "tag:Name"
+    values = [var.subnet_a]
+  }
+}
+
+data "aws_subnet" "b" {
+  filter {
+    name   = "tag:Name"
+    values = [var.subnet_b]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Security group for AWS lambda and AWS RDS connection"
+  vpc_id      = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["127.0.0.1/32"]
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# this modules documented outputs all need a prefix of this_
+module "db" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "~> 3.0"
+
+  name           = "aurora-db-postgres"
+  engine         = "aurora-postgresql"
+  engine_version = "11.9"
+  engine_mode    = "serverless"
+
+  vpc_id                 = data.aws_vpc.selected.id
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  subnets                = [data.aws_subnet.a.id, data.aws_subnet.b.id]
+
+  allowed_security_groups = [aws_security_group.rds_sg.id]
+
+  replica_scale_enabled = false
+  replica_count         = 0
+
+  storage_encrypted = true
+  apply_immediately = true
+  # 0 is used to disable enhanced monitoring
+  monitoring_interval = 0
+  # Remove this to save a final snapshot before database is destroyed
+  skip_final_snapshot  = false
+  enable_http_endpoint = true
+
+  scaling_configuration = {
+    auto_pause               = true
+    min_capacity             = 2
+    max_capacity             = 2
+    seconds_until_auto_pause = 300
+    timeout_action           = "ForceApplyCapacityChange"
+  }
+
+  /*create_random_password = false
+  username               = var.db_username
+  password               = random_password.db_password.result
+  database_name          = var.db_name
+  */
+
+  tags = {
+    "managed-by" = "terraform"
+  }
+}
--- server/variables.tf
+++ server/variables.tf
@@ -12,3 +12,16 @@ variable "aws_region" {
   type    = string
   default = "ca-central-1"
 }
+
+# Stuff for RDS
+
+variable "subnet_a" {
+  description = "Value of the name tag for the subnet in AZ a"
+  default     = "App_Sandbox_aza_net"
+}
+
+variable "subnet_b" {
+  description = "Value of the name tag for the subnet in AZ b"
+  default     = "App_Sandbox_azb_net"
+}
+
