307e28d0cf6792c9792002cf425adc777a086da8
Conrad Gustafson <c.gustafson@cgi.com>
2022-06-23 18:00:36 -0700

    fix: put in correct lambda zip and moved sql

--- server/db-migration.tf
+++ server/db-migration.tf
@@ -8,9 +8,9 @@ resource "aws_lambda_function" "db-migrations" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("db-migrations/lambda/lambda-db-migrations.zip")
+  source_code_hash = filebase64sha256("db-migrations/lambda/flyway-0.0.4.zip")
 
-  runtime = "java8"
+  runtime = "java11"
 
   vpc_config {
     subnet_ids         = [data.aws_subnet.a.id, data.aws_subnet.b.id]
