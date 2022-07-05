e2dc6d9f3d68fe4d338fc784f23cbd7b36cd3c4f
Conrad Gustafson <c.gustafson@cgi.com>
2022-07-05 14:52:10 -0700

    fix: add connection string to flyway

--- server/db-migration.tf
+++ server/db-migration.tf
@@ -73,6 +73,7 @@ resource "aws_lambda_function" "db-migrations" {
   environment {
     variables = {
       DB_SECRET = "${var.db_master_creds_secretname}"
+      DB_CONNECTION_STRING = "${module.db.cluster_endpoint}"
     }
   }
 
