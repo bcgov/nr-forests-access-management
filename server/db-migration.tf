b7e011f654a78427347ed7c1fd4e1ff16229867a
Conrad Gustafson <c.gustafson@cgi.com>
2022-07-05 13:42:12 -0700

    fix: added necessary policy items

--- server/db-migration.tf
+++ server/db-migration.tf
@@ -5,12 +5,32 @@ resource "aws_iam_role_policy" "secretmasterDB_access_policy" {
   {
     "Version": "2012-10-17",
     "Statement": [
-
+      {
+        "Effect": "Allow",
+        "Action": [
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue"
+        ],
+        "Resource": "${aws_secretsmanager_secret.secretmasterDB.arn}"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface",
+            "ec2:AssignPrivateIpAddresses",
+            "ec2:UnassignPrivateIpAddresses"
+        ],
+        "Resource": "*"
+      }
     ]
   }
   EOF
 }
-
 
 data "aws_iam_policy_document" "flyway_lambda_exec_policydoc" {
   statement {
