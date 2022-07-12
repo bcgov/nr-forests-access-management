fd25edaf22853fda0272c5e5519f5ed098bb64a8
Conrad Gustafson <c.gustafson@cgi.com>
2022-07-11 17:00:11 -0700

    fix: source for source_code_hash

--- server/db-migration.tf
+++ server/db-migration.tf
@@ -59,7 +59,7 @@ resource "aws_lambda_function" "db-migrations" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("db-migrations/flyway/flyway-all.jar")
+  source_code_hash = filebase64sha256("/flyway/lambda/flyway-all.jar")
 
   runtime = "java11"
 
