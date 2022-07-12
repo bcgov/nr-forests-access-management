fcc53593bc55b2346b858b71e84b33587746be52
Conrad Gustafson <c.gustafson@cgi.com>
2022-07-12 09:19:00 -0700

    temp: disable build to test flyway

--- flyway/flyway-invoke.tf
+++ flyway/flyway-invoke.tf
@@ -32,7 +32,7 @@ locals {
 # Also need to get the connection string to the Aurora instance
 
 data "aws_db_instance" "database" {
-  name = var.db_instance_name
+  db_instance_identifier = var.db_instance_name
 }
 
 # Invoke the lambda function
