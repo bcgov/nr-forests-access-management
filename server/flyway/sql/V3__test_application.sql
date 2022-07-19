9d5e69cc8eed7f61c8b46d99d876eb19e0cc5983
Conrad Gustafson <c.gustafson@cgi.com>
2022-07-18 17:04:57 -0700

    test: testing gh pr pipeline

---
+++ server/flyway/sql/V3__test_application.sql
@@ -0,0 +1,13 @@
+INSERT INTO app_fam.fam_application (
+    application_name,
+    application_description,
+    create_user,
+    update_user
+)
+VALUES (
+    '333',
+    'N0t R3AL',
+    'fam_proxy_api',
+    'fam_proxy_api'
+)
+;
