4c799ebc94b4feb7a143105a6b2bb96d1782545c
Tolu Agbaje <toluagbaje@gmail.com>
2022-07-29 15:15:34 -0700

    Update artifact location

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -41,12 +41,12 @@ resource "aws_iam_role" "fam_api_lambda_exec" {
 }
 
 resource "aws_lambda_function" "fam_api" {
-  filename      = "${path.module}/fam_api.zip"
+  filename      = "${path.module}/fam_api"
   function_name = "fam_api"
   role          = aws_iam_role.fam_api_lambda_exec.arn
   handler = "app.main.handler"
 
-  source_code_hash = filebase64sha256("${path.module}/fam_api.zip")
+  source_code_hash = filebase64sha256("${path.module}/fam_api")
 
   runtime = "python3.8"
 
