b061993c84b68eaa6a6f504c93759e9f5c0d04c6
Tolu Agbaje <toluagbaje@gmail.com>
2022-07-29 14:51:53 -0700

    Update lamda function package name

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -41,12 +41,12 @@ resource "aws_iam_role" "fam_api_lambda_exec" {
 }
 
 resource "aws_lambda_function" "fam_api" {
-  filename      = "${path.module}/fam_api/function.zip"
+  filename      = "fam_api.zip"
   function_name = "fam_api"
   role          = aws_iam_role.fam_api_lambda_exec.arn
   handler = "app.main.handler"
 
-  source_code_hash = filebase64sha256("${path.module}/fam_api/function.zip")
+  source_code_hash = filebase64sha256("fam_api.zip")
 
   runtime = "python3.8"
 
