94cb10a6af735f05118851fe4c51610de0138bad
Tolu Agbaje <toluagbaje@gmail.com>
2022-08-02 22:06:02 -0700

    Fix Test path

--- server/fam_api/app/test/test_core.py
+++ server/fam_api/app/test/test_core.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
-from api.main import app
+from app.main import app
 
 client = TestClient(app)
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Welcome to FAM"}
