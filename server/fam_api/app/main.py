9c133bd12bbd761c8f0fa316f0485e2144223fcd
Tolu Agbaje <95650688+explorertolu@users.noreply.github.com>
2022-08-25 15:36:20 -0700

    Add database connection variables (#132)

--- server/fam_api/app/main.py
+++ server/fam_api/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import psycopg2
 
 from app.api.api_v1.api import router as api_router
 from mangum import Mangum
@@ -8,7 +10,16 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+    connection = psycopg2.connect(host=os.environ.get('PG_HOST'),
+                                  port=os.environ.get('PG_PORT'),
+                                  user=os.environ.get('PG_USER'),
+                                  password=os.environ.get('PG_PASSWORD'),
+                                  dbname=os.environ.get('PG_DATABASE')
+                                  )
+    connection.autocommit = True  # Ensure data is added to the database immediately after write commands
+    cursor = connection.cursor()
+    cursor.execute('SELECT %s as connected;', ('Connection to postgres successful!',))
+    return {"message": cursor.fetchone()}
 
 app.include_router(api_router, prefix="/api/v1")
 handler = Mangum(app)
