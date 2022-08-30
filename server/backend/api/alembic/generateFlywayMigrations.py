e0301b55b329a687a4d3ed62e798b04342b56446
franTarkenton <kevin.netherton@gov.bc.ca>
2022-08-29 18:48:58 -0700

    Adding seed data to alembic migration
    * figure out the pattern for adding seed data using alembic
    * added the seed data in the V2 flyway migration to alembic

--- server/backend/api/alembic/generateFlywayMigrations.py
+++ server/backend/api/alembic/generateFlywayMigrations.py
@@ -1,12 +1,37 @@
 """
 when deploying to AWS the FAM api will use flyway to run the migrations to
 a RDS database
+
+This script can be run at any time.  It will ensure that any alembic migration
+files have corresponding flyway migration files.
+
+How it works:
+
+1. alembic configuration has been modified to name migrations using the flyway
+    migration naming syntax, ie V1_<migration_name>... V2_<migration_name> etc
+
+2. Script will iterate through the alembic migration files, for each migration
+    file will look for a corresponding file in the flyway directory
+
+3. If an alembic migration file exists without a corresponding flyway migration
+    a flyway migration will be generated.
+
+Assumptions:
+    * flyway migrations will always move forward, a rollback actually will
+        be a new migration that does an undo.
+    * will not use alembic branches
+
+Future considerations
+    * current flow only allows for migrations to flow from alembic.
+    * Could potentially implement a way of supporting migrations from either
+        flyway OR alembic, but creating a script that autogenerates a
+        model-autogen.py file using sqlacodegen, then having a model.py that
+        subclasses the model-autogen.py and implements alembic changes from
+        that class: Needs research spike to confirm it would work.
+
 """
-import sys
-import os
-
-
 import logging
+import os
 import re
 
 LOGGER = None
@@ -36,6 +61,15 @@ class CreateFlywayMigraions():
 
     def createFlywayMigration(self, alembicMigrationFilePath):
         # TODO: finish here
+        LOGGER.debug(f"alembicMigrationFilePath: {alembicMigrationFilePath}")
+        # extract the previous and the current version from the migration
+        # can get migrations either from the names of the files or by
+        # running `alembic show <version>` example: `alembic show V1`
+        #
+        # run the alembic migration
+        # alembic upgrade <prev ver>:<cur ver> --sql > <flyway migration file>
+        #
+
 
 
     def flywayMigrationExists(self, alembicMigrationFilePath):
