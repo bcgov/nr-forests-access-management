2a9ef0d7c11e732672d4f566d4ce6ea15f773bff
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2022-08-30 17:54:25 -0700

    fixed rds proxy log group and iam-role problems (#159)

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -98,7 +98,7 @@ data "aws_iam_policy_document" "api_user_rds_proxy_secret_access_policydoc" {
 }
 
 resource "aws_iam_role" "api_user_rds_proxy_secret_access_role" {
-  name = "fam-rds_proxy_secret_access_role"
+  name = "api_user_rds_proxy_secret_access_role"
   assume_role_policy = data.aws_iam_policy_document.api_user_rds_proxy_secret_access_policydoc.json
 }
 
@@ -139,10 +139,12 @@ module "rds_proxy" {
 
   role_arn = aws_iam_role.api_user_rds_proxy_secret_access_role.arn
 
-  name                   = "api-rds-proxy"
-  iam_role_name          = "api-rds-proxy_role"
+  name                   = "fam-api-rds-proxy"
   vpc_security_group_ids = [data.aws_security_group.a.id]
   vpc_subnet_ids         = [data.aws_subnet.a.id, data.aws_subnet.b.id]
+  manage_log_group       = false
+  create_iam_policy      = false
+  create_iam_role        = false
 
   db_proxy_endpoints = {
     read_write = {
