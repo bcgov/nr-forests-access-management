02e49ce6f22f2e9118c5dab0dca011972b7690fc
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-08-30 17:39:41 -0700

    removed log group from rds-proxy module

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -98,7 +98,7 @@ data "aws_iam_policy_document" "api_user_rds_proxy_secret_access_policydoc" {
 }
 
 resource "aws_iam_role" "api_user_rds_proxy_secret_access_role" {
-  name = "fam-rds_proxy_secret_access_role"
+  name = "api_user_rds_proxy_secret_access_role"
   assume_role_policy = data.aws_iam_policy_document.api_user_rds_proxy_secret_access_policydoc.json
 }
 
@@ -137,12 +137,13 @@ resource "aws_iam_role_policy" "api_user_rds_proxy_secret_access_policy" {
 module "rds_proxy" {
   source = "terraform-aws-modules/rds-proxy/aws"
 
-  role_arn = aws_iam_role.api_user_rds_proxy_secret_access_role.arn
+  # role_arn = aws_iam_role.api_user_rds_proxy_secret_access_role.arn
 
-  name                   = "api-rds-proxy"
-  iam_role_name          = "api-rds-proxy_role"
+  name                   = "fam-api-rds-proxy"
+  iam_role_name          = "fam-api-rds-proxy_role"
   vpc_security_group_ids = [data.aws_security_group.a.id]
   vpc_subnet_ids         = [data.aws_subnet.a.id, data.aws_subnet.b.id]
+  manage_log_group       = false
 
   db_proxy_endpoints = {
     read_write = {
