742daf4ad873a497f3b48d1391da25f287ac9123
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-08-30 17:47:33 -0700

    added role back to proxy

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -137,13 +137,14 @@ resource "aws_iam_role_policy" "api_user_rds_proxy_secret_access_policy" {
 module "rds_proxy" {
   source = "terraform-aws-modules/rds-proxy/aws"
 
-  # role_arn = aws_iam_role.api_user_rds_proxy_secret_access_role.arn
+  role_arn = aws_iam_role.api_user_rds_proxy_secret_access_role.arn
 
   name                   = "fam-api-rds-proxy"
-  iam_role_name          = "fam-api-rds-proxy_role"
   vpc_security_group_ids = [data.aws_security_group.a.id]
   vpc_subnet_ids         = [data.aws_subnet.a.id, data.aws_subnet.b.id]
   manage_log_group       = false
+  create_iam_policy      = false
+  create_iam_role        = false
 
   db_proxy_endpoints = {
     read_write = {
