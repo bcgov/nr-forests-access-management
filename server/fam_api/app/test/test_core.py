c0e34adbb116047be4210945c314c7a02795f69a
Tolu Agbaje <toluagbaje@gmail.com>
2022-08-31 17:27:25 -0700

    Add run test

--- server/fam_api/app/test/test_core.py
+++ server/fam_api/app/test/test_core.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to FAM"}
+    assert response.json() == {"message": cursor.fetchone()}
