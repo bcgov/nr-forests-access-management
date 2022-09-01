d1ef623a45a835d25993006064d3f59f782d171e
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2022-09-01 14:11:54 -0700

    removed session pinning filter (#168)

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -191,7 +191,7 @@ resource "aws_db_proxy_default_target_group" "example" {
     init_query                   = "SET x=1, y=2"
     max_connections_percent      = 100
     max_idle_connections_percent = 50
-    session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
+    session_pinning_filters      = []
   }
 }
 
