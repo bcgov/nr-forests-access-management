77657a366fc225d9a6afbd91d72542abf893475b
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-08-31 17:23:24 -0700

    feat: modifying sample api for rds_proxy

--- server/fam_api.tf
+++ server/fam_api.tf
@@ -18,14 +18,6 @@ resource "aws_iam_role_policy" "fam-api_lambda_access_policy" {
             "ec2:UnassignPrivateIpAddresses"
         ],
         "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "secretsmanager:DescribeSecret",
-          "secretsmanager:GetSecretValue"
-        ],
-        "Resource": "${aws_secretsmanager_secret.secret_api_DB.arn}"
       }
     ]
   }
@@ -66,10 +58,10 @@ resource "aws_lambda_function" "fam-api" {
   environment {
 
     variables = {
-      DB_SECRET = "${var.db_api_creds_secretname}"
       PG_DATABASE = "${var.db_name}"
-      PG_PORT = "${data.aws_rds_cluster.database.port}"
-      PG_HOST = "${data.aws_rds_cluster.database.endpoint}"
+      PG_HOST = "${module.rds_proxy.proxy_endpoint}"
+      PG_USER = "${var.api_db_username}"
+      PG_REGION = "${var.aws_region.default}"
     }
 
   }
@@ -77,13 +69,6 @@ resource "aws_lambda_function" "fam-api" {
   tags = {
     "managed-by" = "terraform"
   }
-}
-
-
-# Setting up database proxy for the api
-
-data "aws_secretsmanager_secret" "api_db_secret" {
-  arn = "arn:aws:secretsmanager:ca-central-1:521834415778:secret:rds-db-credentials/cluster-PHNYJBMNHD44C62N7AZIY2ICV4/fam_proxy_api-sRqbjb"
 }
 
 data "aws_iam_policy_document" "api_user_rds_proxy_secret_access_policydoc" {
@@ -109,26 +94,7 @@ resource "aws_iam_role_policy" "api_user_rds_proxy_secret_access_policy" {
   {
     "Version": "2012-10-17",
     "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "secretsmanager:GetRandomPassword",
-          "secretsmanager:ListSecrets"
-        ],
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "secretsmanager:GetResourcePolicy",
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret",
-          "secretsmanager:ListSecretVersionIds"
-        ],
-        "Resource": [
-          "${data.aws_secretsmanager_secret.api_db_secret.arn}"
-        ]
-      }
+
     ]
   }
   EOF
@@ -154,23 +120,14 @@ module "rds_proxy" {
       tags = {
         "managed-by" = "terraform"
       }
-    },
-    read_only = {
-      name                   = "read-only-endpoint"
-      vpc_security_group_ids = [data.aws_security_group.a.id]
-      vpc_subnet_ids         = [data.aws_subnet.a.id, data.aws_subnet.b.id]
-      target_role            = "READ_ONLY"
-      tags = {
-        "managed-by" = "terraform"
-      }
     }
   }
 
   secrets = {
     "api_user" = {
-      description = data.aws_secretsmanager_secret.api_db_secret.description
-      arn         = data.aws_secretsmanager_secret.api_db_secret.arn
-      kms_key_id  = data.aws_secretsmanager_secret.api_db_secret.kms_key_id
+      description = aws_secretsmanager_secret.secret_api_DB.description
+      arn         = aws_secretsmanager_secret.secret_api_DB.arn
+      kms_key_id  = aws_secretsmanager_secret.secret_api_DB.kms_key_id
     }
   }
 
