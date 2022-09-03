3c44e306b6ac1ff5c17a73b8d0ad38d4fb2d342c
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2022-09-02 18:03:49 -0700

    changed to serverless instance (#174)

--- server/rds-cluster.tf
+++ server/rds-cluster.tf
@@ -25,126 +25,154 @@ variable "famdb_master_username" {
   sensitive   = true
 }
 
+resource "random_password" "famdb_mastercreds_random_password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+# resource "aws_rds_cluster" "famdb_cluster" {
+#   allocated_storage                   = "1"
+#   availability_zones                  = ["ca-central-1a", "ca-central-1b"]
+#   backtrack_window                    = "0"
+#   backup_retention_period             = "7"
+#   cluster_identifier                  = random_pet.famdb_cluster_name.id
+#   cluster_members                     = [aws_db_instance.famdb_cluster_ca_central_1a.id, aws_db_instance.famdb_cluster_ca_central_1b.id]
+#   copy_tags_to_snapshot               = "true"
+#   db_cluster_parameter_group_name     = "default.aurora-postgresql13"
+#   db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
+#   deletion_protection                 = "true"
+#   enable_http_endpoint                = "false"
+#   engine                              = "aurora-postgresql"
+#   engine_mode                         = "provisioned"
+#   engine_version                      = "13.7"
+#   iam_database_authentication_enabled = "false"
+#   iops                                = "0"
+#   kms_key_id                          = data.aws_kms_alias.rds_key.id
+#   master_username                     = var.famdb_master_username
+#   master_password                     = random_password.famdb_mastercreds_random_password.result
+#   port                                = "5432"
+#   preferred_backup_window             = "08:49-09:19"
+#   preferred_maintenance_window        = "wed:09:21-wed:09:51"
+
+#   serverlessv2_scaling_configuration {
+#     max_capacity = "1.0"
+#     min_capacity = "0.5"
+#   }
+
+#   storage_encrypted      = "true"
+#   vpc_security_group_ids = [data.aws_security_group.a.id]
+
+#   tags = {
+#     managed-by = "terraform"
+#   }
+# }
+
 resource "aws_rds_cluster" "famdb_cluster" {
-  allocated_storage                   = "1"
-  availability_zones                  = ["ca-central-1a", "ca-central-1b"]
-  backtrack_window                    = "0"
-  backup_retention_period             = "7"
-  cluster_identifier                  = random_pet.famdb_cluster_name.id
-  cluster_members                     = [aws_db_instance.famdb_cluster_ca_central_1a.id, aws_db_instance.famdb_cluster_ca_central_1b.id]
-  copy_tags_to_snapshot               = "true"
-  db_cluster_parameter_group_name     = "default.aurora-postgresql13"
-  db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
-  deletion_protection                 = "true"
-  enable_http_endpoint                = "false"
-  engine                              = "aurora-postgresql"
-  engine_mode                         = "provisioned"
-  engine_version                      = "13.7"
-  iam_database_authentication_enabled = "false"
-  iops                                = "0"
-  kms_key_id                          = data.aws_kms_alias.rds_key.id
+  cluster_identifier = random_pet.famdb_cluster_name.id
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "13.6"
+  database_name      = random_pet.famdb_cluster_name.id
   master_username                     = var.famdb_master_username
-  master_password                     = random_password.db_password.result
-  port                                = "5432"
-  preferred_backup_window             = "08:49-09:19"
-  preferred_maintenance_window        = "wed:09:21-wed:09:51"
+  master_password                     = random_password.famdb_mastercreds_random_password.result
 
   serverlessv2_scaling_configuration {
-    max_capacity = "64"
-    min_capacity = "8"
-  }
-
-  storage_encrypted      = "true"
-  vpc_security_group_ids = [data.aws_security_group.a.id]
-
-  tags = {
-    managed-by = "terraform"
+    max_capacity = 1.0
+    min_capacity = 0.5
   }
 }
 
-resource "aws_db_instance" "famdb_cluster_ca_central_1a" {
-  allocated_storage                   = "1"
-  auto_minor_version_upgrade          = "true"
-  availability_zone                   = "ca-central-1a"
-  backup_retention_period             = "7"
-  backup_window                       = "08:49-09:19"
-  ca_cert_identifier                  = "rds-ca-2019"
-  copy_tags_to_snapshot               = "false"
-  customer_owned_ip_enabled           = "false"
-  db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
-  deletion_protection                 = "false"
-  engine                              = "aurora-postgresql"
-  engine_version                      = "13.7"
-  iam_database_authentication_enabled = "false"
-  identifier                          = "${random_pet.famdb_cluster_name.id}-ca-central-1a"
-  instance_class                      = "db.serverless"
-  kms_key_id                          = data.aws_kms_alias.rds_key.id
-  license_model                       = "postgresql-license"
-  maintenance_window                  = "tue:05:31-tue:06:01"
-  max_allocated_storage               = "0"
-  # only for enhanced monitoring metrics sent to CloudWatch logs
-  #   monitoring_interval                   = "60"
-  #   monitoring_role_arn                   = "arn:aws:iam::521834415778:role/rds-monitoring-role"
-  multi_az                              = "false"
-  option_group_name                     = "default:aurora-postgresql-13"
-  parameter_group_name                  = "default.aurora-postgresql13"
-  performance_insights_enabled          = "false"
-  performance_insights_retention_period = "0"
-  port                                  = "5432"
-  publicly_accessible                   = "false"
-  storage_encrypted                     = "true"
-  storage_type                          = "standard"
-  username                              = var.famdb_master_username
-  password                              = random_password.db_password.result
-  vpc_security_group_ids                = [data.aws_security_group.a.id]
-
-  tags = {
-    managed-by = "terraform"
-  }
-
+resource "aws_rds_cluster_instance" "famdb_cluster_instance" {
+  cluster_identifier = aws_rds_cluster.famdb_cluster.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.famdb_cluster.engine
+  engine_version     = aws_rds_cluster.famdb_cluster.engine_version
 }
 
-resource "aws_db_instance" "famdb_cluster_ca_central_1b" {
-  allocated_storage                   = "1"
-  auto_minor_version_upgrade          = "true"
-  availability_zone                   = "ca-central-1b"
-  backup_retention_period             = "7"
-  backup_window                       = "08:49-09:19"
-  ca_cert_identifier                  = "rds-ca-2019"
-  copy_tags_to_snapshot               = "false"
-  customer_owned_ip_enabled           = "false"
-  db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
-  deletion_protection                 = "false"
-  engine                              = "aurora-postgresql"
-  engine_version                      = "13.7"
-  iam_database_authentication_enabled = "false"
-  identifier                          = "${random_pet.famdb_cluster_name.id}-ca-central-1b"
-  instance_class                      = "db.serverless"
-  kms_key_id                          = data.aws_kms_alias.rds_key.id
-  license_model                       = "postgresql-license"
-  maintenance_window                  = "thu:06:54-thu:07:24"
-  max_allocated_storage               = "0"
-  # only for enhanced monitoring metrics sent to CloudWatch logs
-  #   monitoring_interval                   = "60"
-  #   monitoring_role_arn                   = "arn:aws:iam::521834415778:role/rds-monitoring-role"
-  multi_az                              = "false"
-  option_group_name                     = "default:aurora-postgresql-13"
-  parameter_group_name                  = "default.aurora-postgresql13"
-  performance_insights_enabled          = "false"
-  performance_insights_retention_period = "0"
-  port                                  = "5432"
-  publicly_accessible                   = "false"
-  storage_encrypted                     = "true"
-  storage_type                          = "standard"
-  username                              = var.famdb_master_username
-  password                              = random_password.db_password.result
-  vpc_security_group_ids                = [data.aws_security_group.a.id]
+# resource "aws_db_instance" "famdb_cluster_ca_central_1a" {
+#   allocated_storage                   = "1"
+#   auto_minor_version_upgrade          = "true"
+#   availability_zone                   = "ca-central-1a"
+#   backup_retention_period             = "7"
+#   backup_window                       = "08:49-09:19"
+#   ca_cert_identifier                  = "rds-ca-2019"
+#   copy_tags_to_snapshot               = "false"
+#   customer_owned_ip_enabled           = "false"
+#   db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
+#   deletion_protection                 = "false"
+#   engine                              = "aurora-postgresql"
+#   engine_version                      = "13.7"
+#   iam_database_authentication_enabled = "false"
+#   identifier                          = "${random_pet.famdb_cluster_name.id}-ca-central-1a"
+#   instance_class                      = "db.serverless"
+#   kms_key_id                          = data.aws_kms_alias.rds_key.id
+#   license_model                       = "postgresql-license"
+#   maintenance_window                  = "tue:05:31-tue:06:01"
+#   max_allocated_storage               = "0"
+#   # only for enhanced monitoring metrics sent to CloudWatch logs
+#   #   monitoring_interval                   = "60"
+#   #   monitoring_role_arn                   = "arn:aws:iam::521834415778:role/rds-monitoring-role"
+#   multi_az                              = "false"
+#   option_group_name                     = "default:aurora-postgresql-13"
+#   parameter_group_name                  = "default.aurora-postgresql13"
+#   performance_insights_enabled          = "false"
+#   performance_insights_retention_period = "0"
+#   port                                  = "5432"
+#   publicly_accessible                   = "false"
+#   storage_encrypted                     = "true"
+#   storage_type                          = "io1"
+#   username                              = var.famdb_master_username
+#   password                              = random_password.db_password.result
+#   vpc_security_group_ids                = [data.aws_security_group.a.id]
 
-  tags = {
-    managed-by = "terraform"
-  }
+#   tags = {
+#     managed-by = "terraform"
+#   }
 
-}
+# }
+
+# resource "aws_db_instance" "famdb_cluster_ca_central_1b" {
+#   allocated_storage                   = "1"
+#   auto_minor_version_upgrade          = "true"
+#   availability_zone                   = "ca-central-1b"
+#   backup_retention_period             = "7"
+#   backup_window                       = "08:49-09:19"
+#   ca_cert_identifier                  = "rds-ca-2019"
+#   copy_tags_to_snapshot               = "false"
+#   customer_owned_ip_enabled           = "false"
+#   db_subnet_group_name                = aws_db_subnet_group.famdb_subnet_group.name
+#   deletion_protection                 = "false"
+#   engine                              = "aurora-postgresql"
+#   engine_version                      = "13.7"
+#   iam_database_authentication_enabled = "false"
+#   identifier                          = "${random_pet.famdb_cluster_name.id}-ca-central-1b"
+#   instance_class                      = "db.serverless"
+#   kms_key_id                          = data.aws_kms_alias.rds_key.id
+#   license_model                       = "postgresql-license"
+#   maintenance_window                  = "thu:06:54-thu:07:24"
+#   max_allocated_storage               = "0"
+#   # only for enhanced monitoring metrics sent to CloudWatch logs
+#   #   monitoring_interval                   = "60"
+#   #   monitoring_role_arn                   = "arn:aws:iam::521834415778:role/rds-monitoring-role"
+#   multi_az                              = "false"
+#   option_group_name                     = "default:aurora-postgresql-13"
+#   parameter_group_name                  = "default.aurora-postgresql13"
+#   performance_insights_enabled          = "false"
+#   performance_insights_retention_period = "0"
+#   port                                  = "5432"
+#   publicly_accessible                   = "false"
+#   storage_encrypted                     = "true"
+#   storage_type                          = "io1"
+#   username                              = var.famdb_master_username
+#   password                              = random_password.db_password.result
+#   vpc_security_group_ids                = [data.aws_security_group.a.id]
+
+#   tags = {
+#     managed-by = "terraform"
+#   }
+
+# }
 
 resource "aws_db_subnet_group" "famdb_subnet_group" {
   description = "For Aurora cluster ${random_pet.famdb_cluster_name.id}"
@@ -174,7 +202,7 @@ resource "aws_secretsmanager_secret_version" "famdb_mastercreds_secret_version" 
   secret_string = <<EOF
    {
     "username": "${var.famdb_master_username}",
-    "password": "${random_password.db_password.result}"
+    "password": "${random_password.famdb_mastercreds_random_password.result}"
    }
 EOF
 }
