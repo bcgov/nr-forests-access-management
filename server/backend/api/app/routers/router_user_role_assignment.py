87bfa4af3a20b8675136ef9c6457fcf2a286472c
Ian Liu <ianliuwk1019@gmail.com>
2022-09-08 18:31:24 -0700

    CreateFamUserRoleAssignment draft implementation.

--- server/backend/api/app/routers/router_user_role_assignment.py
+++ server/backend/api/app/routers/router_user_role_assignment.py
@@ -20,11 +20,12 @@ def create_user_role_assignment(
     db: Session = Depends(dependencies.get_db),
 ):
     """
-    Create FAM user_role Association.
+    Create FAM user_role_xref association.
     """
     LOGGER.debug(f"running router ... {db}")
     createData = crud_user_role.createFamUserRoleAssignment(
         userRoleAssignmentRequset,
         db
     )
+    LOGGER.debug(f"User/Role assignment executed successfully, id: ${createData.user_role_xref_id}")
     return createData
