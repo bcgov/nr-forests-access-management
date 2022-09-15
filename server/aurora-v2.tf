1cc983047e80357d3ef5888db8909c08b41072d3
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-09-14 17:59:01 -0700

    got rid of init query

--- server/aurora-v2.tf
+++ server/aurora-v2.tf
@@ -247,7 +247,7 @@ resource "aws_db_proxy_default_target_group" "famdb_proxy_api_target_group" {
 
   connection_pool_config {
     connection_borrow_timeout    = 120
-    init_query                   = "SET x=1, y=2"
+    # init_query                   = "SET x=1, y=2"
     max_connections_percent      = 100
     max_idle_connections_percent = 50
     # session_pinning_filters      = [ "EXCLUDE_VARIABLE_SETS" ]
