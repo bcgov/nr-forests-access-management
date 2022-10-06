358e12d0e19e412020ddb17c916fea126dc4ebdc
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-10-06 15:37:30 -0700

    fix: 23 test skeleton

--- server/auth_function/function/lambda_function.py
+++ server/auth_function/function/lambda_function.py
@@ -1,11 +1,51 @@
 import os
 import logging
+from pickle import NONE
 import jsonpickle
 import boto3
+import json
+import psycopg2
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+db_connection = None
+testing = False
+
+def obtain_db_connection():
+    global db_connection
+
+    if db_connection is None:
+        secret_name = os.environ.get('DB_SECRET')
+        region_name = "ca-central-1"
+
+        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name=region_name
+        )
+        secret_value = client.get_secret_value( SecretId=secret_name )
+        secret_json = json.loads(secret_value['SecretString'])
+        username = secret_json['username']
+        password = secret_json['password']
+
+        connection = psycopg2.connect(host=os.environ.get('PG_HOST'),
+                                        port=os.environ.get('PG_PORT'),
+                                        dbname=os.environ.get('PG_DATABASE'),
+                                        user=username,
+                                        password=password,
+                                        sslmode='disable')
+        connection.autocommit = False  # Ensure data is added to the database immediately after write commands
+        
+        db_connection = connection
+    return db_connection
+
+
+def release_db_connection(connection):
+    if not testing:
+        db_connection.commit()
+        db_connection.close()
 
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
@@ -13,24 +53,22 @@ def lambda_handler(event, context):
     logger.info('## CONTEXT\r' + jsonpickle.encode(context))
 
     # grab requestor's email address
-    email = event['request']['userAttributes']['email']
+    # email = event['request']['userAttributes']['email']
+
+    connection = obtain_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("select application_description description from app_fam.fam_application app where app.application_name = 'fam';")
+    query_result = cursor.fetchone()
+    app_description = query_result[0]
+
+    release_db_connection(connection)
     
-    # placeholder variable
-    pet_preference = ''
-    
-    # set preference to 'dogs' if email contains @amazon.com
-    # otherwise preference is 'cats'
-    if "@amazon.com" in email:
-        pet_preference = 'dogs'
-    else:
-        pet_preference = 'cats'
-    
-    # this allows us to override claims in the id token
-    # "claimsToAddOrOverride" is the important part 
     event["response"]["claimsOverrideDetails"] = { 
         "claimsToAddOrOverride": { 
-            "pet_preference": pet_preference 
+            "famAuthorization": {
+                "appDescription": app_description 
             }
         } 
-
+    }
+         
     return event
