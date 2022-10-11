e7e3dfd291c6c7700143031a21cafd06769abfa4
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2022-10-11 11:17:47 -0700

    fix: 23 added permission for lambda function (#231)

--- server/auth_lambda.tf
+++ server/auth_lambda.tf
@@ -105,3 +105,11 @@ resource "aws_lambda_function" "fam-auth-function" {
     "managed-by" = "terraform"
   }
 }
+
+resource "aws_lambda_permission" "allow_cognito_invoke_trigger" {
+  statement_id  = "${random_pet.auth_lambda_name.id}-allowCognito"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fam-auth-function.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.fam_user_pool.arn
+}
