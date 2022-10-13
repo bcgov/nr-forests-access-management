5dd17c8e01cfc7d48ad1dfb3825807ba7d16141c
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-10-13 11:50:11 -0700

    debug: 23 lambda UserLambdaValidationException 3

--- server/auth_function/lambda_function.py
+++ server/auth_function/lambda_function.py
@@ -60,16 +60,21 @@ def lambda_handler(event, context):
     # cursor.execute("select application_description description from app_fam.fam_application app where app.application_name = 'fam';")
     # query_result = cursor.fetchone()
     # app_description = query_result[0]
-    app_description = "Hard Coded"
 
     # release_db_connection(connection)
     
+    # event["response"]["claimsOverrideDetails"] = { 
+    #     "claimsToAddOrOverride": { 
+    #         "famAuthorization": {
+    #             "appDescription": app_description 
+    #         }
+    #     } 
+    # }
+
     event["response"]["claimsOverrideDetails"] = { 
         "claimsToAddOrOverride": { 
-            "famAuthorization": {
-                "appDescription": app_description 
+                "pet_preference": "cats" 
             }
         } 
-    }
          
     return event
