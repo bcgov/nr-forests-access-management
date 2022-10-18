c70c37bdf86e129d98c4437c0bca90236ffdd186
Ian Liu <ianliuwk1019@gmail.com>
2022-10-17 20:29:58 -0700

    Add note and fix for Delete method with 204.

--- server/backend/api/app/routers/router_user_role_assignment.py
+++ server/backend/api/app/routers/router_user_role_assignment.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 import logging
 
 from api.app.crud import crud_user_role
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 
 from .. import dependencies, schemas
@@ -31,12 +31,22 @@ def create_user_role_assignment(
     return createData
 
 
-@router.delete("/{user_role_xref_id}", status_code=HTTPStatus.NO_CONTENT)
+@router.delete(
+    "/{user_role_xref_id}",
+    status_code=HTTPStatus.NO_CONTENT,
+    response_class=Response
+)
 def delete_user_role_assignment(
     user_role_xref_id: int, db: Session = Depends(dependencies.get_db)
 ) -> None:
     """
     Delete FAM user_role_xref association.
+    """
+    """
+    Note! Ther appear to be a bug in FasAPI/Starlette, when there is 204 No-Content code (like Delete)
+    but for some reason response still has content and throw error. 
+    To fix: see this => https://lightrun.com/answers/tiangolo-fastapi-response-content-longer-than-content-length-error-for-delete-and-nocontent 
+    (response_class=Response) is added to @router.delete with 204 status.
     """
     crud_user_role.deleteFamUserRoleAssignment(db, user_role_xref_id)
     LOGGER.debug(f"User/Role assignment deleted successfully, id: {user_role_xref_id}")
