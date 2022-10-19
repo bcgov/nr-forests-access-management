8243b5aae1fd7c5ddf74f8313a756b25bd9acdf4
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-10-19 14:47:06 -0700

    fix: 21-auth-lookup started new test

--- server/flyway/sql/V11__user_table_fixes.sql
+++ server/flyway/sql/V11__user_table_fixes.sql
@@ -1,1 +1,3 @@
 ALTER TABLE app_fam.fam_user alter COLUMN cognito_user_id type varchar(100);
+
+ALTER TABLE app_fam.fam_application_client alter COLUMN update_date type timestamp(6) USING update_date::timestamp(6) without time zone;
