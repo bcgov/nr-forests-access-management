b2297a94538fc9c9c782fd652bccc8fd42f29970
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-10-21 16:48:38 -0600

    Use applications object

--- frontend/src/components/ApplicationSelection.vue
+++ frontend/src/components/ApplicationSelection.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const selected = ref(null)
-// TODO: Change to javascript object with id, name, description for each application.
-const applications = ['Forest Operations Map (FOM)', 'Forest Access Management (FAM)', 'Forest Operations Plan (FOP)']
 
-function manage(event) {
+const selected = ref(null)
+
+const applications = [
+  { name: 'FOM', description: 'Forest Operations Map', id: '1001' }, 
+  { name: 'FAM', description: 'Forest Access Management', id: '1002' },
+  { name: 'FOP', description: 'Forest Operations Plan', id: '1003' }]
+
+function manage() {
   alert(`Manage app ${selected.value}`)
-  // alert(event.target.tagName)
 }
 </script>
 
@@ -15,9 +18,9 @@ function manage(event) {
   <div v-if="applications.length">
   <label>Application to Administer</label>
   <br/>
-  <select v-model="selected" :size="applications.length+1" >
+  <select v-model="selected" :size="applications.length+1">
     <!--<option disabled value="">Please select one</option> -->
-    <option v-for="app in applications" :value="app">{{app}}</option>
+    <option v-for="app in applications" :value="app.name+':'+app.id">{{app.description}}</option>
   </select>
   <br />
   <p>Selected application: {{selected}}</p>
