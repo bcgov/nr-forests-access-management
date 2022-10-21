9dd010ad29164256ca4060a1390092bd506b5440
franTarkenton <kevin.netherton@gov.bc.ca>
2022-10-20 18:45:18 -0700

    fix: 21 comply with linters demands

--- server/auth_function/test/conftest.py
+++ server/auth_function/test/conftest.py
@@ -1,19 +1,11 @@
-import pytest
 import logging
 import sys
 import os
 
-modulePath = os.path.join(os.path.dirname(__file__), '..')
+modulePath = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(modulePath)
-import lambda_function
-
-
-
+import lambda_function  # noqa - need import here to make avail to fixtures/tests
 
 LOGGER = logging.getLogger(__name__)
 
-#server/auth_function/test/database_setup_fixtures.py
-pytest_plugins = [
-    "database_setup_fixtures"
-]
-
+pytest_plugins = ["database_setup_fixtures"]
