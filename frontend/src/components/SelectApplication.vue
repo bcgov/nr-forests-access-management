29e685184db68998abfdc14d97e67e2890fcbb73
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-10-25 15:10:47 -0600

    Fix merge conflict.

--- frontend/src/components/SelectApplication.vue
+++ frontend/src/components/SelectApplication.vue
@@ -89,40 +89,13 @@ function manage() {
     <br/>
     <p>Selection: {{selectedApplication}}</p>
     <br/>
-<<<<<<< HEAD
   </div>
   <div v-else>
     <p>Loading...</p>
-=======
-
-  </div>
-  <div v-else>
-    <p>You are not authorized to administer any applications.</p>
->>>>>>> dev
   </div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
