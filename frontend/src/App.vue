d902c8a0479914cc2f0923bd1c8f0d58ba198360
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-10-25 16:59:04 -0600

    Add BC gov logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,7 @@ if (environmentDisplayName.length > 0) {
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="BC Gov logo" class="logo" src="@/assets/bcgov-header-horiz-LG.png" width="140" height="60" />
 
     <div class="wrapper">
       <h1>FAM - Forest Access Management {{environmentLabel}}</h1>
@@ -21,6 +21,7 @@ if (environmentDisplayName.length > 0) {
         <RouterLink to="/application">Select Application</RouterLink>
         <!-- <RouterLink to="/manage">Manage Access</RouterLink> -->
         <RouterLink to="/about">About</RouterLink>
+
       </nav>
     </div>
   </header>
