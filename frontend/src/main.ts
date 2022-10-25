d170603ff74802b63e287debfec5d5959ee0cdbe
Ian Liu <ianliuwk1019@gmail.com>
2022-10-25 14:12:19 -0700

    Add Bootstrap 5 package and adjust vite settings.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/styles/styles.scss'
+import * as bootstrap from 'bootstrap'
 
 const app = createApp(App)
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(async ({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = parseInt(env.VITE_PORT || "5173");
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      }
+    },
+    root: path.resolve(__dirname, 'src'),
+    server: {
+      port: port,
+    },
   }
-})
+});
