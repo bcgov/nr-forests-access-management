c4696399f6093cb107e84e5a03b6a58d905da10a
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-10-25 15:01:06 -0600

    Feat/156 app selection screen (#244)
    * Create application selection page
    * Add very basic functionality for choosing from hardcoded list.
    * Make selection dynamically scale
    * button action works, clean up.
    * Use applications object
    * Add disabled buttons, better object handling.
    * Add fetch from gateway 
    * Fetch app list
    * Start on shared state.
    * Add navigation and breadcrumbs based on selection.
    * Button-driven navigation
    * Get breadcrumb component working.
    * Fix button enabling on select application screen
    * Look into component hot refresh issue
    * Fix hot reloading, auto-load application list.

---
+++ frontend/src/components/ManageAccess.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import router from '@/router';
+import { isApplicationSelected, selectedApplicationDisplayText } from '../services/ApplicationService'
+import Breadcrumb from './Breadcrumb.vue'
+
+var foo = 2;
+</script>
+
+<template>
+  <div>
+  <Breadcrumb activePage='Manage'/>
+
+  <h1>Manage Access - {{selectedApplicationDisplayText}}</h1>
+
+  <button @click="router.push('/grant')" :disabled="isApplicationSelected">Grant Access</button>
+
+  <br/>TODO<br/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>Welcome to FAM</h1>
 </template>
