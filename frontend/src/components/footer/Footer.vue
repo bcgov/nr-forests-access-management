33b056af6cb828012b4f2a1a8a67e9bb6adf12d5
Ian Liu <ianliuwk1019@gmail.com>
2022-11-01 13:27:05 -0700

    Linked to home from footer.

--- frontend/src/components/footer/Footer.vue
+++ frontend/src/components/footer/Footer.vue
@@ -7,7 +7,7 @@
       <div class="clearfix row">
         <div class="col-xs-12 col-sm-12 col-md-12">
           <ul class="gov-links">
-            <li><a href="#">Home</a></li>
+            <li><a href="#" title="Home" @click="$router.push({ name: 'home' })">Home</a></li>
             <li><a href="http://www2.gov.bc.ca/gov/content/home/copyright" target="_blank" rel="noopener">Copyright</a></li>
             <li><a href="http://www2.gov.bc.ca/gov/content/home/disclaimer" target="_blank" rel="noopener">Disclaimer</a></li>
             <li><a href="http://www2.gov.bc.ca/gov/content/home/privacy" target="_blank" rel="noopener">Privacy</a></li>
