9eb73f866ce14932f5c6c49f47a1eedf2118fc46
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-11-01 09:07:03 -0700

    fix: 233 commenting out flyway for debug

--- infrastructure/server/flyway.tf
+++ infrastructure/server/flyway.tf
@@ -10,10 +10,6 @@ data "aws_secretsmanager_secret" "db_flyway_api_creds" {
 
 data "aws_secretsmanager_secret_version" "db_flyway_api_creds_current" {
   secret_id = data.aws_secretsmanager_secret.db_flyway_api_creds.id
-
-  depends_on = [
-    aws_secretsmanager_secret_version.famdb_apicreds_secret_version
-  ]
 }
 
 data "aws_rds_cluster" "flyway_database" {
@@ -82,83 +78,83 @@ resource "aws_iam_role" "flyway_exec" {
   assume_role_policy = data.aws_iam_policy_document.flyway_exec_policydoc.json
 }
 
-resource "aws_lambda_function" "flyway-migrations" {
-  filename      = "${path.module}/flyway-all.jar"
-  function_name = random_pet.flyway_lambda_name.id
-  role          = aws_iam_role.flyway_exec.arn
-  # has to have the form filename.functionname where filename is the file containing the export
-  handler = "com.geekoosh.flyway.FlywayHandler::handleRequest"
+# resource "aws_lambda_function" "flyway-migrations" {
+#   filename      = "${path.module}/flyway-all.jar"
+#   function_name = random_pet.flyway_lambda_name.id
+#   role          = aws_iam_role.flyway_exec.arn
+#   # has to have the form filename.functionname where filename is the file containing the export
+#   handler = "com.geekoosh.flyway.FlywayHandler::handleRequest"
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("${path.module}/flyway-all.jar")
+#   # The filebase64sha256() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+#   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#   source_code_hash = filebase64sha256("${path.module}/flyway-all.jar")
 
-  runtime = "java11"
+#   runtime = "java11"
 
-  vpc_config {
-    subnet_ids         = [data.aws_subnet.a_data.id, data.aws_subnet.b_data.id]
-    security_group_ids = [data.aws_security_group.sg_data.id]
-  }
+#   vpc_config {
+#     subnet_ids         = [data.aws_subnet.a_data.id, data.aws_subnet.b_data.id]
+#     security_group_ids = [data.aws_security_group.sg_data.id]
+#   }
 
-  memory_size = 512
-  timeout     = 240
+#   memory_size = 512
+#   timeout     = 240
 
-  environment {
-    variables = {
-      DB_SECRET      = "${data.aws_secretsmanager_secret.db_flyway_master_creds.name}"
-      FLYWAY_MIXED   = "false"
-      FLYWAY_SCHEMAS = "flyway,app_fam"
-    }
-  }
+#   environment {
+#     variables = {
+#       DB_SECRET      = "${data.aws_secretsmanager_secret.db_flyway_master_creds.name}"
+#       FLYWAY_MIXED   = "false"
+#       FLYWAY_SCHEMAS = "flyway,app_fam"
+#     }
+#   }
 
-  tags = {
-    "managed-by" = "terraform"
-  }
-}
+#   tags = {
+#     "managed-by" = "terraform"
+#   }
+# }
 
 # Everything below here is for invoking flyway.
 
-resource "aws_db_cluster_snapshot" "fam_pre_flyway_snapshot" {
-  db_cluster_identifier          = data.aws_rds_cluster.flyway_database.id
-  db_cluster_snapshot_identifier = var.db_cluster_snapshot_identifier
-  count                          = var.execute_flyway ? 1 : 0
-}
+# resource "aws_db_cluster_snapshot" "fam_pre_flyway_snapshot" {
+#   db_cluster_identifier          = data.aws_rds_cluster.flyway_database.id
+#   db_cluster_snapshot_identifier = var.db_cluster_snapshot_identifier
+#   count                          = var.execute_flyway ? 1 : 0
+# }
 
 # Need to grab the username and password from the database so they can go into the scripts
-locals {
-  flyway_db_creds = jsondecode(data.aws_secretsmanager_secret_version.db_flyway_api_creds_current.secret_string)
-}
+# locals {
+#   flyway_db_creds = jsondecode(data.aws_secretsmanager_secret_version.db_flyway_api_creds_current.secret_string)
+# }
 
 # Run flyway to update the database
 
-data "aws_lambda_invocation" "invoke_flyway_migration" {
-  function_name = aws_lambda_function.flyway-migrations.function_name
+# data "aws_lambda_invocation" "invoke_flyway_migration" {
+#   function_name = aws_lambda_function.flyway-migrations.function_name
 
-  input = <<JSON
-  {
-    "flywayRequest": {
-        "flywayMethod": "MIGRATE",
-        "placeholders": {
-          "api_db_username" : "${local.flyway_db_creds.username}",
-          "api_db_password" : "md5${md5(join("", [local.flyway_db_creds.password, local.flyway_db_creds.username]))}"
-        },
-        "target": "latest"
-    },
-    "dbRequest": {
-        "connectionString": "jdbc:postgresql://${data.aws_rds_cluster.flyway_database.endpoint}/${data.aws_rds_cluster.flyway_database.database_name}"
-    },
-    "gitRequest": {
-        "gitRepository": "${var.github_repository}",
-        "gitBranch": "${var.github_branch}",
-        "folders": "server/flyway/sql"
-    }
-  }
-  JSON
+#   input = <<JSON
+#   {
+#     "flywayRequest": {
+#         "flywayMethod": "MIGRATE",
+#         "placeholders": {
+#           "api_db_username" : "${local.flyway_db_creds.username}",
+#           "api_db_password" : "md5${md5(join("", [local.flyway_db_creds.password, local.flyway_db_creds.username]))}"
+#         },
+#         "target": "latest"
+#     },
+#     "dbRequest": {
+#         "connectionString": "jdbc:postgresql://${data.aws_rds_cluster.flyway_database.endpoint}/${data.aws_rds_cluster.flyway_database.database_name}"
+#     },
+#     "gitRequest": {
+#         "gitRepository": "${var.github_repository}",
+#         "gitBranch": "${var.github_branch}",
+#         "folders": "server/flyway/sql"
+#     }
+#   }
+#   JSON
 
-  depends_on = [
-    aws_db_cluster_snapshot.fam_pre_flyway_snapshot
-  ]
+#   depends_on = [
+#     aws_db_cluster_snapshot.fam_pre_flyway_snapshot
+#   ]
 
-  count = var.execute_flyway ? 1 : 0
-}
+#   count = var.execute_flyway ? 1 : 0
+# }
