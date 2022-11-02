2fccd53136170e96a16fa66730a68d8893e4b18d
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-11-02 10:45:11 -0600

    156 App Selection Screen Part 3 (#256)
    * Create application selection page
    
    * Add very basic functionality for choosing from hardcoded list.
    
    * Make selection dynamically scale
    
    * button action works, clean up.
    
    * Use applications object
    
    * Add disabled buttons, better object handling.
    
    * Add fetch from gateway (not yet working due to cors)
    
    * Fetch app list, remove view.
    
    * Start on shared state.
    
    * Add navigation and breadcrumbs based on selection.
    
    * Button-driven navigation
    
    * Get breadcrumb component working.
    
    * Fix button enabling on select application screen
    
    * Look into component hot refresh issue
    
    * Fix hot reloading, auto-load application list.
    
    * Fix typecheck failure.
    
    * Implement lazy loading of application list.
    
    * Fix merge conflict.
    
    * Add environment-specific parameterization, auto-redirect to manage access if only one application, cleanup.
    
    * More cleanup
    
    * Clean up layout.
    
    * Add BC gov logo
    
    * Fix environment-specific handling using vue's /public directory.
    
    * Move applications list to shared state
    
    * Fix breadcrumb for single-app scenario, other cleanup
    
    * Refactor for  environment settings class
    
    * Add api service, refactor application service name, remove hello world components.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 
-import './assets/main.css'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
