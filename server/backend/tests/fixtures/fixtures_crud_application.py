2a6c7f5c529dd5cbc4415ae653d9dcb6d8ccb871
Kevin Netherton <kevin.netherton@gov.bc.ca>
2022-11-10 14:36:47 -0800

    fix: 126 - getting tests working again

--- server/backend/tests/fixtures/fixtures_crud_application.py
+++ server/backend/tests/fixtures/fixtures_crud_application.py
@@ -7,6 +7,7 @@ from typing import Any, Dict, Generator, Union
 import api.app.schemas as schemas
 from api.app.crud import crud_application as crud_application
 from api.app.crud import crud_role as crud_role
+from api.app.models import model as models
 
 
 LOGGER = logging.getLogger(__name__)
@@ -28,10 +29,13 @@ def dbSession_famApplication_withdata(
         db.delete(appData)
         db.commit()
     except sqlalchemy.orm.exc.ObjectDeletedError as e:
+        LOGGER.debug("app object was already deleted")
+        LOGGER.debug(f"{type(e).__name__}")
+        db.rollback()
+    except Exception as e:
         LOGGER.debug(f"exception: {e}")
         LOGGER.debug(f"{type(e).__name__}")
-        LOGGER.debug("app object was already deleted")
-
+        raise
 
 
 @pytest.fixture(scope="function")
