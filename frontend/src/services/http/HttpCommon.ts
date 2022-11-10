d0c2bd19bc0fa098dd4988593026f6e782d37f7f
Ian Liu <ianliuwk1019@gmail.com>
2022-11-09 21:16:51 -0800

    Add response interceptor.

--- frontend/src/services/http/HttpCommon.ts
+++ frontend/src/services/http/HttpCommon.ts
@@ -1,5 +1,6 @@
 import { EnvironmentSettings } from '@/services/EnvironmentSettings';
 import HttpReqInterceptors from '@/services/http/HttpRequestInterceptors';
+import HttpResInterceptors from '@/services/http/HttpResponseInterceptors';
 import axios from 'axios';
 
 const environmentSettings = new EnvironmentSettings()
@@ -10,16 +11,29 @@ const DEFAULT_REQUEST_TIMEOUT = 5000;
 const defaultAxiosConfig = {
     baseURL: apiBaseUrl,
     timeout: DEFAULT_REQUEST_TIMEOUT,
+    /*
+    Note! This is the default status handling from Axios. Any status other than 2xx will be rejected,
+          and does not show up at Promise 'then(response)' block. We coiuld configure below to change
+          its behaviour.
+
+        validateStatus: function (status) {
+            return status >= 200 && status < 300; // default
+        },
+    */
 };
 
 // Use instance
 const httpInstance = axios.create(defaultAxiosConfig);
 
-// Common default, but individual request will take precedence.
+// Common default settings, but individual request will take precedence.
 httpInstance.defaults.headers.get['Content-type'] = DEFAULT_CONTENT_TYPE;
 
 // Request Interceptors
 httpInstance.interceptors.request.use(HttpReqInterceptors.addAuthHeaderItcpt);
+
+// Response Interceptors
+httpInstance.interceptors.response.use(undefined, HttpResInterceptors.forbiddenStatusItcpt); // Provide error block handling.
+
 
 
 export default httpInstance;
