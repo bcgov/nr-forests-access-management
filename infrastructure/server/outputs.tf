465f6c4823b3a385ed0acc2926deaf2937171872
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-11-14 17:19:15 -0800

    feat: 287 added props to outputs for front-end

--- infrastructure/server/outputs.tf
+++ infrastructure/server/outputs.tf
@@ -2,3 +2,25 @@ output "VUE_APP_API_GW_BASE_URL" {
   description = "Base URL for API Gateway stage."
   value       = aws_api_gateway_deployment.fam_api_gateway_deployment.invoke_url
 }
+
+data "aws_region" "current" {}
+
+output "fam_cognito_region" {
+  description = "Region name for Cognito for front end connection"
+  value       = data.aws_region.current.name
+}
+
+output "fam_user_pool_id" {
+  description = "The ID of the Cognito user pool for front end connection"
+  value       = aws_cognito_user_pool.fam_user_pool.id
+}
+
+output "fam_console_web_client_id" {
+  description = "Web client ID for the FAM OIDC client for front end connection"
+  value       = aws_cognito_user_pool_client.fom_public_oidc_client.id
+}
+
+output "fam_cognito_domain" {
+  description = "Domain associated with the Cognito user pool for front end connection"
+  value       = aws_cognito_user_pool_domain.main.domain
+}
