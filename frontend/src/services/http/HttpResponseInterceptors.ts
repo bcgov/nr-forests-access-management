99cfc45d4a888711dc58694a0739f466af6395f1
Ian Liu <ianliuwk1019@gmail.com>
2022-11-16 14:27:31 -0800

    Implement 403 retry handling.

--- frontend/src/services/http/HttpResponseInterceptors.ts
+++ frontend/src/services/http/HttpResponseInterceptors.ts
@@ -1,21 +1,38 @@
 
 import AuthService from "@/services/AuthService";
+import Http from '@/services/http/HttpCommon';
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
+import { ref } from 'vue';
 /*
 Note! Any status other than 2xx will be rejected (default status handling from Axios unless changed).
 */
 
+const retryCount = ref(0)
+const MAX_RETRY = 2
+
+// --- Interceptors
+
 // Assume it is caught in error block based on default handling.
 async function forbiddenStatusItcpt(error: any) {
-    // 403 special handling.
-    if(error.response.status == 403) {
-        await refreshTokenAndReTry(error.request, error.response);
+    // 403 special handling; until MAX_RETRY reached.
+    if(error.response.status == 403 && (retryCount.value < MAX_RETRY)) {
+        return refreshTokenAndReTry(error.request, error.response);
     }
+
+    retryCount.value = 0 // reset counter when retry ends or not 403.
+    return Promise.reject(error);
 }
 
 async function refreshTokenAndReTry(request: AxiosRequestConfig, response: AxiosResponse) {
-    console.log("refreshing token...")
-    // TODO implement in future.
+    // Refresh token.
+    await AuthService.methods.refreshToken()
+
+    // Try original request again.
+    retryCount.value++
+    return Http.request(request).catch((error) => {
+        console.log("Still encountered error after request retried: ", error)
+        return Promise.reject(error);
+    })
 }
 
 export default { 
