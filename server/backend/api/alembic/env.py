9fbd79b1c3f83a79c251fa09948fe2c5cfca4115
Guy Lafleur <kevin.netherton@gov.bc.ca>
2022-11-28 14:02:24 -0800

    feat: 253 - add docker-compose that starts backend and runs migrations (#307)

--- server/backend/api/alembic/env.py
+++ server/backend/api/alembic/env.py
@@ -1,14 +1,11 @@
+import logging
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+import app.config
+import app.models.model
 from alembic import context
 from alembic.script import ScriptDirectory
-
-from sqlmodel import SQLModel
-import app.models.model
-import app.config
+from sqlalchemy import create_engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,19 +16,26 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-#from app.db.base import Base  # noqa
+# override logging setup for debugging
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.debug("test test test")
+
+
+# from app.db.base import Base  # noqa
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-#target_metadata = None
+# target_metadata = None
 target_metadata = app.models.model.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
 
 def process_revision_directives(context, revision, directives):
     # extract Migration
@@ -44,21 +48,27 @@ def process_revision_directives(context, revision, directives):
         new_rev_id = 1
     else:
         # default branch with incrementation
-        last_rev_id = int(head_revision.lstrip('V'))
+        last_rev_id = int(head_revision.lstrip("V"))
         new_rev_id = last_rev_id + 1
     # fill zeros up to 4 digits: 1 -> 0001
-    #migration_script.rev_id = '{0:04}'.format(new_rev_id)
-    migration_script.rev_id = f'V{new_rev_id}'
+    # migration_script.rev_id = '{0:04}'.format(new_rev_id)
+    migration_script.rev_id = f"V{new_rev_id}"
 
 
 def get_url():
-    # user = os.getenv("POSTGRES_USER", "postgres")
-    # password = os.getenv("POSTGRES_PASSWORD", "")
-    # server = os.getenv("POSTGRES_SERVER", "db")
-    # db = os.getenv("POSTGRES_DB", "app")
-    # url = f"postgresql://{user}:{password}@{server}/{db}"
-    url = app.config.getDBString()
+    url = None
+    x_param_url = context.get_x_argument(as_dictionary=True).get("url")
+    LOGGER.debug(f"x_param_url: {x_param_url}")
+    if x_param_url:
+        url = x_param_url
+        LOGGER.debug(f"url from -x: {url}")
+
+    if not url:
+        url = app.config.getDBString()
+        LOGGER.debug(f"url from app config: {url}")
+    LOGGER.debug(f"captured the url string: {url}")
     return url
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -82,9 +92,8 @@ def run_migrations_offline() -> None:
         dialect_opts={"paramstyle": "named"},
         process_revision_directives=process_revision_directives,
         include_schemas=True,
-        compare_type=True
+        compare_type=True,
     )
-
 
     with context.begin_transaction():
         context.run_migrations()
@@ -97,11 +106,16 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    # connectable = create_engine(get_url())
+    #    with connectable.connect() as connection:
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+    url = get_url()
+    print(f"url: {url}")
+    connectable = create_engine(get_url())
 
     with connectable.connect() as connection:
         context.configure(
