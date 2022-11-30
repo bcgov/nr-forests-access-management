d1e73d967e1fe7d759f6f9d01f4dd96694e8c818
Ian Liu <81595625+ianliuwk1019@users.noreply.github.com>
2022-11-30 11:23:54 -0800

    Feat/178 frontend grant access  input form (#320)
    * Use router 'meta' for title. Add new PageTitle vue component.
    
    * Minor prop adjustment.
    
    * Draft grant access form fields.
    
    * Add button primary scss variable to use.
    
    * Move temporary useToast to ManageAccess.
    
    * Adjust button-info scss to use and adjust slightly SelectApplication layout.
    
    * Bind to model and set default value.
    
    * Remove log

--- frontend/src/components/ManageAccess.vue
+++ frontend/src/components/ManageAccess.vue
@@ -2,8 +2,27 @@
 import PageTitle from '@/components/PageTitle.vue';
 import router from '@/router';
 import { isApplicationSelected, selectedApplication } from '@/services/ApplicationState';
+import { useToast } from 'vue-toastification';
 
 var foo = 2;
+
+function save(result: boolean) {
+  const toast = useToast();
+  if (result) {
+    toast.success("Save successful")
+  } else {
+    toast.warning("Invalid selection.")
+  }  
+}
+
+function saveError() {
+  useToast().error("Save failed due to an error. Please try again. If the error persists then contact support.")
+}
+
+function uncaughtError() {
+  throw new Error("test uncaught error thrown from function")
+}
+
 </script>
 
 <template>
@@ -16,6 +35,12 @@ var foo = 2;
   <p>Selection: {{selectedApplication}}</p>
   <br/>TODO<br/>
   </div>
+  &nbsp;
+  <button @click="save(false)">Validation failure</button>
+  &nbsp;
+  <button @click="saveError()">Save Error</button>
+  &nbsp;
+  <button @click="uncaughtError()">Uncaught Error</button>
 </template>
 
 <style scoped>
