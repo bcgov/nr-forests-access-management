db067dd778adf7dc299b93cef556910ce5957f42
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-12-02 15:09:57 -0700

    Improve default toast behavior.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-import Toast, { useToast, POSITION } from "vue-toastification";
+import Toast, { useToast, POSITION, TYPE } from "vue-toastification";
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 
@@ -17,13 +17,36 @@ Amplify.configure(awsExports); // Config Amplify for Cognito resource.
 const app = createApp(App)
 
 app.use(Toast, {
-    // Defaults for all toast messages
-    position: POSITION.TOP_RIGHT,
-    timeout: 5000, // milliseconds
+    // // Defaults for all toast messages
+    // position: POSITION.TOP_RIGHT,
+    // timeout: 5000, // milliseconds
+    newestOnTop: true,
+
+    // Prevent having multiple messages with the same type and content from being displayed.
+    filterBeforeCreate: (toast, toasts) => {
+      if (toasts.filter(t => t.type === toast.type && t.content === toast.content).length !== 0) {
+        // Returning false discards the toast
+        return false;
+      }
+      return toast;
+    },
+
+    toastDefaults: {
+        // ToastOptions object for each type of toast
+        [TYPE.ERROR]: {
+            position: POSITION.TOP_CENTER,
+            timeout: false,
+        },
+        [TYPE.SUCCESS]: {
+            position: POSITION.TOP_RIGHT,
+            timeout: 4000,
+            hideProgressBar: true,
+        }
+    }
 });
 
 app.config.errorHandler = (err, instance, info) => {
-    // If triggered on component setup/initial render, this can display many times. 
+    // If triggered on component setup/initial render, this can display many times.
     // This can trigger on an uncaught error in a function.
     // This will NOT trigger on an uncaught error in an async setup operation.
     const toast = useToast();
