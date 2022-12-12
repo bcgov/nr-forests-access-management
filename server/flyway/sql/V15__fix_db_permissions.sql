9365c29501d2af95f33159be2713fa7bb3ab95b7
Kevin Netherton <kevin.netherton@gov.bc.ca>
2022-12-12 14:37:10 -0800

    fix/333 - adding the alter statement

--- server/flyway/sql/V15__fix_db_permissions.sql
+++ server/flyway/sql/V15__fix_db_permissions.sql
@@ -4,12 +4,16 @@ BEGIN;
 
 GRANT SELECT, UPDATE, DELETE, INSERT
                 ON ALL TABLES IN SCHEMA app_fam TO
-                ${api_db_username};
+                ${api_db_username};;
 
--- V12/13/14 didn't upgrade the alembic version so forcing to V15 to get it back
--- in sync
+alter default privileges in schema app_fam
+                  grant select, insert, delete, update
+                  on tables to ${api_db_username};;
+
+-- V12/13/14 didn't upgrade the alembic version so forcing to V15 to get
+-- it back in sync
 UPDATE alembic_version SET version_num='V15';
---WHERE alembic_version.version_num = 'V14';
+-- WHERE alembic_version.version_num = 'V14';
 
 COMMIT;
 
