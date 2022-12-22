758304e74a248cbf23c47ac3cc4c757a7bc1b19c
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2022-12-21 18:57:12 -0800

    getting tests to pass (db problems)

--- server/backend/api/app/database.py
+++ server/backend/api/app/database.py
@@ -11,15 +11,16 @@ LOGGER = logging.getLogger(__name__)
 
 SQLALCHEMY_DATABASE_URL = config.get_db_string()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
-LOGGER.debug("database engine created!")
-
 Base = declarative_base()
 
 
 def get_db():
     try:
+        # TODO: Was this stuff initialized (engion and sessionmaker) for
+        # performance? Moved it in here to get tests to pass on GitHub
         LOGGER.debug("starting a new db session")
+        engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+        LOGGER.debug("database engine created!")
         session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
         db = session_local()
         yield db
