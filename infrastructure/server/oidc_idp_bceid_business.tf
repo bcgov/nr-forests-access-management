28ee2c765cb7bcd092f0515bf31a15161ebf5a0a
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2023-01-13 23:21:44 -0800

    328 implement multi-environment stack (#366)

---
+++ infrastructure/server/oidc_idp_bceid_business.tf
@@ -0,0 +1,91 @@
+# BCeID Business IDP that goes to Keycloak and SiteMinder CLP
+
+resource "aws_cognito_identity_provider" "dev_bceid_business_oidc_provider" {
+  user_pool_id  = aws_cognito_user_pool.fam_user_pool.id
+  provider_name = "DEV-BCEIDBUSINESS"
+  provider_type = "OIDC"
+
+  provider_details = {
+    authorize_scopes          = "openid profile email bceidbusiness"
+    client_id                 = var.oidc_bceid_business_idp_client_id
+    client_secret             = var.dev_oidc_bceid_business_idp_client_secret
+    oidc_issuer               = var.dev_oidc_idp_issuer
+    attributes_request_method = "GET"
+  }
+
+  attribute_mapping = {
+    email                      = "email",
+    "custom:idp_name"          = "identity_provider",
+    "custom:idp_user_id"       = "bceid_user_guid",
+    "custom:idp_username"      = "bceid_username",
+    "custom:idp_display_name"  = "display_name"
+    "custom:idp_business_name" = "bceid_business_name",
+    "custom:idp_business_id"   = "bceid_business_guid",
+    "custom:keycloak_username" = "preferred_username"
+  }
+}
+
+resource "aws_cognito_identity_provider" "test_bceid_business_oidc_provider" {
+  user_pool_id  = aws_cognito_user_pool.fam_user_pool.id
+  provider_name = "TEST-BCEIDBUSINESS"
+  provider_type = "OIDC"
+
+  provider_details = {
+    authorize_scopes          = "openid profile email bceidbusiness"
+    client_id                 = var.oidc_bceid_business_idp_client_id
+    client_secret             = var.test_oidc_bceid_business_idp_client_secret
+    oidc_issuer               = var.test_oidc_idp_issuer
+    attributes_request_method = "GET"
+  }
+
+  attribute_mapping = {
+    email                      = "email",
+    "custom:idp_name"          = "identity_provider",
+    "custom:idp_user_id"       = "bceid_user_guid",
+    "custom:idp_username"      = "bceid_username",
+    "custom:idp_display_name"  = "display_name"
+    "custom:idp_business_name" = "bceid_business_name",
+    "custom:idp_business_id"   = "bceid_business_guid",
+    "custom:keycloak_username" = "preferred_username"
+  }
+}
+
+# TODO: Production is not provisioned by IDIM.
+# Update client_secret and oidc issuer when it is
+# Will need to put in the GitHub Secret at that time as well
+resource "aws_cognito_identity_provider" "prod_bceid_business_oidc_provider" {
+  user_pool_id  = aws_cognito_user_pool.fam_user_pool.id
+  provider_name = "PROD-BCEIDBUSINESS"
+  provider_type = "OIDC"
+
+  provider_details = {
+    authorize_scopes          = "openid profile email bceidbusiness"
+    client_id                 = var.oidc_bceid_business_idp_client_id
+    client_secret             = var.test_oidc_bceid_business_idp_client_secret
+    oidc_issuer               = var.test_oidc_idp_issuer
+    attributes_request_method = "GET"
+  }
+
+  attribute_mapping = {
+    email                      = "email",
+    "custom:idp_name"          = "identity_provider",
+    "custom:idp_user_id"       = "bceid_user_guid",
+    "custom:idp_username"      = "bceid_username",
+    "custom:idp_display_name"  = "display_name"
+    "custom:idp_business_name" = "bceid_business_name",
+    "custom:idp_business_id"   = "bceid_business_guid",
+    "custom:keycloak_username" = "preferred_username"
+  }
+}
+
+variable "all_read_list_bceid_business" {
+  description = "The list of all read attributes for BCEIDBUSINESS clients"
+  type        = list(string)
+  default     = ["email", "email_verified", "preferred_username", "profile", "custom:idp_display_name", "custom:idp_name", "custom:idp_user_id", "custom:idp_username", "custom:idp_business_name", "custom:idp_business_id", "custom:keycloak_username"]
+}
+
+variable "all_write_list_bceid_business" {
+  description = "The list of all write attributes for BCEIDBUSINESS clients"
+  type        = list(string)
+  default     = ["email", "preferred_username", "profile", "custom:idp_display_name", "custom:idp_name", "custom:idp_user_id", "custom:idp_username", "custom:idp_business_name", "custom:idp_business_id", "custom:keycloak_username"]
+}
--- infrastructure/server/outputs.tf
+++ infrastructure/server/outputs.tf
@@ -34,3 +34,8 @@ output "target_env" {
   description = "dev, test, or prod in AWS"
   value = var.target_env
 }
+
+output "fam_console_idp_name" {
+  description = "Identifies which version of IDIR to use (DEV, TEST, or PROD)"
+  value = var.fam_console_idp_name
+}
