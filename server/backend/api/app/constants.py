f2eab4152e3014c31c7d7adcd4f0bd409355dc35
Ian Liu <ianliuwk1019@gmail.com>
2023-01-14 10:27:50 -0800

    Fix test due to introduction environment_type for application.

--- server/backend/api/app/constants.py
+++ server/backend/api/app/constants.py
@@ -12,6 +12,12 @@ class RoleType(str, Enum):
     ROLE_TYPE_CONCRETE = "C"
 
 
+class AppEnvType(str, Enum):
+    APP_ENV_TYPE_DEV = "DEV"
+    APP_ENV_TYPE_TEST = "TEST"
+    APP_ENV_TYPE_PROD = "PROD"
+
+
 FAM_PROXY_API_USER = "fam_proxy_api"
 
 # TODO: Only uses this before forest-client api integration from front-end.
