e339034e576ac42aaefc4065a3b2939c67763a66
Basil Vandegriend <basil.vandegriend@cgi.com>
2023-02-07 12:17:19 -0700

    Address sonarcloud warning.

--- server/backend/api/app/database.py
+++ server/backend/api/app/database.py
@@ -10,7 +10,6 @@ from . import config
 LOGGER = logging.getLogger(__name__)
 
 # Log SQL queries from SQLalcheny
-logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
 SQLALCHEMY_DATABASE_URL = config.get_db_string()
