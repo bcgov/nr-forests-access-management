4be014ab85f14a897cbdbce9f8cfb0914c1c99c7
Ian Liu <ianliuwk1019@gmail.com>
2023-02-18 08:39:25 -0800

    Fixed whole bunch tests fail due to id_token added into authorization header.

--- server/backend/tests/authorization/test_jwt_validation.py
+++ server/backend/tests/authorization/test_jwt_validation.py
@@ -15,8 +15,8 @@ from api.app.jwt_validation import (ERROR_TOKEN_DECODE,
                                     ERROR_VALIDATION,
                                     JWT_CLIENT_ID_KEY)
 import json
-from jwt_utils import (create_jwt_token,
-                       create_jwt_claims,
+from jwt_utils import (create_jwt_access_token,
+                       create_jwt_access_claims,
                        assert_error_response,
                        headers)
 from Crypto.PublicKey import RSA
@@ -28,7 +28,8 @@ endPoint = f"{apiPrefix}/fam_applications"
 
 def test_get_application_success(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
     test_client_fixture_unit.app.dependency_overrides[database.get_db] = \
         lambda: UnifiedAlchemyMagicMock(data=[
@@ -37,9 +38,12 @@ def test_get_application_success(
             ),
         ])
 
-    token = create_jwt_token(test_rsa_key)
+    access_token = create_jwt_access_token(test_rsa_key)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert response.status_code == 204
     data = response.json()
@@ -58,81 +62,109 @@ def test_get_application_no_authentication_failure(
 def test_get_application_bad_token_failure(
         test_client_fixture_unit: starlette.testclient.TestClient):
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers("12345"))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers("123", "456")
+    )
 
     assert_error_response(response, 401, ERROR_TOKEN_DECODE)
 
 
 def test_get_application_bad_client_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
-    claims = create_jwt_claims()
+    claims = create_jwt_access_claims()
     claims[JWT_CLIENT_ID_KEY] = "incorrect"
-    token = create_jwt_token(test_rsa_key, claims)
+    access_token = create_jwt_access_token(test_rsa_key, claims)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_INVALID_CLIENT)
 
 
 def test_get_application_wrong_alg_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
-    claims = create_jwt_claims()
+    claims = create_jwt_access_claims()
     invalid_algorithm = 'HS256'
-    token = create_jwt_token(test_rsa_key, claims, invalid_algorithm)
+    access_token = create_jwt_access_token(test_rsa_key, claims, invalid_algorithm)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_INVALID_ALGORITHM)
 
 
 def test_get_application_missing_kid_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
-    token = create_jwt_token(test_rsa_key, test_headers={})
+    access_token = create_jwt_access_token(test_rsa_key, test_headers={})
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_MISSING_KID)
 
 
 def test_get_application_missing_rsa_key_failure(
+        test_id_token,
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key_missing):
+        test_rsa_key_missing
+):
 
-    token = create_jwt_token(test_rsa_key_missing)
+    access_token = create_jwt_access_token(test_rsa_key_missing)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_NO_RSA_KEY)
 
 
 def test_get_application_expired_token_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
-    claims = create_jwt_claims()
+    claims = create_jwt_access_claims()
     claims['exp'] = time.time() - 60000
-    token = create_jwt_token(test_rsa_key, claims)
+    access_token = create_jwt_access_token(test_rsa_key, claims)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_EXPIRED_TOKEN)
 
 
 def test_get_application_bad_issuer_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
-    claims = create_jwt_claims()
+    claims = create_jwt_access_claims()
     claims['iss'] = 'incorrect'
-    token = create_jwt_token(test_rsa_key, claims)
+    access_token = create_jwt_access_token(test_rsa_key, claims)
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_CLAIMS)
     assert json.loads(response.text)['detail']['description'] == 'Invalid issuer'
@@ -140,13 +172,17 @@ def test_get_application_bad_issuer_failure(
 
 def test_get_application_invalid_signature_failure(
         test_client_fixture_unit: starlette.testclient.TestClient,
-        test_rsa_key):
+        test_rsa_key,
+        test_id_token):
 
     wrong_key = RSA.generate(2048)
 
-    token = create_jwt_token(wrong_key.exportKey("PEM"))
+    access_token = create_jwt_access_token(wrong_key.exportKey("PEM"))
 
-    response = test_client_fixture_unit.get(f"{endPoint}", headers=headers(token))
+    response = test_client_fixture_unit.get(
+        f"{endPoint}",
+        headers=headers(test_id_token, access_token)
+    )
 
     assert_error_response(response, 401, ERROR_VALIDATION)
 
