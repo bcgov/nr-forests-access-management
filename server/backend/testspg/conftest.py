ab878a145f71aa52d3797e9271190d4abbcdf590
MCatherine <Mcatherine1994@gmail.com>
2023-03-06 15:32:43 -0800

    Test container for postgres testing (#465)
    * feat/backend:
    - add a simple postgres test
    - adjust tests folder for sqllite test so it will not complain about the postgres test
    - update the unit test in pipeline to check if the postgres test works
    refs: #403
    
    * feat/backend:
    - update to run container before all tests and stop container after all tests
    - update to open db connection before all and close db connection after all
    - add simple tests for crud and router
    refs: #403
    
    * cleanup/backend:
    - remove copied jwt_utils file
    refs: #403
    
    * fix/pipeline:
    - fix postgres unit test run directory in the pipeline, cause the postgres test not compliable with the sqllite test
    refs: #406
    
    * fix/backend:
    -remove the router test for now, as it is not compliable with the sqllite test
    refs: #406
    
    * feat/backend:
    - add the router test back
    refs: #403
    
    * feat/backend:
    - remove duplicate docker-compose file for testing, use the one in root directory
    - make the scope for test container to session, so it only be created once
    refs: #403
    
    * cleanup/backend:
    - format the test file
    refs: #403
    
    * fix/pipeline:
    - add env variables for backend test, otherwise it cannot find db when run postgres test with sqllite test
    refs: #403
    
    * fix/backend:
    - fix test file import
    refs: #403
    
    * fix/backend:
    - comment out the router test for now as it complains with the sqllite test
    refs: #403
    
    * try/pipeline:
    - try router testing
    refs: #403
    
    * fix/pipeline:
    - update unit test postgres db credential
    refs: #403
    
    * cleanup/pipeline:
    - remove unused comment
    refs: #403

---
+++ server/backend/testspg/conftest.py
@@ -0,0 +1,82 @@
+import pytest
+import time
+import logging
+import os
+import sys
+import testcontainers.compose
+from sqlalchemy.engine.base import Engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Crypto.PublicKey import RSA
+from fastapi.testclient import TestClient
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import api.app.jwt_validation as jwt_validation
+from api.app.main import app
+
+
+LOGGER = logging.getLogger(__name__)
+# the folder contains test docker-compose.yml, ours in the root directory
+COMPOSE_PATH = os.path.join(os.path.dirname(__file__), '../../../')
+
+
+@pytest.fixture(scope="session")
+def dbPgContainer():
+    compose = testcontainers.compose.DockerCompose(COMPOSE_PATH)
+    compose.start()
+    time.sleep(5)  # wait db migration script to run
+    yield compose
+    compose.stop()
+
+
+@pytest.fixture(scope="session")
+def dbPgEngine() -> Engine:
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/fam")
+    return engine
+
+
+@pytest.fixture(scope="module")
+def dbPgConnection(dbPgContainer, dbPgEngine: Engine):
+    _session_local = sessionmaker(bind=dbPgEngine)
+    db = _session_local()
+    yield db
+    db.close()
+
+
+@pytest.fixture(scope="function")
+def dbPgSession(dbPgConnection: sessionmaker) -> sessionmaker:
+    return dbPgConnection
+
+
+@pytest.fixture(scope="function")
+def test_client_fixture() -> TestClient:
+    """returns a requests object of the current app,
+    with the objects defined in the model created in it.
+
+    :rtype: starlette.testclient
+    """
+    client = TestClient(app)
+    return client
+
+
+@pytest.fixture(scope="function")
+def test_rsa_key():
+
+    new_key = RSA.generate(2048)
+    global public_rsa_key
+    public_rsa_key = new_key.publickey().exportKey("PEM")
+
+    app.dependency_overrides[jwt_validation.get_rsa_key_method] = \
+        override_get_rsa_key_method
+
+    return new_key.exportKey("PEM")
+
+
+def override_get_rsa_key_method():
+    return override_get_rsa_key
+
+
+def override_get_rsa_key(kid):
+    global public_rsa_key
+    return public_rsa_key
