286b160b87abe6579260b50f2816f86f55e5f2d2
Conrad Boyd Elliott Gustafson <75282706+ConradBoydElliottGustafson@users.noreply.github.com>
2023-06-21 09:58:49 -0700

    refactor(api-tests): #649 refactor tests api (#652)

--- server/backend/testspg/conftest.py
+++ server/backend/testspg/conftest.py
@@ -1,19 +1,15 @@
 import logging
 import os
 import sys
-import time
-
-import jwt_utils as jwt_utils
 import pytest
 import testcontainers.compose
 from Crypto.PublicKey import RSA
 from fastapi.testclient import TestClient
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from sqlalchemy import create_engine
-from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import Session, sessionmaker
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import api.app.database as database
 import api.app.jwt_validation as jwt_validation
@@ -21,63 +17,65 @@ from api.app.main import app
 
 LOGGER = logging.getLogger(__name__)
 # the folder contains test docker-compose.yml, ours in the root directory
-COMPOSE_PATH = os.path.join(os.path.dirname(__file__), '../../../')
+COMPOSE_PATH = os.path.join(os.path.dirname(__file__), "../../../")
 COMPOSE_FILE_NAME = "docker-compose-testcontainer.yml"
+
 
 @pytest.fixture(scope="session")
 def db_pg_container():
+    # LOGGER.debug("db_pg_container() commented out for local testing")
     compose = testcontainers.compose.DockerCompose(
-        COMPOSE_PATH,
-        compose_file_name=COMPOSE_FILE_NAME
+        COMPOSE_PATH, compose_file_name=COMPOSE_FILE_NAME
     )
     compose.start()
-    time.sleep(6)  # wait db migration script to run
+    # NGINX is set to start only when flyway is complete
+    compose.wait_for("http://localhost:8181")
     yield compose
     compose.stop()
 
 
 @pytest.fixture(scope="session")
-def db_pg_engine() -> Engine:
-    engine = create_engine("postgresql+psycopg2://" +
-                           f"{os.environ.get('POSTGRES_USER')}:" +
-                           f"{os.environ.get('POSTGRES_PASSWORD')}@" +
-                           f"{os.environ.get('POSTGRES_HOST')}:"
-                           f"{os.environ.get('POSTGRES_PORT')}/"
-                           f"{os.environ.get('POSTGRES_DB')}")
-    return engine
+def db_pg_connection(db_pg_container):
 
+    engine = create_engine(
+        "postgresql+psycopg2://"
+        + f"{os.environ.get('POSTGRES_USER')}:"
+        + f"{os.environ.get('POSTGRES_PASSWORD')}@"
+        + f"{os.environ.get('POSTGRES_HOST')}:"
+        f"{os.environ.get('POSTGRES_PORT')}/"
+        f"{os.environ.get('POSTGRES_DB')}"
+    )
 
-@pytest.fixture(scope="module")
-def db_pg_connection(db_pg_container, db_pg_engine: Engine):
-    _session_local = sessionmaker(bind=db_pg_engine)
-    db = _session_local()
-    yield db
-    db.close()
+    session_local = sessionmaker(bind=engine)
+    test_db = session_local()
+
+    yield test_db
+    test_db.close()
 
 
 @pytest.fixture(scope="function")
-def db_pg_session(db_pg_connection: Session) -> Session:
-    return db_pg_connection
+def db_pg_session(db_pg_connection: Session):
+    yield db_pg_connection
+    db_pg_connection.rollback()
 
 
 @pytest.fixture(scope="function")
 def test_client_fixture_unit() -> TestClient:
 
-    app.dependency_overrides[database.get_db] = \
-        lambda: UnifiedAlchemyMagicMock(data=[])
+    app.dependency_overrides[database.get_db] = lambda: UnifiedAlchemyMagicMock(data=[])
 
     return TestClient(app)
 
 
 @pytest.fixture(scope="function")
-def test_client_fixture() -> TestClient:
+def test_client_fixture(db_pg_session) -> TestClient:
     """returns a requests object of the current app,
     with the objects defined in the model created in it.
 
     :rtype: starlette.testclient
     """
     # reset to default database which points to postgres container
-    app.dependency_overrides[database.get_db] = database.get_db
+    app.dependency_overrides[database.get_db] = lambda: db_pg_session
     return TestClient(app)
 
 
@@ -88,8 +86,9 @@ def test_rsa_key():
     global public_rsa_key
     public_rsa_key = new_key.publickey().exportKey("PEM")
 
-    app.dependency_overrides[jwt_validation.get_rsa_key_method] = \
-        override_get_rsa_key_method
+    app.dependency_overrides[
+        jwt_validation.get_rsa_key_method
+    ] = override_get_rsa_key_method
 
     return new_key.exportKey("PEM")
 
@@ -101,8 +100,9 @@ def test_rsa_key_missing():
     global public_rsa_key
     public_rsa_key = new_key.publickey().exportKey("PEM")
 
-    app.dependency_overrides[jwt_validation.get_rsa_key_method] = \
-        override_get_rsa_key_method_none
+    app.dependency_overrides[
+        jwt_validation.get_rsa_key_method
+    ] = override_get_rsa_key_method_none
 
     return new_key.exportKey("PEM")
 
@@ -122,3 +122,4 @@ def override_get_rsa_key_method_none():
 
 def override_get_rsa_key_none(kid):
     return None
+
