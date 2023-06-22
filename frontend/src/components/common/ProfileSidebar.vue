0a2dd057e10c7bee47939fa54c0f197f86a7c8b5
J0taFerreira <126905995+J0taFerreira@users.noreply.github.com>
2023-06-22 15:21:50 -0300

    feat: 648 - creating profile component (#658)

---
+++ frontend/src/components/common/ProfileSidebar.vue
@@ -0,0 +1,170 @@
+<script setup lang="ts">
+import authService from '@/services/AuthService'
+import { useProfileSidebarVisible } from '../../store/useProfileVisibleStore'
+import userProfileInfo from '../../static/userProfileInfo.json'
+
+// svg icon
+import CloseIcon from '../icons/CloseIcon.vue'
+import LogoutIcon from '../icons/LogoutIcon.vue'
+
+</script>
+
+<template>
+        <div 
+            :class="useProfileSidebarVisible.isProfileVisible ? 'fade-in' : 'fade-out'" 
+            @click="useProfileSidebarVisible.toggleVisible()"
+        ></div>
+    <Transition name="slide">
+
+        <div class="profile-container" v-if="useProfileSidebarVisible.isProfileVisible">
+            <div class="profile-header">
+                <h2>Profile</h2>
+                <button 
+                    class="btn-icon"
+                    @click="useProfileSidebarVisible.toggleVisible()" 
+                    aria-label="Close"
+                >
+                    <CloseIcon />
+                </button>
+            </div>
+            <div class="sidebar-body">
+                <div class="img-wrapper">
+                    <img src="../../assets/images/tyrannosaurus-rex1.png" alt="User avatar" /> 
+                </div>
+                <div class="profile-info">
+                    <p class="profile-name">{{ userProfileInfo[0].userInfo.name }}</p>
+                    <p class="profile-idir">IDIR: {{ userProfileInfo[0].idir }}</p>
+                    <p class="profile-email">{{ userProfileInfo[0].userInfo.email }}</p>
+                </div>
+            </div>
+            <hr class="profile-divider">
+            <p class="options">Options</p>
+            <button 
+                class="sign-out"
+                title="Sign out"
+                aria-expanded="false"
+                aria-label="sign out"
+                @click="authService.methods.logout"
+                >
+                <i><LogoutIcon/></i>
+                Sign out
+            </button>
+
+        </div>    
+    </Transition>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/styles.scss';
+.profile-container {
+    background-color: #FFF;
+    border-left: 1px solid #dfdfe1;
+    color: #000;
+    height: calc(100vh - 48px);
+    inset: 0 0 0 70%;
+    margin: 48px 0 0;
+    padding: 0 16px 0;
+    position: fixed;
+    overflow: hidden;
+    z-index: 9999;
+}
+
+.profile-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 16px 0 ;
+
+    h2 {
+        margin: 0;
+        font-weight: 400;
+    }
+
+    .btn-icon {
+        border: none;
+        background-color: transparent;
+    }
+}
+
+.sidebar-body {
+    display: flex;
+    margin: 30px 0 0;
+
+    .img-wrapper {
+        margin-right: 32px;
+    }
+
+    .profile-info {
+        margin: 6px 0 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .profile-name, .profile-idir {
+        margin-bottom: 6px;
+    }
+}
+
+.profile-name, .sign-out {
+        font-size: 14px;
+        font-weight: 700;
+}
+
+.sign-out {
+    border: none;
+    background-color: transparent;
+    color: #000;
+}
+
+.sign-out:hover {
+    background-color: transparent;
+    background-color: #FFFFFF;
+}
+
+.profile-idir, .profile-email, .options {
+        font-size: 12px;
+        font-weight: 400;
+}
+
+.profile-divider {
+    background: #FFFFFF;
+    background-blend-mode: multiply;
+    margin: 16px 0;
+}
+
+.sign-out {
+    cursor: pointer;
+
+    i {
+        margin-right: 18px;
+    }
+}
+
+/// profile sidebar slide animation
+.slide-enter-active, .slide-leave-active {
+        transform: translateX(0%);
+        transition: ease-out 350ms;
+    }
+
+.slide-enter-from, .slide-leave-to {
+    transform: translateX(100%);
+    transition: ease-out 350ms;
+}
+
+/// background fade transition
+.fade-in, .fade-out {
+    background-color: rgba($color: #131315, $alpha: .5);
+    transition: ease-in 350ms;
+    position: fixed;
+    width: 100%;
+    inset: 48px 0 0 0;
+    z-index: 999;
+}
+
+.fade-out {
+    visibility: hidden;
+    background-color: rgba($color: #131315, $alpha: 0);
+}
+
+
+</style>
