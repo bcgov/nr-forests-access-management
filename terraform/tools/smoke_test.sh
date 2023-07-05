8cee4c1ef44a0ef8a4f079a92620988342ac526a
ConradBoydElliottGustafson <c.gustafson@cgi.com>
2023-07-04 17:27:19 -0700

    debug5

--- terraform/tools/smoke_test.sh
+++ terraform/tools/smoke_test.sh
@@ -3,6 +3,6 @@ terragrunt run-all output > output.txt
 fam_api_base_url1=$(awk -F= '/fam_api_base_url/ { gsub(" ","",$2);print $2 }' output.txt)
 fam_api_base_url2=$(sed 's/^"//' <<< $fam_api_base_url1)
 fam_api_base_url3=$(sed 's/"$//' <<< $fam_api_base_url2)
-smoke_test_url="$fam_api_base_url3/smoke_test"
+smoke_test_url=$(echo "$fam_api_base_url3/smoke_test")
 echo "Smoke test URL: [$smoke_test_url]"
 curl --silent --show-error --fail $smoke_test_url
