75848c48930c6c13febd3d0cc03bf65753cb20d8
Ian Liu <ianliuwk1019@gmail.com>
2023-07-25 16:51:29 -0700

    Add security check for idir_search endpoint and extract requester for endpoint.

--- server/backend/api/app/routers/router_idim_proxy.py
+++ server/backend/api/app/routers/router_idim_proxy.py
@@ -1,19 +1,24 @@
 import logging
-from typing import List
+from typing import Annotated, List
 
 from api.app.integration.idim_proxy import IdimProxyService
-from fastapi import APIRouter, Query, Request
+from api.app.requester import (Requester, get_current_requester,
+                               internal_only_action)
+from fastapi import APIRouter, Query
+from fastapi.params import Depends
+
+ERROR_EXTERNAL_USER_ACTION_PROHIBITED = "external_user_action_prohibited"
 
 LOGGER = logging.getLogger(__name__)
 
 router = APIRouter()
 
-@router.get("/idir", response_model=List[dict])
+@router.get("/idir", response_model=List[dict], dependencies = [Depends(internal_only_action)],)
 def idir_search(
-    request: Request,
+    requester: Annotated[Requester, Depends(get_current_requester)],
     user_id: str = Query(min_length=3)
 ):
-    LOGGER.debug(f"Searching IDIR user with parameter user_id: {request.query_params}")
-    idim_proxy_api = IdimProxyService(request)
-    search_result = idim_proxy_api.search_idir()
+    LOGGER.debug(f"Searching IDIR user with parameter user_id: {user_id}")
+    idim_proxy_api = IdimProxyService(requester)
+    search_result = idim_proxy_api.search_idir({user_id: user_id})
     return search_result
