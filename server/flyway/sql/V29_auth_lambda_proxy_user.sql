9dfe14e5f6251b959ab06e4c424b917202d4d65e
Basil Vandegriend <basil.vandegriend@cgi.com>
2023-08-02 12:46:32 -0600

    Specify only required permissions for auth lambda user.

--- server/flyway/sql/V29_auth_lambda_proxy_user.sql
+++ server/flyway/sql/V29_auth_lambda_proxy_user.sql
@@ -1,6 +1,16 @@
 
 CREATE USER ${auth_lambda_db_user} WITH NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD '${auth_lambda_db_password}';
 
+-- Needed to access anything in the schema
 GRANT USAGE ON SCHEMA app_fam TO ${auth_lambda_db_user};
 
-GRANT SELECT, UPDATE, DELETE, INSERT ON ALL TABLES IN SCHEMA app_fam TO ${auth_lambda_db_user};
+-- Update and insert on app_fam needed for creating/updating the user record
+GRANT UPDATE, INSERT ON app_fam.fam_users TO ${auth_lambda_db_user};
+
+-- Select on these tables needed to determine for the given application (cognito client) which roles the user can access
+GRANT SELECT ON app_fam.fam_user TO ${auth_lambda_db_user};
+GRANT SELECT ON app_fam.fam_role TO ${auth_lambda_db_user};
+GRANT SELECT ON app_fam.fam_application TO ${auth_lambda_db_user};
+GRANT SELECT ON app_fam.fam_application_client TO ${auth_lambda_db_user};
+GRANT SELECT ON app_fam.fam_user_role_xref TO ${auth_lambda_db_user};
+
