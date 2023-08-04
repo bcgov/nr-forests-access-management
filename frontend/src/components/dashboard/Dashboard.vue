171767d53d5d852d9cebaee0e2bbf0d1f2cc3891
Nicola Saglioni <nicola.saglioni@mobeus.com>
2023-08-04 16:40:00 -0300

    Fixing error handling on Dashboard page

--- frontend/src/components/dashboard/Dashboard.vue
+++ frontend/src/components/dashboard/Dashboard.vue
@@ -37,19 +37,23 @@ const userRoleAssignments = ref<FamApplicationUserRoleAssignmentGet[]>();
 
 onMounted(async () => {
     // Reload list each time we navigate to this page to avoid forcing user to refresh if their access changes.
-    try {
-        loading.value = true;
-        applicationsUserAdministers.value = (
-            await applicationsApi.getApplications()
-        ).data;
-        if (isApplicationSelected) {
-            getAppUserRoleAssignment();
-        }
-    } catch (error: any) {
-        return Promise.reject(error);
-    } finally {
-        loading.value = false;
-    }
+    loading.value = true;
+    await applicationsApi
+        .getApplications()
+        .then(async (response) => {
+            applicationsUserAdministers.value = response.data;
+            if (isApplicationSelected) {
+                await getAppUserRoleAssignment();
+            }
+        })
+        .catch((error) => {
+            {
+                return Promise.reject(error);
+            }
+        })
+        .finally(() => {
+            loading.value = false;
+        });
 });
 
 async function getAppUserRoleAssignment() {
@@ -81,10 +85,14 @@ async function getAppUserRoleAssignment() {
     }
 }
 
-const selectApplication = (e: DropdownChangeEvent) => {
-    setSelectedApplication(e.value ? JSON.stringify(e.value) : null);
-    if (isApplicationSelected) {
-        getAppUserRoleAssignment();
+const selectApplication = async (e: DropdownChangeEvent) => {
+    try {
+        setSelectedApplication(e.value ? JSON.stringify(e.value) : null);
+        if (applicationsUserAdministers) {
+            await getAppUserRoleAssignment();
+        }
+    } catch (error: any) {
+        return Promise.reject(error);
     }
 };
 
