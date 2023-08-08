cd27d893d41c2b9026d393298843099867b3dbd8
Nicola Saglioni <nicola.saglioni@mobeus.com>
2023-08-08 13:33:53 -0300

    Handling the grant access option in the parent component

--- frontend/src/layouts/ProtectedLayout.vue
+++ frontend/src/layouts/ProtectedLayout.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 import Header from '@/components/header/Header.vue';
 import sideNavData from '@/static/sideNav.json';
-import SideNav from '@/components/common/SideNav.vue';
+import SideNav, { type ISideNavData } from '@/components/common/SideNav.vue';
+import { ref, watch } from 'vue';
+import { isApplicationSelected } from '@/store/ApplicationState';
+
+const navigationData = ref<[ISideNavData]>(sideNavData as any);
+
+watch(isApplicationSelected, (value) => {
+    disableSideNavOption('Grant Access', !value);
+});
+
+function disableSideNavOption(optionName: string, disabled: boolean) {
+    navigationData.value.map((item) => {
+        item.items.map((child) => {
+            if (child.name === optionName) {
+                child.disabled = disabled;
+            }
+        });
+    });
+}
 </script>
 <template>
     <Header title="FAM" subtitle="Forest Access Management" />
 
-    <SideNav :data="(sideNavData as any)" />
+    <SideNav :data="navigationData" />
     <div class="main">
         <main>
             <RouterView></RouterView>
