6f1463c7c2d9a4aa781ac825c9b8f72b1e0d0111
catherine meng <mcatherine1994@gmail.com>
2023-08-15 14:19:16 -0700

    switch temp update back

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -28,6 +28,7 @@ resource "aws_s3_bucket_policy" "web_distribution" {
 }
 
 resource "aws_cloudfront_distribution" "web_distribution" {
+  aliases             = ["${var.cloudfront_vanity_domain}"]
   enabled             = true
   is_ipv6_enabled     = true
   wait_for_deployment = false
@@ -35,7 +36,7 @@ resource "aws_cloudfront_distribution" "web_distribution" {
   price_class         = "PriceClass_100"
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = "${var.cloudfront_certificate_arn}"
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
