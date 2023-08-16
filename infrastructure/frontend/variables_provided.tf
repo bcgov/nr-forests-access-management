1046e0bf10eab1d4f19485bec811cc2f8c780a79
Ian Liu <ianliuwk1019@gmail.com>
2023-08-16 15:17:06 -0700

    Add fam_waf_acl_cloudfront_arn variable to see if it can take value from server output.

--- infrastructure/frontend/variables_provided.tf
+++ infrastructure/frontend/variables_provided.tf
@@ -8,3 +8,8 @@ variable "cloudfront_certificate_arn" {
   description = "ARN for certificate to use in cloudfront"
   type = string
 }
+
+variable "fam_waf_acl_cloudfront_arn" {
+  description = "The ARN of the WAF ACL Cloudfront"
+  type = string
+}
