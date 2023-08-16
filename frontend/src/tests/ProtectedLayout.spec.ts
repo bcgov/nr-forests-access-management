a6797475b4cb77ce76364dcb6404c93a0aa2070d
NickSaglioni <117364634+NickSaglioni@users.noreply.github.com>
2023-08-16 17:45:18 -0300

    fix: #800 Frontend test breaks the pipeline build (#801)
    Co-authored-by: Nicola Saglioni <nicola.saglioni@mobeus.com>

--- frontend/src/tests/ProtectedLayout.spec.ts
+++ frontend/src/tests/ProtectedLayout.spec.ts
@@ -1,10 +1,8 @@
 import ProtectedLayout from '@/layouts/ProtectedLayout.vue';
-import { flushPromises, mount, shallowMount } from '@vue/test-utils';
+import { flushPromises, shallowMount } from '@vue/test-utils';
 import type { VueWrapper } from '@vue/test-utils/dist/vueWrapper';
 import { it, describe, expect, beforeEach } from 'vitest';
-import { routes } from '@/router';
-import SideNav from '@/components/common/SideNav.vue';
-// import { createRouter, createWebHistory, type Router } from 'vue-router';
+
 import sideNavData from '@/static/sideNav.json';
 describe('disableSideNavOption', () => {
     let wrapper: VueWrapper;
@@ -14,7 +12,7 @@ describe('disableSideNavOption', () => {
         await flushPromises();
     });
     it('should change value for navigation option Grant Access', async () => {
-        wrapper.vm.disableSideNavOption('Grant Access', false);
+        (wrapper.vm as any).disableSideNavOption('Grant Access', false);
         let value = sideNavData.map(item => item.items.find(child => child.name === 'Grant Access')?.disabled)[0];
         expect(value).toBe(false)
     });
