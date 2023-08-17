c4bfafdf4e16cb9a048be82a81a652c9efd5dc37
Ian Liu <ianliuwk1019@gmail.com>
2023-08-17 13:59:11 -0700

    Try moving Cloudfront ACL into frontend.

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -28,10 +28,10 @@ resource "aws_s3_bucket_policy" "web_distribution" {
   policy = data.aws_iam_policy_document.web_distribution.json
 }
 
-data "aws_wafv2_web_acl" "fam_waf_cloudfront" {
-  name   = "${local.fam_waf_cloudfront_resource_name}"
-  scope  = "CLOUDFRONT"
-}
+# data "aws_wafv2_web_acl" "fam_waf_cloudfront" {
+#   name   = "${local.fam_waf_cloudfront_resource_name}"
+#   scope  = "CLOUDFRONT"
+# }
 
 # # TODO: remove this if not working.
 # # Try to find a way to reference from "frontend" terraform module to "server" terraform module (but different folders and different apply)
@@ -41,6 +41,132 @@ data "aws_wafv2_web_acl" "fam_waf_cloudfront" {
 #   fam_waf_acl_cloudfront_arn = module.fam_waf_acl_cloudfront_arn
 # }
 
+
+# # CloudFront WAF ACL
+resource "aws_wafv2_web_acl" "fam_waf_cloudfront" {
+    name        = "${local.fam_waf_cloudfront_resource_name}"
+    description = "CloudFront WAF Rules"
+    scope       = "CLOUDFRONT"
+    provider    = aws.east # Cloudfront ACL has to be created in this region.
+
+    default_action {
+        allow {}
+    }
+
+
+    ## AWS Managed rules below.
+    rule {
+        name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+        priority = 0
+
+        override_action {
+            none {}
+        }
+        statement {
+            managed_rule_group_statement {
+                vendor_name = "AWS"
+                name        = "AWSManagedRulesAmazonIpReputationList"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesAmazonIpReputationList"
+            sampled_requests_enabled   = true
+        }
+    }
+
+    rule {
+        name     = "AWS-AWSManagedRulesCommonRuleSet"
+        priority = 1
+
+        override_action {
+            none {}
+        }
+        statement {
+            managed_rule_group_statement {
+                vendor_name = "AWS"
+                name        = "AWSManagedRulesCommonRuleSet"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesCommonRuleSet"
+            sampled_requests_enabled   = true
+        }
+    }
+
+    rule {
+        name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+        priority = 2
+
+        override_action {
+            none {}
+        }
+        statement {
+            managed_rule_group_statement {
+                vendor_name = "AWS"
+                name        = "AWSManagedRulesKnownBadInputsRuleSet"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesKnownBadInputsRuleSet"
+            sampled_requests_enabled   = true
+        }
+    }
+
+    rule {
+        name     = "AWS-AWSManagedRulesLinuxRuleSet"
+        priority = 3
+
+        override_action {
+            none {}
+        }
+        statement {
+            managed_rule_group_statement {
+                vendor_name = "AWS"
+                name        = "AWSManagedRulesLinuxRuleSet"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesLinuxRuleSet"
+            sampled_requests_enabled   = true
+        }
+    }
+
+    rule {
+        name     = "AWS-AWSManagedRulesSQLiRuleSet"
+        priority = 4
+
+        override_action {
+            none {}
+        }
+        statement {
+            managed_rule_group_statement {
+                vendor_name = "AWS"
+                name        = "AWSManagedRulesSQLiRuleSet"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesSQLiRuleSet"
+            sampled_requests_enabled   = true
+        }
+    }
+
+    tags = {
+        env = "${var.target_env}"
+        managed-by = "terraform"
+    }
+
+    visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "${local.fam_waf_cloudfront_resource_name}"
+        sampled_requests_enabled   = true
+    }
+}
+
 resource "aws_cloudfront_distribution" "web_distribution" {
   aliases             = ["${var.cloudfront_vanity_domain}"]
   enabled             = true
@@ -49,7 +175,8 @@ resource "aws_cloudfront_distribution" "web_distribution" {
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
   # web_acl_id          = "${module.server_info.fam_waf_acl_cloudfront_arn}"
-  web_acl_id          = "${data.aws_wafv2_web_acl.fam_waf_cloudfront.arn}"
+  # web_acl_id          = "${data.aws_wafv2_web_acl.fam_waf_cloudfront.arn}"
+  web_acl_id          = "${aws_wafv2_web_acl.fam_waf_cloudfront.arn}"
 
   viewer_certificate {
     acm_certificate_arn = "${var.cloudfront_certificate_arn}"
--- infrastructure/server/waf-firewall.tf
+++ infrastructure/server/waf-firewall.tf
@@ -226,136 +226,136 @@ resource "aws_wafv2_web_acl" "fam_waf_cognito" {
 }
 
 
-# Additional provider configuration for us-east-1 region; resources can
-# reference this as `aws.east`.
-provider "aws" {
-  alias  = "east"
-  region = "us-east-1"
-}
-# CloudFront WAF ACL
-locals {
-    fam_waf_cloudfront_resource_name = "${local.web_acl_name_prefix}-cloudfront"
-}
-resource "aws_wafv2_web_acl" "fam_waf_cloudfront" {
-    name        = "${local.fam_waf_cloudfront_resource_name}"
-    description = "CloudFront WAF Rules"
-    scope       = "CLOUDFRONT"
-    provider    = aws.east # Cloudfront ACL has to be created in this region.
+# # Additional provider configuration for us-east-1 region; resources can
+# # reference this as `aws.east`.
+# provider "aws" {
+#   alias  = "east"
+#   region = "us-east-1"
+# }
+# # CloudFront WAF ACL
+# locals {
+#     fam_waf_cloudfront_resource_name = "${local.web_acl_name_prefix}-cloudfront"
+# }
+# resource "aws_wafv2_web_acl" "fam_waf_cloudfront" {
+#     name        = "${local.fam_waf_cloudfront_resource_name}"
+#     description = "CloudFront WAF Rules"
+#     scope       = "CLOUDFRONT"
+#     provider    = aws.east # Cloudfront ACL has to be created in this region.
 
-    default_action {
-        allow {}
-    }
+#     default_action {
+#         allow {}
+#     }
 
 
-    ## AWS Managed rules below.
-    rule {
-        name     = "AWS-AWSManagedRulesAmazonIpReputationList"
-        priority = 0
+#     ## AWS Managed rules below.
+#     rule {
+#         name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+#         priority = 0
 
-        override_action {
-            none {}
-        }
-        statement {
-            managed_rule_group_statement {
-                vendor_name = "AWS"
-                name        = "AWSManagedRulesAmazonIpReputationList"
-            }
-        }
-        visibility_config {
-            cloudwatch_metrics_enabled = true
-            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesAmazonIpReputationList"
-            sampled_requests_enabled   = true
-        }
-    }
+#         override_action {
+#             none {}
+#         }
+#         statement {
+#             managed_rule_group_statement {
+#                 vendor_name = "AWS"
+#                 name        = "AWSManagedRulesAmazonIpReputationList"
+#             }
+#         }
+#         visibility_config {
+#             cloudwatch_metrics_enabled = true
+#             metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesAmazonIpReputationList"
+#             sampled_requests_enabled   = true
+#         }
+#     }
 
-    rule {
-        name     = "AWS-AWSManagedRulesCommonRuleSet"
-        priority = 1
+#     rule {
+#         name     = "AWS-AWSManagedRulesCommonRuleSet"
+#         priority = 1
 
-        override_action {
-            none {}
-        }
-        statement {
-            managed_rule_group_statement {
-                vendor_name = "AWS"
-                name        = "AWSManagedRulesCommonRuleSet"
-            }
-        }
-        visibility_config {
-            cloudwatch_metrics_enabled = true
-            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesCommonRuleSet"
-            sampled_requests_enabled   = true
-        }
-    }
+#         override_action {
+#             none {}
+#         }
+#         statement {
+#             managed_rule_group_statement {
+#                 vendor_name = "AWS"
+#                 name        = "AWSManagedRulesCommonRuleSet"
+#             }
+#         }
+#         visibility_config {
+#             cloudwatch_metrics_enabled = true
+#             metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesCommonRuleSet"
+#             sampled_requests_enabled   = true
+#         }
+#     }
 
-    rule {
-        name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
-        priority = 2
+#     rule {
+#         name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+#         priority = 2
 
-        override_action {
-            none {}
-        }
-        statement {
-            managed_rule_group_statement {
-                vendor_name = "AWS"
-                name        = "AWSManagedRulesKnownBadInputsRuleSet"
-            }
-        }
-        visibility_config {
-            cloudwatch_metrics_enabled = true
-            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesKnownBadInputsRuleSet"
-            sampled_requests_enabled   = true
-        }
-    }
+#         override_action {
+#             none {}
+#         }
+#         statement {
+#             managed_rule_group_statement {
+#                 vendor_name = "AWS"
+#                 name        = "AWSManagedRulesKnownBadInputsRuleSet"
+#             }
+#         }
+#         visibility_config {
+#             cloudwatch_metrics_enabled = true
+#             metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesKnownBadInputsRuleSet"
+#             sampled_requests_enabled   = true
+#         }
+#     }
 
-    rule {
-        name     = "AWS-AWSManagedRulesLinuxRuleSet"
-        priority = 3
+#     rule {
+#         name     = "AWS-AWSManagedRulesLinuxRuleSet"
+#         priority = 3
 
-        override_action {
-            none {}
-        }
-        statement {
-            managed_rule_group_statement {
-                vendor_name = "AWS"
-                name        = "AWSManagedRulesLinuxRuleSet"
-            }
-        }
-        visibility_config {
-            cloudwatch_metrics_enabled = true
-            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesLinuxRuleSet"
-            sampled_requests_enabled   = true
-        }
-    }
+#         override_action {
+#             none {}
+#         }
+#         statement {
+#             managed_rule_group_statement {
+#                 vendor_name = "AWS"
+#                 name        = "AWSManagedRulesLinuxRuleSet"
+#             }
+#         }
+#         visibility_config {
+#             cloudwatch_metrics_enabled = true
+#             metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesLinuxRuleSet"
+#             sampled_requests_enabled   = true
+#         }
+#     }
 
-    rule {
-        name     = "AWS-AWSManagedRulesSQLiRuleSet"
-        priority = 4
+#     rule {
+#         name     = "AWS-AWSManagedRulesSQLiRuleSet"
+#         priority = 4
 
-        override_action {
-            none {}
-        }
-        statement {
-            managed_rule_group_statement {
-                vendor_name = "AWS"
-                name        = "AWSManagedRulesSQLiRuleSet"
-            }
-        }
-        visibility_config {
-            cloudwatch_metrics_enabled = true
-            metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesSQLiRuleSet"
-            sampled_requests_enabled   = true
-        }
-    }
+#         override_action {
+#             none {}
+#         }
+#         statement {
+#             managed_rule_group_statement {
+#                 vendor_name = "AWS"
+#                 name        = "AWSManagedRulesSQLiRuleSet"
+#             }
+#         }
+#         visibility_config {
+#             cloudwatch_metrics_enabled = true
+#             metric_name                = "${local.fam_waf_cloudfront_resource_name}-AWSManagedRulesSQLiRuleSet"
+#             sampled_requests_enabled   = true
+#         }
+#     }
 
-    tags = {
-        env = "${var.target_env}"
-        managed-by = "terraform"
-    }
+#     tags = {
+#         env = "${var.target_env}"
+#         managed-by = "terraform"
+#     }
 
-    visibility_config {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "${local.fam_waf_cloudfront_resource_name}"
-        sampled_requests_enabled   = true
-    }
-}
+#     visibility_config {
+#         cloudwatch_metrics_enabled = true
+#         metric_name                = "${local.fam_waf_cloudfront_resource_name}"
+#         sampled_requests_enabled   = true
+#     }
+# }
