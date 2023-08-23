241d1246358746b4f8d8a71ba20ba6d1cea50d38
MCatherine <Mcatherine1994@gmail.com>
2023-08-23 12:31:47 -0700

    fix: #814 update dynamic icon import to fix pipeline slow issue (#816)

---
+++ frontend/src/alltypes.d.ts
@@ -0,0 +1,17 @@
+// declare missing types for carbon icons
+
+declare module '@carbon/icons-vue/es/close/16';
+declare module '@carbon/icons-vue/es/user--follow/16';
+declare module '@carbon/icons-vue/es/dashboard/16';
+declare module '@carbon/icons-vue/es/virtual-column--key/16';
+declare module '@carbon/icons-vue/es/settings/16';
+declare module '@carbon/icons-vue/es/help/16';
+declare module '@carbon/icons-vue/es/search/16';
+
+declare module '@carbon/icons-vue/es/login/20';
+declare module '@carbon/icons-vue/es/checkmark--filled/20';
+declare module '@carbon/icons-vue/es/add/20';
+declare module '@carbon/icons-vue/es/user--avatar--filled/20';
+declare module '@carbon/icons-vue/es/user--avatar/20';
+
+declare module '@carbon/icons-vue/es/error--filled/24';
