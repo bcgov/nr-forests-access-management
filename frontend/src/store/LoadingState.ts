86974891c968427664bd30f100c555bda1a854a1
Ian Liu <ianliuwk1019@gmail.com>
2023-09-10 13:17:13 -0700

    Add comments.

--- frontend/src/store/LoadingState.ts
+++ frontend/src/store/LoadingState.ts
@@ -1,9 +1,14 @@
 import { ref } from 'vue';
 
-// Loading state indicator (true/false).
-// It is being triggered when async http request happens (used by axios interceptors).
-// Components can read this indicator but DON'T need to set the value as it is being
-// taking care by axios interceptors.
+/*
+    Loading state indicator (true/false). For this Vue app axios instance.
+    It is being triggered when async http request happens (used by app's axios interceptors).
+    Components can read this indicator but *-DON'T-* need to set the value since it is being
+    taken care by axios interceptors.
+
+    If http is being triggered by third-party libraries, loading will need to be manually 
+    set (not part of app's axios instance).
+*/
 const isLoading = ref<boolean>(false);
 
 export default {
