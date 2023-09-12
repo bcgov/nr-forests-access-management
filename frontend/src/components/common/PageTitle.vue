2bca2db346a716f631e27646708350ef0bd78af4
Joao Ferreira <joao.ferreira@encora.com>
2023-09-12 13:52:04 -0300

    change manage acces title to new design

--- frontend/src/components/common/PageTitle.vue
+++ frontend/src/components/common/PageTitle.vue
@@ -22,6 +22,7 @@ const props = defineProps({
     font-size: 2rem;
     line-height: 2.5rem;
     color: $light-text-primary;
+    font-weight: 400;
 }
 
 .subtitle {
--- frontend/src/components/managePermissions/ManagePermissionsTitle.vue
+++ frontend/src/components/managePermissions/ManagePermissionsTitle.vue
@@ -1,39 +1,16 @@
-<script setup lang="ts">
-import router from '@/router';
-import { IconSize } from '@/enum/IconEnum';
-
-const props = defineProps({
-    isApplicationSelected: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div class="row">
         <div class="col-6">
-            <PageTitle title="Dashboard" subtitle="Manage permissions" />
-        </div>
-        <div class="col-6">
-            <Button
-                v-if="props.isApplicationSelected"
-                class="dashboard-button"
-                label="Grant new access"
-                @click="router.push('/grant')"
-            >
-                <Icon icon="add" :size="IconSize.medium" />
-            </Button>
+            <PageTitle
+                title="Manage permissions"
+                subtitle="Add permissions or edit users for the selected application"
+            />
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/base.scss';
-
-.dashboard-button {
-    width: 14.6875rem;
-    float: right;
-}
 </style>
