0ca777f6572621c7839a22a412279445dd7d0361
Ian Liu <ianliuwk1019@gmail.com>
2023-09-19 16:07:43 -0700

    Experiment on adding aws_iam_role and instance_profile.

--- infrastructure/server/ec2_instances.tf
+++ infrastructure/server/ec2_instances.tf
@@ -20,6 +20,8 @@ resource "aws_instance" "fam_bastion_host" {
 
   depends_on = [aws_security_group.fam_app_sg]
 
+  iam_instance_profile = "${aws_iam_role.fam_ec2_bastion_host_profile.id}"
+
   tags = {
       Name = "fam_bastion_host"
       managed-by = "terraform"
@@ -32,12 +34,37 @@ resource "aws_instance" "fam_bastion_host" {
   echo "Postgres installation done" >> init.log
 
   EOF
-
-
 }
 
 resource "aws_ec2_instance_state" "fam_bastion_host_state" {
   instance_id = aws_instance.fam_bastion_host.id
   # fam_bastion_host instance created in dormant state.
   state       = "stopped"
+}
+
+# TODO: experimeting on permission below.
+resource "aws_iam_instance_profile" "fam_ec2_bastion_host_profile" {
+  name = "${aws_instance.fam_bastion_host}_instance_profile"
+  role = "${aws_iam_role.fam_ec2_bastion_host_role.name}"
+}
+
+resource "aws_iam_role" "fam_ec2_bastion_host_role" {
+  name = "${aws_instance.fam_bastion_host}_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+
+  EOF
 }
