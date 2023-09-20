2fbc69effd72fa3182143424de15669e11b6959f
Joao Ferreira <joao.ferreira@encora.com>
2023-09-20 15:03:51 -0300

    userIdentityCard styling

--- frontend/src/components/grantaccess/UserIdentityCard.vue
+++ frontend/src/components/grantaccess/UserIdentityCard.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
+import { toRaw } from 'vue';
 import type { IdimProxyIdirInfo } from 'fam-api';
 import Card from 'primevue/card';
+import Icon from '../common/Icon.vue';
+import { IconSize } from '@/enum/IconEnum';
 
 const props = defineProps<{
     userIdentity: IdimProxyIdirInfo;
 }>(); // Vue3 alternative way for Type the defineProps().
+console.log(toRaw(props.userIdentity))
 </script>
 
 <template>
     <Card class="mb-2 p-0">
+        <template #header>
+            <Icon icon="checkmark--filled" :size="IconSize.medium" />
+            <p>Verified user information</p>
+        </template>
         <template #content>
-            <div class="row align-items-center">
-                <label class="col-3">User ID:</label>
-                <span class="col-sm">{{ props.userIdentity.userId }}</span>
+            <div class="col">
+                <label class="row">User ID:</label>
+                <span class="row">{{ props.userIdentity.userId }}</span>
             </div>
-            <div class="row align-items-center" v-if="props.userIdentity.found">
-                <label class="col-3">Display Name: </label>
-                <span class="col-sm">{{ props.userIdentity.displayName }}</span>
+            <div class="col" v-if="props.userIdentity.found">
+                <label class="row" >Display Name: </label>
+                <span  class="row">{{ props.userIdentity.displayName }}</span>
             </div>
-            <div class="row" v-if="!props.userIdentity.found">
+            <div class="col d-flex align-items-center" v-if="!props.userIdentity.found">
                 <span class="px-0 invalid"> User does not exist </span>
             </div>
         </template>
@@ -27,6 +35,18 @@ const props = defineProps<{
 
 <style lang="scss" scoped>
 @import '@/assets/styles/styles.scss';
+
+label {
+    color: $light-text-primary;
+    margin: 0 0 0.75rem !important;
+}
+
+span {
+    margin: 0 !important;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    letter-spacing: 0.01rem;
+}
 
 .invalid {
     font-size: 0.875em;
