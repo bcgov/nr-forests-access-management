0c2f02f969c58686897e410eb060ea6efa083292
Ian Liu <ianliuwk1019@gmail.com>
2023-09-20 15:25:36 -0700

    Reanme instance and comment out default ebs encryption to try.

--- infrastructure/server/ec2_instances.tf
+++ infrastructure/server/ec2_instances.tf
@@ -1,35 +1,38 @@
 locals {
-    fam_bastion_host_instance_profile_name_prefix = "fam_bastion_host"
+    fam_util_ec2_instance_profile_name_prefix = "fam_util_ec2_instance"
 }
 
-variable "fam_bastion_host_instance_ami" {
-  description = "Instance image for Bastion Host"
+variable "fam_util_ec2_instance_ami" {
+  description = "Instance image for FAM Util EC2"
   type        = string
   # Amazon Linux 2 Kernel 5.10 AMI 2.0.20230119.1 x86_64 HVM gp2
   default     = "ami-092e716d46cd65cac"
 }
 
-variable "fam_bastion_host_instance_type" {
+variable "fam_util_ec2_instance_type" {
   description = "Instance type to use for the instance"
   type        = string
   default     = "t2.micro"
 }
 
-resource "aws_ebs_encryption_by_default" "fam_default_ebs_encryption" {
-  enabled = true
-}
+# Comment out to see if any difference.
+# resource "aws_ebs_encryption_by_default" "fam_default_ebs_encryption" {
+#   enabled = true
+# }
 
-resource "aws_instance" "fam_bastion_host" {
-  ami                     = "${var.fam_bastion_host_instance_ami}"
-  instance_type           = "${var.fam_bastion_host_instance_type}"
+resource "aws_instance" "fam_util_ec2_instance" {
+  ami                     = "${var.fam_util_ec2_instance_ami}"
+  instance_type           = "${var.fam_util_ec2_instance_type}"
   subnet_id = data.aws_subnet.a_app.id
   vpc_security_group_ids = ["${aws_security_group.fam_app_sg.id}"]
 
-  depends_on = [aws_security_group.fam_app_sg, aws_ebs_encryption_by_default.fam_default_ebs_encryption]
+  # depends_on = [aws_security_group.fam_app_sg, aws_ebs_encryption_by_default.fam_default_ebs_encryption]
+  depends_on = [aws_security_group.fam_app_sg]
 
-  iam_instance_profile = "${aws_iam_instance_profile.fam_ec2_bastion_host_profile.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.fam_util_ec2_instance_profile.id}"
 
   root_block_device {
+    # At default, this is not set.
     encrypted = true
   }
 
@@ -54,13 +57,13 @@ resource "aws_ec2_instance_state" "fam_bastion_host_state" {
 }
 
 # TODO: experimeting on permission below.
-resource "aws_iam_instance_profile" "fam_ec2_bastion_host_profile" {
-  name = "${local.fam_waf_api_gateway_resource_name}_instance_profile"
-  role = "${aws_iam_role.fam_ec2_bastion_host_role.name}"
+resource "aws_iam_instance_profile" "fam_util_ec2_instance_profile" {
+  name = "${local.fam_util_ec2_instance_profile_name_prefix}_instance_profile"
+  role = "${aws_iam_role.fam_util_ec2_instance_role.name}"
 }
 
-resource "aws_iam_role" "fam_ec2_bastion_host_role" {
-  name = "${local.fam_waf_api_gateway_resource_name}_role"
+resource "aws_iam_role" "fam_util_ec2_instance_role" {
+  name = "${local.fam_util_ec2_instance_profile_name_prefix}_role"
 
   assume_role_policy = <<EOF
 {
