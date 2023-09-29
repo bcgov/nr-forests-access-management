08d597e348549111f3f85269709ca8f51e89c569
Ian Liu <ianliuwk1019@gmail.com>
2023-09-29 07:25:47 -0700

    Add temporary cloudfronte default certificate for tools.

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -37,9 +37,10 @@ resource "aws_cloudfront_distribution" "web_distribution" {
   web_acl_id          = "${aws_wafv2_web_acl.fam_waf_cloudfront.arn}"
 
   viewer_certificate {
-    acm_certificate_arn = "${var.cloudfront_certificate_arn}"
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = true # TODO: remove this. Temporary only.
+    # acm_certificate_arn = "${var.cloudfront_certificate_arn}"
+    # ssl_support_method = "sni-only"
+    # minimum_protocol_version = "TLSv1.2_2021"
   }
 
   origin {
