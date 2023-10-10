b3dbc05aba1c14c996bfa071db12a6b440f38356
Ian Liu <ianliuwk1019@gmail.com>
2023-10-10 16:02:35 -0700

    Clean up previous unused ec2 policy.

--- infrastructure/server/ec2_instances.tf
+++ infrastructure/server/ec2_instances.tf
@@ -56,26 +56,4 @@ resource "aws_ec2_instance_state" "fam_util_ec2_instance_state" {
 resource "aws_iam_instance_profile" "fam_util_ec2_instance_profile" {
   name = "${local.fam_util_ec2_instance_profile_name_prefix}_instance_profile"
   role = "EC2-Default-SSM-AD-Role"
-  # role = "${aws_iam_role.fam_util_ec2_instance_role.name}"
 }
-
-# resource "aws_iam_role" "fam_util_ec2_instance_role" {
-#   name = "${local.fam_util_ec2_instance_profile_name_prefix}_role"
-
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ec2.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-
-#   EOF
-# }
