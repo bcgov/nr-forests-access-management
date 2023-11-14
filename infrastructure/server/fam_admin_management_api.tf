e8ac04e00660101c45bf6dbdbd8e030171fa0dc7
catherine meng <mcatherine1994@gmail.com>
2023-11-14 15:15:21 -0800

    fix(884): fix missing env issue in pipeline for admin management test, refs: #884

--- infrastructure/server/fam_admin_management_api.tf
+++ infrastructure/server/fam_admin_management_api.tf
@@ -4,9 +4,9 @@ data "aws_secretsmanager_secret" "db_admin_management_api_creds_secret" {
   name = aws_secretsmanager_secret.famdb_admin_management_apicreds_secret.name
 }
 
-# data "aws_secretsmanager_secret" "fam_oidc_client_id_secret" {
-#   name = aws_secretsmanager_secret.fam_oidc_client_id_secret.name
-# }
+data "aws_secretsmanager_secret" "fam_oidc_client_id_secret" {
+  name = aws_secretsmanager_secret.fam_oidc_client_id_secret.name
+}
 
 data "aws_rds_cluster" "admin_management_api_database" {
   cluster_identifier = var.famdb_cluster_name
@@ -22,55 +22,6 @@ data "aws_db_proxy" "admin_management_api_lambda_db_proxy" {
 locals {
   admin_management_api_lambda_name = "fam-admin-management-api-lambda-${var.target_env}"
 }
-
-# resource "aws_iam_role_policy" "fam_admin_management_api_lambda_access_policy" {
-#   name   = "${local.admin_management_api_lambda_name}-access-policy"
-#   role   = aws_iam_role.fam_admin_management_api_lambda_exec.id
-#   policy = <<-EOF
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Effect": "Allow",
-#         "Action": [
-#             "logs:CreateLogGroup",
-#             "logs:CreateLogStream",
-#             "logs:PutLogEvents",
-#             "ec2:CreateNetworkInterface",
-#             "ec2:DescribeNetworkInterfaces",
-#             "ec2:DeleteNetworkInterface",
-#             "ec2:AssignPrivateIpAddresses",
-#             "ec2:UnassignPrivateIpAddresses"
-#         ],
-#         "Resource": "*"
-#       },
-#       {
-#         "Effect": "Allow",
-#         "Action": [
-#           "secretsmanager:DescribeSecret",
-#           "secretsmanager:GetSecretValue"
-#         ],
-#         "Resource": "${data.aws_secretsmanager_secret.db_admin_management_api_creds_secret.arn}"
-#       },
-#       {
-#         "Effect": "Allow",
-#         "Action": [
-#           "secretsmanager:DescribeSecret",
-#           "secretsmanager:GetSecretValue"
-#         ],
-#         "Resource": "${data.aws_secretsmanager_secret.fam_oidc_client_id_secret.arn}"
-#       },
-#       {
-#         "Effect": "Allow",
-#         "Action": [
-#                 "kms:*"
-#             ],
-#         "Resource": "${aws_kms_key.bcsc_key.arn}"
-#       }
-#     ]
-#   }
-#   EOF
-# }
 
 resource "aws_iam_role_policy" "fam_admin_management_api_lambda_access_policy" {
   name   = "${local.admin_management_api_lambda_name}-access-policy"
@@ -100,6 +51,14 @@ resource "aws_iam_role_policy" "fam_admin_management_api_lambda_access_policy" {
           "secretsmanager:GetSecretValue"
         ],
         "Resource": "${data.aws_secretsmanager_secret.db_admin_management_api_creds_secret.arn}"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue"
+        ],
+        "Resource": "${data.aws_secretsmanager_secret.fam_oidc_client_id_secret.arn}"
       },
       {
         "Effect": "Allow",
@@ -164,23 +123,15 @@ resource "aws_lambda_function" "fam-admin-management-api-function" {
       PG_DATABASE              = "${data.aws_rds_cluster.admin_management_api_database.database_name}"
       PG_PORT                  = "5432"
       PG_HOST                  = "${data.aws_db_proxy.admin_management_api_lambda_db_proxy.endpoint}"
-      # COGNITO_REGION           = "${data.aws_region.current.name}"
-      # COGNITO_USER_POOL_ID     = "${aws_cognito_user_pool.fam_user_pool.id}"
-      # COGNITO_USER_POOL_DOMAIN = "${var.fam_user_pool_domain_name}"
-      # COGNITO_CLIENT_ID_SECRET = "${data.aws_secretsmanager_secret.fam_oidc_client_id_secret.name}"
-      # BCSC_KEY_ID              = "${aws_kms_key.bcsc_key.id}"
+      COGNITO_REGION           = "${data.aws_region.current.name}"
+      COGNITO_USER_POOL_ID     = "${aws_cognito_user_pool.fam_user_pool.id}"
+      COGNITO_USER_POOL_DOMAIN = "${var.fam_user_pool_domain_name}"
+      COGNITO_CLIENT_ID_SECRET = "${data.aws_secretsmanager_secret.fam_oidc_client_id_secret.name}"
 
       API_GATEWAY_STAGE_NAME = "${var.api_gateway_stage_name}"
-      # COGNITO_CLIENT_ID        = "3hv7q2mct0okt12m5i3p5v4phu"
+      COGNITO_CLIENT_ID        = "3hv7q2mct0okt12m5i3p5v4phu"
 
       ALLOW_ORIGIN = "${var.front_end_redirect_path}"
-
-      # FC_API_TOKEN              = "${var.forest_client_api_api_key}"
-      # FC_API_BASE_URL           = "${var.forest_client_api_base_url}"
-      # ENABLE_BCSC_JWKS_ENDPOINT = "True"
-      # IDIM_PROXY_BASE_URL       = "${var.idim_proxy_api_base_url}"
-      # IDIM_PROXY_API_KEY        = "${var.idim_proxy_api_api_key}"
-      # GC_NOTIFY_EMAIL_API_KEY   = "${var.gc_notify_email_api_key}"
     }
 
   }
