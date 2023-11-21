95ebcfe05e5d68cab9afef95881f881bc3d69b86
catherine meng <mcatherine1994@gmail.com>
2023-11-21 13:31:19 -0800

    fix(885): fix username str constrains, refs: #885

--- server/admin_management/api/app/schemas.py
+++ server/admin_management/api/app/schemas.py
@@ -9,7 +9,6 @@ from typing_extensions import Annotated
 LOGGER = logging.getLogger(__name__)
 
 
-
 # -------------------------------------- Requester --------------------------------------- #
 # Schema classes Requester and TargetUser are for backend system used and
 # NOT intended as part of the request/respoinse body in the endpoint. Logged
@@ -22,7 +21,7 @@ class Requester(BaseModel):
 
     # cognito_user_id => Cognito OIDC access token maps this to: username (ID token => "custom:idp_name" )
     cognito_user_id: Union[str, None] = None
-    user_name: Annotated[str, StringConstraints(max_length=15)]
+    user_name: Annotated[str, StringConstraints(max_length=20)]
     # "B"(BCeID) or "I"(IDIR). It is the IDP provider.
     user_type_code: Union[famConstants.UserType, None] = None
     access_roles: Union[
@@ -42,7 +41,7 @@ class FamUser(BaseModel):
     cognito_user_id: Optional[
         Annotated[str, StringConstraints(max_length=100)]
     ] = None  # temporarily optional
-    user_name: Annotated[str, StringConstraints(max_length=100)]
+    user_name: Annotated[str, StringConstraints(max_length=20)]
     user_guid: Optional[Annotated[str, StringConstraints(max_length=32)]] = None
     create_user: Annotated[str, StringConstraints(max_length=60)]
     update_user: Optional[Annotated[str, StringConstraints(max_length=60)]] = None
@@ -54,7 +53,7 @@ class FamUser(BaseModel):
 # Application Admin assignment with one application at a time for the user.
 class FamAppAdminCreate(BaseModel):
     user_name: Annotated[
-        str, StringConstraints(min_length=3, max_length=100)
+        str, StringConstraints(min_length=3, max_length=20)
     ]  # db max length
     user_type_code: famConstants.UserType
     application_id: int
