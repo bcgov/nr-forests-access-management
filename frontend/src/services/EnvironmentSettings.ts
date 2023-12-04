c45d88f547fa302580a5cf4dc4a8202caedad287
Ian Liu <ianliuwk1019@gmail.com>
2023-12-04 13:58:39 -0800

    Add return of AdminMgmtApiBaseUrl.

--- frontend/src/services/EnvironmentSettings.ts
+++ frontend/src/services/EnvironmentSettings.ts
@@ -1,6 +1,13 @@
 export class EnvironmentSettings {
     env: any;
 
+    private environmentDisplayNameKey: string = 'fam_environment_display_name';
+
+    private readonly API = {
+        ADMIN_MANAGEMENT_API: "admin_management_api",
+        APP_ACCESS_CONTROL_API: "app_access_control_api"
+    };
+
     constructor() {
         this.env = JSON.parse(
             window.localStorage.getItem('env_data') as string
@@ -22,10 +29,14 @@ export class EnvironmentSettings {
         return this.env?.fam_console_idp_name.value;
     }
 
-    getApiBaseUrl(): string {
-        let apiBaseUrl = this.env?.fam_api_base_url.value;
-        apiBaseUrl ? null : (apiBaseUrl = 'http://127.0.0.1:8000'); // local api
-        return apiBaseUrl;
+    // Admin Management API
+    getAdminMgmtApiBaseUrl(): string {
+        return this.getApiBaseUrl(this.API.ADMIN_MANAGEMENT_API);
+    }
+
+    // App Access Control API
+    getAppAcsctlApiBaseUrl(): string {
+        return this.getApiBaseUrl(this.API.APP_ACCESS_CONTROL_API);
     }
 
     getEnvironmentDisplayName(prefix = '', suffix = ''): string {
@@ -44,5 +55,18 @@ export class EnvironmentSettings {
         window.localStorage.setItem(this.environmentDisplayNameKey, name);
     }
 
-    private environmentDisplayNameKey: string = 'fam_environment_display_name';
+    private getApiBaseUrl(useApi?: string) {
+        let apiBaseUrl
+
+        // Default to 'ADMIN_MANAGEMENT_API'
+        if (!useApi || useApi == this.API.ADMIN_MANAGEMENT_API) {
+            apiBaseUrl = this.env?.fam_admin_management_api_base_url.value
+                            || 'http://localhost:8001'; // local api
+        }
+        else {
+            apiBaseUrl = this.env?.fam_api_base_url.value
+                            || 'http://localhost:8000';
+        }
+        return apiBaseUrl;
+    }
 }
