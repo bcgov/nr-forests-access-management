7e475cd32b6120cf9d9b42fa3f43e80d1081bdb6
Ian Liu <ianliuwk1019@gmail.com>
2023-12-05 14:38:03 -0800

    Minor comment adjustment.

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,6 +1,6 @@
 import { inject, type InjectionKey } from "vue";
 
-// Helper functions for Vue's provide/inject to resolve with "undefined" case when component inject the dependency.
+// Helper function for Vue's provide/inject to resolve with "undefined" case when component inject the dependency.
 function requireInjection<T>(key: InjectionKey<T>, defaultValue?: T) {
     const resolved = inject(key, defaultValue);
     if (!resolved) {
