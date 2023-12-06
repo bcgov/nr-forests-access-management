6a87bb62b0976d856aed8211ec4b619412e8cce8
catherine meng <mcatherine1994@gmail.com>
2023-12-06 14:28:31 -0800

    refactor(1069): add user domain select component, refs: #1069

---
+++ frontend/src/components/grantaccess/form/UserDomainSelect.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import RadioButton from 'primevue/radiobutton';
+
+const domainOptions = { IDIR: 'I', BCEID: 'B' };
+
+const props = defineProps({
+    domain: {
+        type: String,
+        default: 'I',
+    },
+});
+
+const emit = defineEmits(['change']);
+
+const computedDomain = computed({
+    get() {
+        return props.domain;
+    },
+    set(newSelectedDomain: string) {
+        emit('change', newSelectedDomain);
+    },
+});
+</script>
+
+<template>
+    <div class="user-radio-group">
+        <label> Select user's domain </label>
+        <div class="px-0">
+            <RadioButton
+                v-model="computedDomain"
+                inputId="idirSelect"
+                name="domainRadioOptions"
+                :value="domainOptions.IDIR"
+            />
+            <label class="mx-2" for="idirSelect">IDIR</label>
+        </div>
+        <div class="px-0">
+            <RadioButton
+                v-model="computedDomain"
+                inputId="becidSelect"
+                name="domainRadioOptions"
+                :value="domainOptions.BCEID"
+            />
+            <label class="mx-2" for="becidSelect">BCeID</label>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
