58bf2b451e736e6f7f96401d8f44142666c658c4
Ian Liu <ianliuwk1019@gmail.com>
2023-12-28 16:17:31 -0800

    Use Catherine's good ToastMessage refactoring.

--- frontend/src/components/common/ToastMessage.vue
+++ frontend/src/components/common/ToastMessage.vue
@@ -1,77 +1,32 @@
 <script setup lang="ts">
-import { app } from '@/main';
-import axios from 'axios';
-import Toast from 'primevue/toast';
-import { useToast } from 'primevue/usetoast';
 import { IconSize } from '@/enum/IconEnum';
+import { app } from '@/main';
+import { getToastErrorMsg, routeToastError, setRouteToastError, useToastService } from '@/store/ToastState';
+import Toast, { type ToastMessageOptions } from 'primevue/toast';
+import { watch } from 'vue';
 
-const toast = useToast();
+const { showToastErrorTopRight } = useToastService();
 
-const showToastTopRight = (sev: any, title: string, text: string) => {
-    toast.add({ severity: sev, summary: title, detail: text, group: 'tl' });
+app.config.errorHandler = (err) => {
+    showToastErrorTopRight(getToastErrorMsg(err));
 };
 
-const onError = (error: any, info: string) => {
-    console.error(`Error occurred: ${error.toString()}`);
-    const genericErrorMsg = {
-        title: 'Error',
-        text: 'An application error has occurred. Please try again. If the error persists contact support.',
-    };
-
-    // Axios Http instance error that we like to pop out additional toast message.
-    if (axios.isAxiosError(error)) {
-        const err = error;
-        const axiosResponse = err.response;
-        const status = axiosResponse?.status;
-
-        const e401_authenticationErrorMsg = {
-            title: 'Error',
-            text: 'You are not logged in. Please log in.',
-        };
-
-        const e403_authorizationErrorMsg = {
-            title: 'Error',
-            text: 'You do not have the necessary authorization for the requested action.',
-        };
-
-        if (!status) {
-            showToastTopRight(
-                'error',
-                genericErrorMsg.title,
-                genericErrorMsg.text
-            );
-        } else if (status == 401) {
-            showToastTopRight(
-                'error',
-                e401_authenticationErrorMsg.title,
-                e401_authenticationErrorMsg.text
-            );
-        } else if (status == 403) {
-            showToastTopRight(
-                'error',
-                e403_authorizationErrorMsg.title,
-                e403_authorizationErrorMsg.text
-            );
-        } else if (status == 409) {
-            showToastTopRight(
-                'error',
-                genericErrorMsg.title,
-                axiosResponse?.data.detail
-            );
-        }
-        return;
+// Watch RouteError state change and popup toast if needed.
+watch(routeToastError, (value) => {
+    if (value) {
+        showToastErrorTopRight(getToastErrorMsg(value));
     }
+});
 
-    showToastTopRight('error', genericErrorMsg.title, genericErrorMsg.text);
-};
+const handleToastTimeoutEnds = (message: ToastMessageOptions) => {
+    setRouteToastError(undefined); // clear routeToastError state
+}
 
-app.config.errorHandler = (err, instance, info) => {
-    onError(err, info);
-};
 </script>
 
 <template>
-    <Toast group="tl" position="top-right" #icon>
+    <Toast group="tl" position="top-right" #icon
+        @life-end="handleToastTimeoutEnds">
         <Icon
             icon="error--filled"
             :size="IconSize.large"
---
+++ frontend/src/store/ToastState.ts
@@ -0,0 +1,96 @@
+import { ref } from 'vue';
+import axios from 'axios';
+import { useToast } from 'primevue/usetoast';
+import { RouteErrorName, FamRouteError } from '@/router/RouteError';
+
+// the toastError state is just used to store the error before enter the router
+export const toastError = ref('');
+export const setToastError = (error: string) => {
+    toastError.value = error;
+};
+export const clearToastError = () => {
+    toastError.value = '';
+};
+
+// TODO: Below is similiar as above Catherine's good usage.
+// But with custom RouteError passing instead of string type.
+
+// A RotetoastError state. Special case for handling routing error that
+// cannot be caught from Vue.
+export const routeToastError = ref();
+export const setRouteToastError = (error: FamRouteError | undefined) => {
+    routeToastError.value = error;
+};
+
+// the primevue toast can only be injected in setup or functional component,
+// in order to reuse the toast service,
+// we have to define it in the way as below
+// https://stackoverflow.com/questions/72425297/vue-warn-inject-can-only-be-used-inside-setup-or-functional-components
+export const useToastService = () => {
+    const toast = useToast();
+
+    const showToastErrorTopRight = (text: string) => {
+        toast.add({
+            severity: 'error',
+            summary: 'ERROR',
+            detail: text,
+            group: 'tl',
+            life: 5000 // delay before auto cleared.
+        });
+    };
+
+    return { showToastErrorTopRight };
+};
+
+const genericErrorMsg = {
+    title: 'Error',
+    text: 'An application error has occurred. Please try again. If the error persists contact support.',
+};
+
+export const getToastErrorMsg = (error: any) => {
+    console.error(`Error occurred: ${error.toString()}`);
+
+    // Axios Http instance error that we like to pop out additional toast message.
+    if (axios.isAxiosError(error)) {
+        const axiosResponse = error.response;
+        const status = axiosResponse?.status;
+
+        const e401_authenticationErrorMsg = {
+            title: 'Error',
+            text: 'You are not logged in. Please log in.',
+        };
+
+        const e403_authorizationErrorMsg = {
+            title: 'Error',
+            text: 'You do not have the necessary authorization for the requested action.',
+        };
+
+        if (!status) {
+            return genericErrorMsg.text;
+        } else if (status == 401) {
+            return e401_authenticationErrorMsg.text;
+        } else if (status == 403) {
+            return e403_authorizationErrorMsg.text;
+        } else if (status == 409) {
+            return axiosResponse?.data.detail;
+        }
+        return;
+    }
+
+    if (error instanceof FamRouteError) {
+        return handleRouteErrorMessage(error);
+    }
+
+    return genericErrorMsg.text;
+};
+
+const handleRouteErrorMessage = (error: FamRouteError): string => {
+    if (RouteErrorName.NO_APPLICATION_SELECTED_ERROR == error.name) {
+        return error.message;
+    }
+    return genericErrorMsg.text;
+}
+
+export const setToastErrorMsg = (error: any) => {
+    setToastError(getToastErrorMsg(error));
+};
