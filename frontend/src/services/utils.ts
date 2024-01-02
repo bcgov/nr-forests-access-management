c66149a7b6f6cfeabd7615acb5980c01f47d4487
Ian Liu <81595625+ianliuwk1019@users.noreply.github.com>
2024-01-02 15:17:40 -0800

    fix: #1097 add route security (#1104)
    Co-authored-by: catherine meng <mcatherine1994@gmail.com>

---
+++ frontend/src/services/utils.ts
@@ -0,0 +1,20 @@
+type AsyncWrapType = {
+    data: any;
+    error: any;
+};
+
+/**
+ * This is for use in case if you need to warp normal async return
+ * (promise's resolve or reject) so you can await for it and use
+ * normal "if (data)/else { handle error}" than typical try-catch.
+ * @param promise Promise()
+ * @returns '{data, error}' in tuple containing 'data' (underfined when 'error' present).
+ */
+export const asyncWrap = async (promise: Promise<any>): Promise<AsyncWrapType> => {
+    try {
+        const data = await promise;
+        return {data, error: undefined};
+    } catch (error) {
+        return {data: undefined, error}
+    }
+}
