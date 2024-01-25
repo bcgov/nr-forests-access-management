3e2af6849ab5c806e8371d797b8baf2b607c9c32
Joao Ferreira <joao.ferreira@encora.com>
2024-01-25 16:24:29 -0300

    using new state on tabPanel

--- frontend/src/components/managePermissions/ManagePermissions.vue
+++ frontend/src/components/managePermissions/ManagePermissions.vue
@@ -138,7 +138,7 @@ const deleteAppAdmin = async (admin: FamAppAdminGet) => {
             >
                 <TabPanel
                     header="Application admins"
-                    v-if="selectedApplication?.application_id === FAM_APPLICATION_ID"
+                    v-if="selectedApplicationId === FAM_APPLICATION_ID"
                 >
                     <template #header>
                         <Icon icon="enterprise" :size="IconSize.small" />
