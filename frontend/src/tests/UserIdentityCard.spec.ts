ab436e60ce05a8e228aec9bba7e450de7660c762
Joao Ferreira <joao.ferreira@encora.com>
2024-01-26 11:11:26 -0300

    moving variable to test that applies it

--- frontend/src/tests/UserIdentityCard.spec.ts
+++ frontend/src/tests/UserIdentityCard.spec.ts
@@ -15,14 +15,6 @@ describe('UserIdentityCard', () => {
     let cardEl: HTMLDivElement;
     let username: DOMWrapper<HTMLElement>;
     let usernameEl: HTMLSpanElement;
-    let firstName: DOMWrapper<HTMLElement>;
-    let firstNameEl: HTMLSpanElement;
-    let lastName: DOMWrapper<HTMLElement>;
-    let lastNameEl: HTMLSpanElement;
-    let userNotExist: DOMWrapper<HTMLElement>;
-    let userNotExistEl: HTMLSpanElement;
-    let checkmarkIcon: DOMWrapper<SVGImageElement>;
-    let errorIcon: DOMWrapper<SVGImageElement>;
 
     const props = {
         userIdentity: {
@@ -54,31 +46,23 @@ describe('UserIdentityCard', () => {
         cardEl = card.element as HTMLInputElement;
         username = wrapper.find('#userId');
         usernameEl = username.element as HTMLSpanElement;
-        firstName = wrapper.find('#firstName');
-        firstNameEl = firstName.element as HTMLSpanElement;
-        lastName = wrapper.find('#lastName');
-        lastNameEl = lastName.element as HTMLSpanElement;
-        checkmarkIcon = wrapper.find('#checkmarkIcon');
-        errorIcon = wrapper.find('#errorIcon');
     });
 
     it('Should show correct info on card based on props', () => {
         //it renders
-        expect(card.element).toBeTruthy();
+        expect(wrapper.find('.custom-card').element).toBeTruthy();
 
         expect(usernameEl.textContent).toContain(props.userIdentity.userId);
-        expect(firstNameEl.textContent).toContain(props.userIdentity.firstName);
-        expect(lastNameEl.textContent).toContain(props.userIdentity.lastName);
-        expect(checkmarkIcon).toBeTruthy();
+        expect(wrapper.find('#firstName').element.textContent).toContain(props.userIdentity.firstName);
+        expect(wrapper.find('#lastName').element.textContent).toContain(props.userIdentity.lastName);
+        expect(wrapper.find('#checkmarkIcon')).toBeTruthy();
     });
 
     it('Should show the User does not exist when receive not found prop', async () => {
         await wrapper.setProps(propsNotFound);
-        userNotExist = wrapper.find('#userNotExist');
-        userNotExistEl = userNotExist.element as HTMLSpanElement;
 
         expect(usernameEl.textContent).toContain(propsNotFound.userIdentity.userId);
-        expect(userNotExistEl.textContent).toContain('User does not exist');
-        expect(errorIcon).toBeTruthy();
+        expect(wrapper.find('#userNotExist').element.textContent).toContain('User does not exist');
+        expect(wrapper.find('#errorIcon')).toBeTruthy();
     });
 });
