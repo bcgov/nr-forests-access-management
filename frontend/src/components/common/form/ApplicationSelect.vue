ceba2b6ff1e49c02547748835264940bb0f468fd
Nicola Saglioni <nicola.saglioni@mobeus.com>
2024-01-31 07:49:50 -0300

    Adding missing file from git

---
+++ frontend/src/components/common/form/ApplicationSelect.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, type PropType, onMounted } from 'vue';
+import Dropdown, { type DropdownChangeEvent } from 'primevue/dropdown';
+import type { FamApplication } from 'fam-app-acsctl-api';
+
+const props = defineProps({
+    label: { type: String, required: false, default: 'Select application:' },
+    model: {
+        type: (Object as PropType<FamApplication>) || null,
+        required: true,
+    },
+    options: {
+        type: Array<FamApplication>,
+        default: [],
+        required: true,
+    },
+    placeholder: {
+        type: String,
+        required: false,
+        default: 'Choose an application',
+    },
+});
+
+const model = ref<FamApplication>();
+
+const emit = defineEmits(['onApplicationSelected']);
+
+onMounted(() => {
+    model.value = props.model;
+});
+
+const onApplicationSelected = async (e: DropdownChangeEvent) => {
+    emit('onApplicationSelected', e);
+};
+</script>
+<template>
+    <label>{{ label }}</label>
+    <Dropdown
+        v-model="model"
+        @change="onApplicationSelected"
+        :options="props.options"
+        optionLabel="application_description"
+        :placeholder="props.placeholder"
+        class="application-dropdown"
+    />
+</template>
+<style scoped lang="scss">
+.application-dropdown {
+    max-width: calc(100vw - 3rem);
+    height: 3rem;
+    padding: 0;
+
+    &:deep(.p-dropdown-label) {
+        padding: 0.8375rem 1rem;
+    }
+}
+@media (min-width: 495px) {
+    .application-dropdown {
+        max-width: 29rem;
+    }
+}
+@media (min-width: 1024px) {
+    .application-dropdown {
+        max-width: 38rem;
+    }
+}
+</style>
