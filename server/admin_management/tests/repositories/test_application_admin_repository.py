03bc2afeeb781505ec9903388f3f39011ab23fbb
Ian Liu <ianliuwk1019@gmail.com>
2024-02-06 09:31:23 -0800

    Adjust and finish test_get_user_app_admin_grants test.

--- server/admin_management/tests/repositories/test_application_admin_repository.py
+++ server/admin_management/tests/repositories/test_application_admin_repository.py
@@ -3,13 +3,13 @@ import logging
 import pytest
 from api.app.repositories.application_admin_repository import \
     ApplicationAdminRepository
-from api.app.repositories.application_repository import ApplicationRepository
 from api.app.repositories.user_repository import UserRepository
 from sqlalchemy.exc import IntegrityError
 from tests.constants import (ERROR_VOLIATE_UNIQUE_CONSTRAINT,
                              TEST_ANOTHER_CREATER,
                              TEST_APPLICATION_ADMIN_APPLICATION_ID,
-                             TEST_APPLICATION_ID_FAM, TEST_CREATOR,
+                             TEST_APPLICATION_ID_FAM,
+                             TEST_APPLICATION_ID_FOM_DEV, TEST_CREATOR,
                              TEST_FAM_ADMIN_USER,
                              TEST_NEW_APPLICATION_ADMIN_USER_ID)
 
@@ -93,7 +93,7 @@ def test_delete_application_admin(application_admin_repo: ApplicationAdminReposi
     assert application_admin is None
 
 
-def test_get_user_app_admin_grants_as_fam_admin(
+def test_get_user_app_admin_grants(
     application_admin_repo: ApplicationAdminRepository,
     user_repo: UserRepository
 ):
@@ -111,7 +111,7 @@ def test_get_user_app_admin_grants_as_fam_admin(
     assert new_user.user_name == TEST_FAM_ADMIN_USER.user_name
     assert new_user.user_type_code == TEST_FAM_ADMIN_USER.user_type_code
 
-    # prepare a new FAM admin from the new_user
+    # add new_user as a new FAM admin
     new_fam_admin = application_admin_repo.create_application_admin(
         TEST_APPLICATION_ID_FAM,
         new_user.user_id,
@@ -120,12 +120,53 @@ def test_get_user_app_admin_grants_as_fam_admin(
     assert new_fam_admin.application_id == TEST_APPLICATION_ID_FAM
     assert new_fam_admin.user_id == new_user.user_id
 
-    # test get_user_app_admin_grants for FAM admin.
+    # test get_user_app_admin_grants for FAM admin user "new_user".
     fam_admin_grants = application_admin_repo.get_user_app_admin_grants(
-        new_fam_admin.user_id
+        new_user.user_id
     )
 
     # granted application for the user should be only for FAM.
     assert len(fam_admin_grants) == 1
     granted_app = fam_admin_grants[0]
     assert granted_app.application_id == TEST_APPLICATION_ID_FAM
+
+    # add new_user as a new FOM_DEV admin
+    new_app_admin = application_admin_repo.create_application_admin(
+        TEST_APPLICATION_ID_FOM_DEV,
+        new_user.user_id,
+        TEST_CREATOR,
+    )
+    assert new_app_admin.application_id == TEST_APPLICATION_ID_FOM_DEV
+    assert new_app_admin.user_id == new_user.user_id
+
+    # test get_user_app_admin_grants for "new_user".
+    app_admin_grants = application_admin_repo.get_user_app_admin_grants(
+        new_user.user_id
+    )
+
+    # granted applications for the user should be for FAM and FOM_DEV.
+    assert len(app_admin_grants) == 2
+    granted_application_list = list(
+        map(lambda x: x.application_id, app_admin_grants)
+    )
+    assert set(granted_application_list) == set(
+        [TEST_APPLICATION_ID_FAM, TEST_APPLICATION_ID_FOM_DEV])
+
+    # unassign FAM application admin from new_user
+    application_admin_repo.delete_application_admin(
+        new_fam_admin.application_admin_id
+    )
+    fam_admin = application_admin_repo.get_application_admin_by_id(
+        new_fam_admin.application_admin_id
+    )
+    assert fam_admin is None
+
+    # test get_user_app_admin_grants for "new_user".
+    admin_grants = application_admin_repo.get_user_app_admin_grants(
+        new_user.user_id
+    )
+
+    # granted application for the user should now be only for FOM_DEV.
+    assert len(admin_grants) == 1
+    granted_app = admin_grants[0]
+    assert granted_app.application_id == TEST_APPLICATION_ID_FOM_DEV
