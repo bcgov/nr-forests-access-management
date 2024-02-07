9ac557572aa1cfa166b1cecbe90b9c3d5da54796
Joao Ferreira <joao.ferreira@encora.com>
2024-02-07 17:36:03 -0300

    now using template sintax, fixed broken styles

--- frontend/src/components/common/PageTitle.vue
+++ frontend/src/components/common/PageTitle.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
 import Breadcrumb from 'primevue/breadcrumb';
 import { breadcrumbState } from '@/store/BreadcrumbState';
 
@@ -18,7 +18,23 @@ const route = useRoute();
 </script>
 
 <template>
-    <Breadcrumb v-if="route.meta.hasBreadcrumb" :model="breadcrumbState" />
+    <Breadcrumb
+        v-if="route.meta.hasBreadcrumb"
+        :model="breadcrumbState"
+        :pt="{
+            menuitem: { class: 'custom-menuitem' },
+        }"
+    >
+        <template #item="{ item }">
+            <RouterLink
+                v-if="item.path"
+                :to="item.path"
+                class="custom-menuitem-link"
+            >
+                <span class="custom-menutitem-text">{{ item.label }}</span>
+            </RouterLink>
+        </template>
+    </Breadcrumb>
     <h1 class="title">{{ props.title }}</h1>
     <h2 class="subtitle">{{ props.subtitle }}</h2>
 </template>
@@ -37,5 +53,36 @@ const route = useRoute();
     line-height: 1.125rem;
     letter-spacing: 0.01rem;
     color: $light-text-secondary;
+}
+
+.custom-menuitem:focus-visible,
+.custom-menuitem-link:focus-visible {
+    outline: none;
+}
+
+.custom-menuitem-link {
+    padding: 0.2rem;
+    text-decoration: none;
+}
+
+.custom-menuitem-link:focus {
+    box-shadow: inset 0 0px 0px 0.063rem $light-focus;
+}
+
+.custom-menuitem:last-child .custom-menuitem-link {
+    pointer-events: none;
+}
+
+.custom-menutitem-text {
+    color: $light-link-primary;
+}
+
+.custom-menutitem-text:hover {
+    color: $light-link-primary-hover;
+}
+
+.custom-menuitem:last-child .custom-menutitem-text {
+    color: $light-text-primary;
+    display: contents;
 }
 </style>
