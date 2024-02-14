5f9ec78650f7f7a7540bf49360b8af6803ec36af
catherine meng <mcatherine1994@gmail.com>
2024-02-14 15:23:53 -0800

    fix(1197): fix string, refs: #1197

--- infrastructure/server/oidc_idp_idir.tf
+++ infrastructure/server/oidc_idp_idir.tf
@@ -11,10 +11,10 @@ resource "aws_cognito_identity_provider" "dev_idir_oidc_provider" {
     client_secret             = var.dev_oidc_idir_idp_client_secret
     oidc_issuer               = var.dev_oidc_idp_issuer
     attributes_request_method = "GET"
-    authorize_url = "${var.dev_oidc_idp_authorization_endpoint}"
-    token_url = "${var.dev_oidc_idp_token_endpoint}"
-    attributes_url = "${var.dev_oidc_idp_userinfo_endpoint}"
-    jwks_uri = "${var.dev_oidc_idp_jwk_endpoint}"
+    authorize_url = var.dev_oidc_idp_authorization_endpoint
+    token_url = var.dev_oidc_idp_token_endpoint
+    attributes_url = var.dev_oidc_idp_userinfo_endpoint
+    jwks_uri = var.dev_oidc_idp_jwk_endpoint
   }
 
   attribute_mapping = {
@@ -38,10 +38,10 @@ resource "aws_cognito_identity_provider" "test_idir_oidc_provider" {
     client_secret             = var.test_oidc_idir_idp_client_secret
     oidc_issuer               = var.test_oidc_idp_issuer
     attributes_request_method = "GET"
-    authorize_url = "${var.test_oidc_idp_authorization_endpoint}"
-    token_url = "${var.test_oidc_idp_token_endpoint}"
-    attributes_url = "${var.test_oidc_idp_userinfo_endpoint}"
-    jwks_uri = "${var.test_oidc_idp_jwk_endpoint}"
+    authorize_url = var.test_oidc_idp_authorization_endpoint
+    token_url = var.test_oidc_idp_token_endpoint
+    attributes_url = var.test_oidc_idp_userinfo_endpoint
+    jwks_uri = var.test_oidc_idp_jwk_endpoint
   }
 
   attribute_mapping = {
@@ -66,10 +66,10 @@ resource "aws_cognito_identity_provider" "prod_idir_oidc_provider" {
     client_secret             = var.prod_oidc_idir_idp_client_secret
     oidc_issuer               = var.prod_oidc_idp_issuer
     attributes_request_method = "GET"
-    authorize_url = "${var.prod_oidc_idp_authorization_endpoint}"
-    token_url = "${var.prod_oidc_idp_token_endpoint}"
-    attributes_url = "${var.prod_oidc_idp_userinfo_endpoint}"
-    jwks_uri = "${var.prod_oidc_idp_jwk_endpoint}"
+    authorize_url = var.prod_oidc_idp_authorization_endpoint
+    token_url = var.prod_oidc_idp_token_endpoint
+    attributes_url = var.prod_oidc_idp_userinfo_endpoint
+    jwks_uri = var.prod_oidc_idp_jwk_endpoint
   }
 
   attribute_mapping = {
