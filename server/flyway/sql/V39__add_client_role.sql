e6912b824c624cbc23620f62740268fc804a3aa1
catherine meng <mcatherine1994@gmail.com>
2024-03-13 13:30:50 -0700

    fix(1232): add flyway script to add a new client role, refs: 1232

---
+++ server/flyway/sql/V39__add_client_role.sql
@@ -0,0 +1,47 @@
+-- Delete any role assignments to the CLIENT initial testing roles
+DELETE FROM app_fam.fam_user_role_xref WHERE role_id IN
+    (SELECT role_id from app_fam.fam_role WHERE application_id IN
+        (SELECT application_id FROM app_fam.fam_application WHERE application_name IN ('CLIENT_DEV', 'CLIENT_TEST', 'CLIENT_PROD'))
+    )
+;
+
+-- Delete previously created CLIENT testing roles for client_dev, client_test and client_prod applications
+DELETE FROM app_fam.fam_role WHERE application_id IN (
+    SELECT application_id FROM app_fam.fam_application WHERE application_name IN ('CLIENT_DEV', 'CLIENT_TEST', 'CLIENT_PROD')
+);
+
+-- Add a role for CLIENT_DEV
+INSERT INTO app_fam.fam_role (
+    role_name,
+    role_purpose,
+    application_id,
+    role_type_code,
+    create_user,
+    create_date
+)
+VALUES ('CLIENT_EDITOR', 'Create and edit client information', (select application_id from app_fam.fam_application where application_name = 'CLIENT_DEV'), 'C', CURRENT_USER, CURRENT_DATE)
+;
+
+-- Add a role for CLIENT_TEST
+INSERT INTO app_fam.fam_role (
+    role_name,
+    role_purpose,
+    application_id,
+    role_type_code,
+    create_user,
+    create_date
+)
+VALUES ('CLIENT_EDITOR', 'Create and edit client information', (select application_id from app_fam.fam_application where application_name = 'CLIENT_TEST'), 'C', CURRENT_USER, CURRENT_DATE)
+;
+
+-- Add a role for CLIENT_PROD
+INSERT INTO app_fam.fam_role (
+    role_name,
+    role_purpose,
+    application_id,
+    role_type_code,
+    create_user,
+    create_date
+)
+VALUES ('CLIENT_EDITOR', 'Create and edit client information', (select application_id from app_fam.fam_application where application_name = 'CLIENT_PROD'), 'C', CURRENT_USER, CURRENT_DATE)
+;
