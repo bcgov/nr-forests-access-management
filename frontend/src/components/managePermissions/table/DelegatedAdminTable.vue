a1c2c0c4409d2e594ef93ee00a25ef51966c7eea
Joao Ferreira <joao.ferreira@encora.com>
2024-04-03 10:35:31 -0300

    role sorting

--- frontend/src/components/managePermissions/table/DelegatedAdminTable.vue
+++ frontend/src/components/managePermissions/table/DelegatedAdminTable.vue
@@ -37,7 +37,7 @@ const props = defineProps({
 const delegatedAdminFilters = ref({
     global: { value: '', matchMode: FilterMatchMode.CONTAINS },
     'user.user_name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-    'user.user_type.description': {
+        'user.user_type.description': {
         value: null,
         matchMode: FilterMatchMode.CONTAINS,
     },
@@ -45,7 +45,7 @@ const delegatedAdminFilters = ref({
         value: null,
         matchMode: FilterMatchMode.CONTAINS,
     },
-    'role.parent_role.role_name': {
+'role.parent_role.role_name': {
         value: null,
         matchMode: FilterMatchMode.CONTAINS,
     },
@@ -151,6 +151,7 @@ const deleteDelegatedAdmin = (
                 <Column
                     header="Role Enabled To Assign"
                     sortable
+                    field="role.role_name"
                 >
                     <template #body="{ data }">
                         {{
