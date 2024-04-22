e661e3a9ee61d6790c3497f5c00b5236dc3f36df
Joao Ferreira <joao.ferreira@encora.com>
2024-04-22 14:01:05 -0300

    validation error test

--- frontend/src/tests/GrantApplicationAdmin.spec.ts
+++ frontend/src/tests/GrantApplicationAdmin.spec.ts
@@ -1,9 +1,10 @@
 import { routes } from '@/router';
 import { mount, VueWrapper } from '@vue/test-utils';
-import { it, describe, beforeEach, expect, afterEach,vi } from 'vitest';
+import { it, describe, beforeEach, expect, afterEach, vi } from 'vitest';
 import { routeItems } from '@/router/routeItem';
 import { fixJsdomCssErr } from './common/fixJsdomCssErr';
 import GrantApplicationAdmin from '@/components/grantaccess/GrantApplicationAdmin.vue';
+import waitForExpect from 'wait-for-expect';
 
 fixJsdomCssErr();
 vi.mock('vue-router', async () => {
@@ -17,6 +18,7 @@ vi.mock('vue-router', async () => {
         },
     };
 });
+
 describe('GrantApplicationAdmin', () => {
     let wrapper: VueWrapper;
 
@@ -31,14 +33,57 @@ describe('GrantApplicationAdmin', () => {
     it('Should display the correct heading texts', () => {
         // console.log(wrapper.html())
         wrapper.findAll('.title').forEach((title) => {
-            expect(title.isVisible()).toBe(true)
-        })
+            expect(title.isVisible()).toBe(true);
+        });
 
         wrapper.findAll('.subtitle').forEach((subtitle) => {
-            expect(subtitle.isVisible()).toBe(true)
-        })
-        expect(wrapper.find('#userIdInput').isVisible()).toBe(true)
-        expect(wrapper.find('[aria-label="Verify user IDIR"]').isVisible()).toBe(true)
-    })
+            expect(subtitle.isVisible()).toBe(true);
+        });
+        expect(wrapper.find('#userIdInput').isVisible()).toBe(true);
+        expect(
+            wrapper.find('[aria-label="Verify user IDIR"]').isVisible()
+        ).toBe(true);
+        expect(
+            wrapper.find('.application-admin-group .p-dropdown').isVisible()
+        ).toBe(true);
+    });
 
-})
+    it('Should show validation error when username input invalid', async () => {
+        const usernameInputText = wrapper.find('#userIdInput');
+        const usernameInputTextEl =
+            usernameInputText.element as HTMLInputElement;
+
+        await usernameInputText.setValue('I');
+        expect(usernameInputTextEl.value).toBe('I');
+
+        await waitForExpect(() => {
+            expect(
+                wrapper.find('.input-with-verify-button .invalid-feedback')
+                    .element.textContent
+            ).toContain('at least 2 characters');
+
+            //verify btn is disabled
+            expect(
+                wrapper
+                    .find('.input-with-verify-button button')
+                    .classes('p-disabled')
+            ).toBe(true);
+        });
+
+        await usernameInputText.setValue('');
+
+        expect(usernameInputTextEl.value).toBe('');
+        await waitForExpect(() => {
+            expect(
+                wrapper.find('.input-with-verify-button .invalid-feedback')
+                    .element.textContent
+            ).toContain('required');
+
+            expect(
+                wrapper
+                    .find('.input-with-verify-button button')
+                    .classes('p-disabled')
+            ).toBe(true);
+        });
+    });
+});
