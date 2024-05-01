b83b29524c6d13027eae3aaca5bae5ff9dd41ee6
catherine meng <mcatherine1994@gmail.com>
2024-05-01 11:06:09 -0700

    fix(1306): fix desscription for user type code B, refs: #1306

--- server/backend/api/app/routers/router_guards.py
+++ server/backend/api/app/routers/router_guards.py
@@ -81,9 +81,12 @@ async def get_current_requester(
     access_roles: List[str] = Depends(get_access_roles),
     db: Session = Depends(database.get_db),
 ):
+    LOGGER.debug(f"Debug router_guard get_current_requester, current request_cognito_user_id: {request_cognito_user_id}")
     fam_user: FamUser = crud_user.get_user_by_cognito_user_id(
         db, request_cognito_user_id
     )
+    LOGGER.debug(f"Debug router_guard get_current_requester, current db connection: {db}")
+    LOGGER.debug(f"Debug router_guard get_current_requester, current fam_user: {fam_user}")
     if fam_user is None:
         raise no_requester_exception
 
