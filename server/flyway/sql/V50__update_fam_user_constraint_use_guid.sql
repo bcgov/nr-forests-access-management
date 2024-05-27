e63b82c3c5b7a0776edff69cb97f31a9a7de6fab
catherine meng <mcatherine1994@gmail.com>
2024-05-27 16:25:28 -0700

    fix(747): fix fam user unique index, refs: #747

--- server/flyway/sql/V50__update_fam_user_constraint_use_guid.sql
+++ server/flyway/sql/V50__update_fam_user_constraint_use_guid.sql
@@ -1,3 +1,5 @@
-ALTER TABLE app_fam.fam_user DROP CONSTRAINT fam_usr_uk;
+-- Update unique constraint in fam user table to be user type code and user guid
+
+DROP INDEX app_fam.fam_usr_uk;
 
 CREATE UNIQUE INDEX fam_usr_uk ON app_fam.fam_user(user_type_code, lower(user_guid));
