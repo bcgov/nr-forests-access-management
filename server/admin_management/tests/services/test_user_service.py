333d540f8ccfc54a668158b1eeb1e220fc89e339
catherine meng <mcatherine1994@gmail.com>
2024-05-30 10:23:06 -0700

    feat(747): add test for user service in admin management, refs: #747

--- server/admin_management/tests/services/test_user_service.py
+++ server/admin_management/tests/services/test_user_service.py
@@ -1,10 +1,16 @@
 import logging
 
+from api.app.repositories.user_repository import UserRepository
 from api.app.services.user_service import UserService
-from tests.constants import TEST_NEW_IDIR_USER
+from tests.constants import (
+    TEST_NEW_IDIR_USER,
+    TEST_NEW_BCEID_USER,
+    TEST_USER_BUSINESS_GUID_BCEID,
+)
 
 
 LOGGER = logging.getLogger(__name__)
+NEW_USERNAME = "NEW_USERNAME"
 
 
 def test_find_or_create(user_service: UserService):
@@ -44,3 +50,101 @@ def test_find_or_create(user_service: UserService):
     users = user_service.get_users()
     # verify no user created
     assert len(users) == len(after_add_users_count)
+
+    # give the existing user with a new username need to be updated
+    updated_user = user_service.find_or_create(
+        TEST_NEW_IDIR_USER.user_type_code,
+        NEW_USERNAME,
+        TEST_NEW_IDIR_USER.user_guid,
+        TEST_NEW_IDIR_USER.create_user,
+    )
+    users = user_service.get_users()
+    # verify no user created
+    assert len(users) == len(after_add_users_count)
+    # verify username is updated
+    assert updated_user.user_name == NEW_USERNAME
+
+
+def test_update_user_name(user_service: UserService, user_repo: UserRepository):
+    # create a user
+    new_user = user_repo.create_user(TEST_NEW_IDIR_USER)
+    # verify new user is created
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_IDIR_USER.user_type_code, TEST_NEW_IDIR_USER.user_name
+    )
+    assert new_user.user_id == found_user.user_id
+
+    # test update user name
+    updated_user = user_service.update_user_name(
+        found_user, NEW_USERNAME, TEST_NEW_IDIR_USER.create_user
+    )
+    assert updated_user.user_name == NEW_USERNAME
+    # verify the username is updated
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_IDIR_USER.user_type_code, NEW_USERNAME
+    )
+    assert found_user.user_id == updated_user.user_id
+    assert found_user.user_name == NEW_USERNAME
+
+    # test user name no need to be updated
+    updated_user = user_service.update_user_name(
+        found_user, NEW_USERNAME.lower(), TEST_NEW_IDIR_USER.create_user
+    )
+    assert updated_user.user_name == NEW_USERNAME
+    # verify the username is not updated
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_IDIR_USER.user_type_code, NEW_USERNAME
+    )
+    assert found_user.user_id == updated_user.user_id
+    assert found_user.user_name == NEW_USERNAME
+
+
+def test_update_user_business_guid(
+    user_service: UserService, user_repo: UserRepository
+):
+    # create a business user
+    new_user = user_repo.create_user(TEST_NEW_BCEID_USER)
+    # verify new user is created
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_BCEID_USER.user_type_code, TEST_NEW_BCEID_USER.user_name
+    )
+    assert new_user.user_id == found_user.user_id
+
+    # test update business guid when no business guid stored
+    updated_user = user_service.update_user_business_guid(
+        found_user.user_id,
+        TEST_USER_BUSINESS_GUID_BCEID,
+        TEST_NEW_IDIR_USER.create_user,
+    )
+    assert updated_user.business_guid == TEST_USER_BUSINESS_GUID_BCEID
+    # verify the business_guid is updated
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_BCEID_USER.user_type_code, TEST_NEW_BCEID_USER.user_name
+    )
+    assert found_user.user_id == updated_user.user_id
+    assert found_user.business_guid == TEST_USER_BUSINESS_GUID_BCEID
+
+    # test update business guid when business guid mismatch
+    new_business_guid = "MOCKEDNEWBUSINESSGUID5D4ACA9FA90"
+    updated_user = user_service.update_user_business_guid(
+        found_user.user_id, new_business_guid, TEST_NEW_IDIR_USER.create_user
+    )
+    assert updated_user.business_guid == new_business_guid
+    # verify the business guid is updated
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_BCEID_USER.user_type_code, TEST_NEW_BCEID_USER.user_name
+    )
+    assert found_user.user_id == updated_user.user_id
+    assert found_user.business_guid == new_business_guid
+
+    # test business no need to be updated
+    updated_user = user_service.update_user_business_guid(
+        found_user.user_id, new_business_guid, TEST_NEW_IDIR_USER.create_user
+    )
+    assert updated_user.business_guid == new_business_guid
+    # verify the business guid is updated
+    found_user = user_service.get_user_by_domain_and_name(
+        TEST_NEW_BCEID_USER.user_type_code, TEST_NEW_BCEID_USER.user_name
+    )
+    assert found_user.user_id == updated_user.user_id
+    assert found_user.business_guid == new_business_guid
