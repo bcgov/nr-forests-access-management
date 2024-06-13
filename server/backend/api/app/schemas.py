56d3db6c29f608637d14ee1920bc402276132be0
MCatherine <Mcatherine1994@gmail.com>
2024-06-13 13:26:42 -0700

    feat: #1382 Store terms and conditions acceptance record (#1447)

--- server/backend/api/app/schemas.py
+++ server/backend/api/app/schemas.py
@@ -37,7 +37,9 @@ class FamUser(BaseModel):
         None  # temporarily optional
     )
     user_name: Annotated[str, StringConstraints(max_length=20)]
-    user_guid: Union[Annotated[str, StringConstraints(min_length=32, max_length=32)], None]
+    user_guid: Union[
+        Annotated[str, StringConstraints(min_length=32, max_length=32)], None
+    ]
     create_user: Annotated[str, StringConstraints(max_length=100)]
     update_user: Optional[Annotated[str, StringConstraints(max_length=100)]] = None
 
