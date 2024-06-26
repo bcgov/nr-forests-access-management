add74658a999206994a6249d22cf5cc0e052f5c1
Joao Ferreira <joao.ferreira@encora.com>
2024-06-26 15:31:11 -0300

    formatting

--- frontend/src/store/TermsAndConditionsState.ts
+++ frontend/src/store/TermsAndConditionsState.ts
@@ -5,12 +5,16 @@ const termsClosableState = ref(false);
 
 export const isTermsVisible = computed({
     get: () => termsVisibleState.value,
-    set: (value) => { termsVisibleState.value = value; }
+    set: (value) => {
+        termsVisibleState.value = value;
+    },
 });
 
 export const isTermsClosable = computed({
     get: () => termsClosableState.value,
-    set: (value) => { termsClosableState.value = value; }
+    set: (value) => {
+        termsClosableState.value = value;
+    },
 });
 
 export const toggleTermsCloseble = () => {
