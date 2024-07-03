e9590d9a90d57dda0e985e22c1348da6803acafe
catherine meng <mcatherine1994@gmail.com>
2024-07-03 10:25:44 -0700

    fix(1365): add comment to the new added columns, refs: #1365

--- server/flyway/sql/V52__add_first_last_name_and_email_to_fam_user.sql
+++ server/flyway/sql/V52__add_first_last_name_and_email_to_fam_user.sql
@@ -9,3 +9,11 @@ ADD
     COLUMN last_name VARCHAR(50),
 ADD
     COLUMN email VARCHAR(250);
+
+COMMENT ON COLUMN app_fam.fam_user.first_name IS 'The first name of the user';
+
+COMMENT ON COLUMN app_fam.fam_user.last_name IS 'The last name of the user.';
+
+COMMENT ON COLUMN app_fam.fam_user.email IS 'The email of the user.';
+
+COMMENT ON COLUMN app_fam.fam_user.business_guid IS 'The business guid of the user if is a business bceid user.';
