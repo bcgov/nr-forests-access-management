9f4a86cdb09268cb58998990676cdc3589bf8434
catherine meng <mcatherine1994@gmail.com>
2024-08-13 14:46:25 -0700

    fix(1469): add gc notify key to admin management terraform config, refs: #1469

--- infrastructure/server/fam_admin_management_api.tf
+++ infrastructure/server/fam_admin_management_api.tf
@@ -136,6 +136,7 @@ resource "aws_lambda_function" "fam_admin_management_api_function" {
 
       IDIM_PROXY_BASE_URL_PROD = "${var.idim_proxy_api_base_url_prod}"
       IDIM_PROXY_API_KEY = "${var.idim_proxy_api_api_key}"
+      GC_NOTIFY_EMAIL_API_KEY = "${var.gc_notify_email_api_key}"
       TARGET_ENV = "${var.target_env}"
     }
 
