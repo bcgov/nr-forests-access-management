dc288bd36a342b6e9a4e6d40b590b75134f49a11
catherine meng <mcatherine1994@gmail.com>
2024-08-23 14:39:09 -0700

    fix: add new user to local setup script

--- server/flyway/local_sql/sample_V1002__add_local_user_cognito_id.sql
+++ server/flyway/local_sql/sample_V1002__add_local_user_cognito_id.sql
@@ -49,5 +49,6 @@ VALUES
 ('JFERREIR','I','test-idir_278f48bd9mangledf7e618d@idir',CURRENT_USER),
 ('NSAGLION','I','test-idir_3f1414bd1mangled18a1293c40@idir',CURRENT_USER),
 ('CMENG','I','test-idir_a9888e8ac6a04mangled35df625bf@idir',CURRENT_USER),
-('OLIBERCH','I','test-idir_b7e191fa11dmangled9df1854b71@idir',CURRENT_USER);
+('OLIBERCH','I','test-idir_b7e191fa11dmangled9df1854b71@idir',CURRENT_USER),
+('CYU','I','test-idir_234d9cbe4373457famangledd26ab162@idir',CURRENT_USER);
 
