fc3a3c09b57b051b36483276405bba4a78be0a44
Craig Yu <craig.yu93@gmail.com>
2024-08-30 19:54:18 -0700

    fix: definitions

--- server/backend/api/app/models/fam_role.py
+++ server/backend/api/app/models/fam_role.py
@@ -28,6 +28,7 @@ class FamRoleModel(Base):
             cycle=False,
             cache=1,
         ),
+        primary_key=True,
         comment="Automatically generated key used to identify the uniqueness "
         "of a Role within the FAM Application",
     )
@@ -124,5 +125,52 @@ class FamRoleModel(Base):
 
 class FamRoleTypeModel(Base):
     __tablename__ = "fam_role_type"
-    role_type_code = Column(String(2), nullable=False)
-    description = Column(String(100), nullable=True)
+
+    # ROLE_TYPE_ABSTRACT = 'A'
+    # ROLE_TYPE_CONCRETE = 'C'
+
+    role_type_code = Column(
+        String(2),
+        nullable=False,
+        comment="role type code",
+        primary_key=True,
+    )
+
+    description = Column(
+        String(100),
+        nullable=True,
+        comment="Description of what the role_type_code represents",
+    )
+
+    effective_date = Column(
+        TIMESTAMP(timezone=True, precision=6),
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        comment="The date and time the code was effective.",
+    )
+
+    expiry_date = Column(
+        TIMESTAMP(timezone=True, precision=6),
+        nullable=True,
+        default=None,
+        comment="The date and time the code expired.",
+    )
+
+    update_date = Column(
+        TIMESTAMP(timezone=True, precision=6),
+        onupdate=datetime.datetime.utcnow,
+        comment="The date and time the record was created or last updated.",
+    )
+
+    __table_args__ = (
+        PrimaryKeyConstraint("role_type_code", name="fam_role_type_code_pk"),
+        # CheckConstraint(role_type_code.in_(['C', 'A'])),
+        {
+            "comment": "A role type is a code that is associated with roles "
+            "that will influence what can be associate with a role.  At time "
+            "of implementation an abstract role can only have other roles "
+            "related to it, while a concrete role can only have users "
+            "associated with it",
+            "schema": "app_fam",
+        },
+    )
