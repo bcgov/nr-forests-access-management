557e9eb0358b3ddfe51afdde7e190e364891f350
Craig Yu <craig.yu93@gmail.com>
2024-08-30 21:19:19 -0700

    fix: class order

--- server/backend/api/app/models/fam_user.py
+++ server/backend/api/app/models/fam_user.py
@@ -17,9 +17,74 @@ from sqlalchemy.orm import relationship, Mapped
 from .base import Base
 
 
+class FamUserTermsConditionsModel(Base):
+    __tablename__ = "fam_user_terms_conditions"
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["user_id"],
+            ["app_fam.fam_user.user_id"],
+            name="reffam_user_terms_conditions_user",
+        ),
+        PrimaryKeyConstraint(
+            "user_terms_conditions_id", name="fam_user_terms_conditions_pk"
+        ),
+        UniqueConstraint("user_id", "version", name="fam_tc_user_version_uk"),
+        Index("ix_app_fam_fam_user_terms_conditions_user_id", "user_id"),
+        {
+            "comment": "User Terms Conditions records identify the users who accept the "
+            "terms and conditions, as well as the version of it.",
+            "schema": "app_fam",
+        },
+    )
+
+    user_terms_conditions_id = Column(
+        BigInteger,
+        Identity(
+            start=1,
+            increment=1,
+            minvalue=1,
+            maxvalue=9223372036854775807,
+            cycle=False,
+            cache=1,
+        ),
+        comment="Automatically generated key used to identify the uniqueness of a terms and conditions acceptance record.",
+    )
+    user_id = Column(
+        BigInteger,
+        nullable=False,
+        comment="Unique ID to reference and identify the user within FAM system.",
+    )
+    version = Column(
+        String(30),
+        nullable=False,
+        comment="Number to identity the version of the terms and conditions the user accepted.",
+    )
+    create_user = Column(
+        String(100),
+        nullable=False,
+        comment="The user or proxy account that created the record.",
+    )
+    create_date = Column(
+        TIMESTAMP(timezone=True, precision=6),
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        comment="The date and time the record was created.",
+    )
+    update_user = Column(
+        String(100),
+        comment="The user or proxy account that created or last updated the record.",
+    )
+    update_date = Column(
+        TIMESTAMP(timezone=True, precision=6),
+        default=datetime.datetime.utcnow,
+        comment="The date and time the record was created or last updated.",
+    )
+
+    user = relationship("FamUserModel", back_populates="fam_user_terms_conditions")
+
+
 class FamUserModel(Base):
     from .fam_access_control import FamAccessControlPrivilegeModel
-    from .fam_user import FamUserTermsConditionsModel
 
     __tablename__ = "fam_user"
 
@@ -144,69 +209,3 @@ class FamUserTypeModel(Base):
             "schema": "app_fam",
         },
     )
-
-
-class FamUserTermsConditionsModel(Base):
-    __tablename__ = "fam_user_terms_conditions"
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ["user_id"],
-            ["app_fam.fam_user.user_id"],
-            name="reffam_user_terms_conditions_user",
-        ),
-        PrimaryKeyConstraint(
-            "user_terms_conditions_id", name="fam_user_terms_conditions_pk"
-        ),
-        UniqueConstraint("user_id", "version", name="fam_tc_user_version_uk"),
-        Index("ix_app_fam_fam_user_terms_conditions_user_id", "user_id"),
-        {
-            "comment": "User Terms Conditions records identify the users who accept the "
-            "terms and conditions, as well as the version of it.",
-            "schema": "app_fam",
-        },
-    )
-
-    user_terms_conditions_id = Column(
-        BigInteger,
-        Identity(
-            start=1,
-            increment=1,
-            minvalue=1,
-            maxvalue=9223372036854775807,
-            cycle=False,
-            cache=1,
-        ),
-        comment="Automatically generated key used to identify the uniqueness of a terms and conditions acceptance record.",
-    )
-    user_id = Column(
-        BigInteger,
-        nullable=False,
-        comment="Unique ID to reference and identify the user within FAM system.",
-    )
-    version = Column(
-        String(30),
-        nullable=False,
-        comment="Number to identity the version of the terms and conditions the user accepted.",
-    )
-    create_user = Column(
-        String(100),
-        nullable=False,
-        comment="The user or proxy account that created the record.",
-    )
-    create_date = Column(
-        TIMESTAMP(timezone=True, precision=6),
-        nullable=False,
-        default=datetime.datetime.utcnow,
-        comment="The date and time the record was created.",
-    )
-    update_user = Column(
-        String(100),
-        comment="The user or proxy account that created or last updated the record.",
-    )
-    update_date = Column(
-        TIMESTAMP(timezone=True, precision=6),
-        default=datetime.datetime.utcnow,
-        comment="The date and time the record was created or last updated.",
-    )
-
-    user = relationship("FamUserModel", back_populates="fam_user_terms_conditions")
