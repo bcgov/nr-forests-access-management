553f11ac608535caa7ef42bc34d4ae1f5cb18241
Craig Yu <craig.yu93@gmail.com>
2024-10-09 15:01:40 -0700

    feat: Org list component

--- frontend/src/components/UserPermissionHistoryTable/PermissionDetailsCol.vue
+++ frontend/src/components/UserPermissionHistoryTable/PermissionDetailsCol.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { formatForestClientDisplayName } from '@/utils/ForestClientUtils';
-import { PrivilegeDetailsPermissionTypeEnum, PrivilegeDetailsScopeTypeEnum } from 'fam-app-acsctl-api/model';
+import { PrivilegeDetailsPermissionTypeEnum } from 'fam-app-acsctl-api/model';
 import type { PrivilegeDetailsSchema } from 'fam-app-acsctl-api/model/privilege-details-schema';
 import Chip from 'primevue/chip';
+import OrganizationsList from '@/components/UserPermissionHistoryTable/OrganizationList.vue';
 
 const props = defineProps<{
     permissionDetails: PrivilegeDetailsSchema;
@@ -16,23 +16,14 @@ enum PermissionChangeType {
 </script>
 
 <template>
-
     <!-- Details display for: permission Type change history as an 'EndUser' -->
-    <div v-if="props.permissionDetails.permission_type === PrivilegeDetailsPermissionTypeEnum.EndUser"
-        v-for="role in props.permissionDetails.roles" class="permission-details-col-container">
-        <div class="permission-type-container">
-            <p>Role: </p>
-            <Chip :label="role.role" />
-        </div>
-        <div class="organizations-container" v-if="role.scopes?.find((scope) => scope !== null)">
-            <p>Organizations:</p>
-            <div class="organizations-list">
-                <div v-for="scope in role.scopes">
-                    <div v-if="scope && scope.scope_type === PrivilegeDetailsScopeTypeEnum.Client">
-                        {{ formatForestClientDisplayName(scope.client_id, scope.client_name) }}
-                    </div>
-                </div>
+    <div v-if="props.permissionDetails.permission_type === PrivilegeDetailsPermissionTypeEnum.EndUser">
+        <div v-for="role in props.permissionDetails.roles" :key="role.role" class="permission-details-col-container">
+            <div class="permission-type-container">
+                <p>Role: </p>
+                <Chip :label="role.role" />
             </div>
+            <OrganizationsList v-if="role.scopes?.find(scope => scope !== null)" :scopes="role.scopes" />
         </div>
     </div>
 
@@ -42,21 +33,18 @@ enum PermissionChangeType {
             <p>Role: </p>
             <Chip :label="props.permissionDetails.permission_type" />
         </div>
-        <div v-for="role in props.permissionDetails.roles" class="permission-details-col-container">
-            <div>
-                <p>{{ props.permissionChangeType === PermissionChangeType.GRANT
-                    ? 'Role enabled to assign:' : 'Role revoked from assignment:' }} {{ role.role }}</p>
+        <div v-for="role in props.permissionDetails.roles" :key="role.role" class="permission-details-col-container">
+            <div class="d-admin-role-container">
+                <p>
+                    {{
+                        props.permissionChangeType === PermissionChangeType.GRANT
+                            ? 'Role enabled to assign:'
+                            : 'Role revoked from assignment:'
+                    }}
+                    {{ role.role }}
+                </p>
             </div>
-            <div class="organizations-container" v-if="role.scopes?.find((scope) => scope !== null)">
-                <p>Organizations:</p>
-                <div class="organizations-list">
-                    <div v-for="scope in role.scopes">
-                        <div v-if="scope && scope.scope_type === PrivilegeDetailsScopeTypeEnum.Client">
-                            {{ formatForestClientDisplayName(scope.client_id, scope.client_name) }}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <OrganizationsList v-if="role.scopes?.find(scope => scope !== null)" :scopes="role.scopes" />
         </div>
     </div>
 </template>
@@ -77,18 +65,13 @@ enum PermissionChangeType {
         color: colors.$blue-80;
     }
 }
+
 .permission-details-col-container {
     display: flex;
     flex-direction: column;
 
-    .organizations-container {
-        display: flex;
-        flex-direction: row;
+    .d-admin-role-container {
         margin-top: 0.5rem;
-
-        .organizations-list {
-            margin-left: 0.5rem;
-        }
     }
 }
 </style>
