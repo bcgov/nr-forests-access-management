460f0fe95c48553c6cd44da4c283567fa80869a6
Craig Yu <craig.yu93@gmail.com>
2024-10-15 14:55:27 -0700

    fix: vitest config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,13 @@
 /// <reference types="vitest" />
 
-import { fileURLToPath, URL } from "node:url";
-
-import vue from "@vitejs/plugin-vue";
-import path from "path";
 import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "unplugin-vue-components/resolvers";
+import { fileURLToPath, URL } from "url";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(async ({ command, mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
     const port = parseInt(env.VITE_PORT || "5173");
 
@@ -39,7 +37,6 @@ export default defineConfig(async ({ command, mode }) => {
             },
             extensions: [".js", ".ts", ".jsx", ".tsx", ".vue"],
         },
-        // root: path.resolve(__dirname, 'src'),
         server: {
             port: port,
         },
@@ -47,17 +44,11 @@ export default defineConfig(async ({ command, mode }) => {
             preprocessorOptions: {
                 scss: {
                     additionalData: `
-                  @use '@bcgov-nr/nr-theme/design-tokens/colors.scss' as colors;
-                  @use '@carbon/type' as type;
-                `,
+            @use '@bcgov-nr/nr-theme/design-tokens/colors.scss' as colors;
+            @use '@carbon/type' as type;
+          `,
                 },
             },
         },
-        // Note: define 'global' to solve aws-amplify `global is not defined` error.
-        // but does not work when using Vitest and configured as using 'jsdom' above.
-        //    ,
-        //    define: {
-        //      "global": {}, // Important var defined for solving Aws-Amplify bug with Vite(https://dev.to/richardbray/how-to-fix-the-referenceerror-global-is-not-defined-error-in-sveltekitvite-2i49)
-        //    },
     };
 });
