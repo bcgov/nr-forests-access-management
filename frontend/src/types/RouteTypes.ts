524cede366f4ed98f6febe91e3f1bfdea8740488
Craig Yu <craig.yu93@gmail.com>
2024-11-14 16:50:58 -0800

    fix: remove RouteConfigType

--- frontend/src/types/RouteTypes.ts
+++ frontend/src/types/RouteTypes.ts
@@ -1,13 +1,3 @@
-import type { Component } from "vue";
-
-export type RouteConfigType = {
-    path: string;
-    component: () => Promise<Component>;
-    name: string;
-    meta?: Record<string, unknown>;
-    children?: Record<string, RouteConfigType>;
-};
-
 export type AddAppPermissionRequestType =
     | "addUserPermission"
     | "addDelegatedAdmin";
