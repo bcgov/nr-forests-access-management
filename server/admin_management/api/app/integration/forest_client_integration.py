3d54142140c4ffc772c3f3ff366e1ea4ce0bbbbf
Ian Liu <ianliuwk1019@gmail.com>
2024-12-09 11:54:41 -0800

    Remove find_by_client_number from forest_client_integration and only use "search".

--- server/admin_management/api/app/integration/forest_client_integration.py
+++ server/admin_management/api/app/integration/forest_client_integration.py
@@ -6,7 +6,6 @@ import requests
 from api.app.constants import ApiInstanceEnv
 from api.app.schemas.forest_client_integration import \
     ForestClientIntegrationSearchParmsSchema
-from api.app.schemas.schemas import ForestClientIntegrationFindResponse
 from api.config import config
 
 LOGGER = logging.getLogger(__name__)
@@ -42,53 +41,6 @@ class ForestClientIntegrationService():
         # See Python: https://requests.readthedocs.io/en/latest/user/advanced/
         self.session = requests.Session()
         self.session.headers.update(self.headers)
-
-
-    # TODO: temporarily leave this method here; only referenced by tests. Will adjust and remove this function soon. # noqa NOSONAR
-    def find_by_client_number(self, p_client_number: str) -> List[ForestClientIntegrationFindResponse]:
-        """
-        Find Forest Client(s) information based on p_client_number search query field.
-
-        :param p_client_number: Forest Client Number string (8 digits).
-                                Note! Current Forest Client API can only do exact match.
-                                '/api/clients/findByClientNumber/{clientNumber}'
-        :return: Search result as List for a Forest Client information object.
-                 Current Forest Client API returns exact one result or http status
-                 other than 200 with message content. The intent for FAM search is for
-                 wild card search and Forest Client API could be capable of doing that
-                 in next version.
-        """
-        url = f"{self.api_clients_url}/findByClientNumber/{p_client_number}"
-        LOGGER.debug(f"ForestClientIntegrationService find_by_client_number() - url: {url}")
-
-        try:
-            r = self.session.get(url, timeout=self.TIMEOUT)
-            r.raise_for_status()
-            # !! Don't map and return schema.FamForestClient or object from "scheam.py" as that
-            # will create circular dependency issue. let crud to map the result.
-            api_result = r.json()
-            LOGGER.debug(f"API result: {api_result}")
-            return [api_result]
-
-        # Below except catches only HTTPError not general errors like network connection/timeout.
-        except requests.exceptions.HTTPError as he:
-            status_code = r.status_code
-            LOGGER.debug(f"API status code: {status_code}")
-            LOGGER.debug(f"API result: {r.content or r.reason}")
-
-            # For some reason Forest Client API uses (a bit confusing):
-            #    - '404' as general "client 'Not Found'", not as conventional http Not Found.
-            #
-            # Forest Client API returns '400' as "Invalid Client Number"; e.g. /findByClientNumber/abcde0001
-            # Howerver FAM 'search' (as string type param) is intended for either 'number' or 'name' search),
-            # so if 400, will return empty.
-            if ((status_code == HTTPStatus.NOT_FOUND) or (status_code == HTTPStatus.BAD_REQUEST)):
-                return [] # return empty for FAM forest client search
-
-            # Else raise error, including 500
-            # There is a general error handler, see: requests_http_error_handler
-            raise he
-
 
     def search(self, search_params: ForestClientIntegrationSearchParmsSchema):
         """
--- server/admin_management/tests/integration/test_forest_client_integration.py
+++ server/admin_management/tests/integration/test_forest_client_integration.py
@@ -1,7 +1,10 @@
 import logging
 
 import pytest
-from api.app.integration.forest_client_integration import ForestClientIntegrationService
+from api.app.integration.forest_client_integration import \
+    ForestClientIntegrationService
+from api.app.schemas.forest_client_integration import \
+    ForestClientIntegrationSearchParmsSchema
 
 LOGGER = logging.getLogger(__name__)
 
@@ -36,16 +39,18 @@ class TestForestClientIntegrationServiceClass(object):
         ("99999999", []),  # 8 digits - client not exist.
         ("000001011", [])  # more than 8 digits.
     ])
-    def test_find_by_client_number_not_exists_noresult(self, client_id_to_test, expected_result):
-        assert self.fc_api.find_by_client_number(client_id_to_test) == expected_result
+    def test_search__client_number_not_exists_no_result(self, client_id_to_test, expected_result):
+        fc_search_params = ForestClientIntegrationSearchParmsSchema(forest_client_numbers=[client_id_to_test])
+        assert self.fc_api.search(fc_search_params) == expected_result
 
-    def test_find_by_client_number_exists_returns_list_one_item(self):
+    def test_search__client_number_exists_returns_list_one_item(self):
         """
         The test validating API does return single client dictionary and
         with the expected key(s) structure.
         """
         client_id_to_test = self.example_expected_valid_result["clientNumber"]
-        result = self.fc_api.find_by_client_number(client_id_to_test)
+        fc_search_params = ForestClientIntegrationSearchParmsSchema(forest_client_numbers=[client_id_to_test])
+        result = self.fc_api.search(fc_search_params)
         assert len(result) == 1
         assert isinstance(result[0], dict)
         result_fc_dict = result[0]
