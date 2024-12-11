01a2eeee3b00a7c16894516167a8ded2f9b9a54c
Ian Liu <ianliuwk1019@gmail.com>
2024-12-10 16:35:53 -0800

    Add tests for decorator for admin privileges.

--- server/admin_management/tests/decorators/test_forest_client_dec.py
+++ server/admin_management/tests/decorators/test_forest_client_dec.py
@@ -2,20 +2,26 @@ import logging
 from typing import List
 
 import pytest
+from api.app.constants import AdminRoleAuthGroup
 from api.app.decorators.forest_client_dec import post_sync_forest_clients_dec
 from api.app.integration.forest_client_integration import \
     ForestClientIntegrationService
 from api.app.schemas.pagination import PagedResultsSchema, PageResultMetaSchema
-from api.app.schemas.schemas import FamAccessControlPrivilegeGetResponse
+from api.app.schemas.schemas import (AdminUserAccessResponse,
+                                     FamAccessControlPrivilegeGetResponse,
+                                     FamAuthGrantDto)
 from mock import patch
 from tests.test_data.forest_client_mock_data import (
+    ADMIN_GET_ACCESS_PRIVILEGE_RESULTS,
     APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META,
-    TestAccessControlResultDictKeys)
+    TestFcDecoratorFnResultConditions)
 
 LOGGER = logging.getLogger(__name__)
 
+# --- setup decorator for differnt target functions. ---
+
 @post_sync_forest_clients_dec
-def dummy_delegated_admin_assignment_fn_to_be_decorated(
+def dummy_decorated_delegated_admin_assignment_fn(
     some_results: List[FamAccessControlPrivilegeGetResponse]
 ) -> PagedResultsSchema[FamAccessControlPrivilegeGetResponse]:
     """ Dummy function to test decorator 'post_sync_forest_clients_dec' can handle
@@ -26,29 +32,40 @@ def dummy_delegated_admin_assignment_fn_to_be_decorated(
         results=some_results
     )
 
+@post_sync_forest_clients_dec
+def dummy_decorated_get_access_grants_fn(
+    some_results: AdminUserAccessResponse
+) -> AdminUserAccessResponse:
+    """ Dummy function to test decorator 'post_sync_forest_clients_dec' can handle
+        'AdminUserAccessService.get_access_grants()'
+    """
+    return some_results
+
+# --- tests below ---
+
 @pytest.mark.parametrize(
     "mock_fn_return, expected_results_condition",
     [
         (   # empty result.
-            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestAccessControlResultDictKeys.NO_RESULT],
-            TestAccessControlResultDictKeys.NO_RESULT
+            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestFcDecoratorFnResultConditions.NO_RESULT],
+            TestFcDecoratorFnResultConditions.NO_RESULT
         ),
         (   # fn return with no FC in items.
-            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestAccessControlResultDictKeys.NO_FC_IN_RESULTS],
-            TestAccessControlResultDictKeys.NO_FC_IN_RESULTS
+            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS],
+            TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS
         ),
         (   # fn return with FC in items.
-            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestAccessControlResultDictKeys.WITH_FC_IN_RESULTS],
-            TestAccessControlResultDictKeys.WITH_FC_IN_RESULTS
+            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS],
+            TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS
         ),
         (   # fn return with FC in items but FC does not exist in search (legacy or not active).
-            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestAccessControlResultDictKeys.WITH_FC_NOT_ACTIVE_RESULT],
-            TestAccessControlResultDictKeys.WITH_FC_NOT_ACTIVE_RESULT
+            APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT],
+            TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT
         ),
     ],
 )
 @patch.object(ForestClientIntegrationService, "search")
-def test_should_update_client_name_for_forest_client_fn_results(
+def test_should_update_client_name_for_delegated_admin_assignment_fn_results(
         mock_fc_search,
         mock_fn_return: List[FamAccessControlPrivilegeGetResponse],
         expected_results_condition,
@@ -67,12 +84,12 @@ def test_should_update_client_name_for_forest_client_fn_results(
         mock_fc_search.return_value = fcapi_search_return_dict
 
     # call decorated function
-    fn_dec_return = dummy_delegated_admin_assignment_fn_to_be_decorated(some_results=mock_fn_return)
+    fn_dec_return = dummy_decorated_delegated_admin_assignment_fn(some_results=mock_fn_return)
 
-    if expected_results_condition is not TestAccessControlResultDictKeys.WITH_FC_IN_RESULTS:
+    if expected_results_condition is not TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS:
         # expect decorated results is the same and no forest client name update.
         assert fn_dec_return.results == APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META[expected_results_condition]
-        if (expected_results_condition is TestAccessControlResultDictKeys.WITH_FC_NOT_ACTIVE_RESULT):
+        if (expected_results_condition is TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT):
             assert mock_fc_search.call_count == 1
         else:
             assert mock_fc_search.call_count == 0
@@ -83,3 +100,80 @@ def test_should_update_client_name_for_forest_client_fn_results(
         expeced_client_name_set = {item["clientName"] for item in fcapi_search_return_dict}
         results_client_name_set == expeced_client_name_set
         assert mock_fc_search.call_count == 1
+
+@pytest.mark.parametrize(
+    "mock_fn_return, expected_results_condition",
+    [
+        (   # empty result.
+            ADMIN_GET_ACCESS_PRIVILEGE_RESULTS[TestFcDecoratorFnResultConditions.NO_RESULT],
+            TestFcDecoratorFnResultConditions.NO_RESULT
+        ),
+        (   # fn return with no FC in items.
+            ADMIN_GET_ACCESS_PRIVILEGE_RESULTS[TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS],
+            TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS
+        ),
+        (   # fn return with FC in items.
+            ADMIN_GET_ACCESS_PRIVILEGE_RESULTS[TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS],
+            TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS
+        ),
+        (   # fn return with FC in items but FC does not exist in search (legacy or not active).
+            ADMIN_GET_ACCESS_PRIVILEGE_RESULTS[TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT],
+            TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT
+        ),
+    ],
+)
+@patch.object(ForestClientIntegrationService, "search")
+def test_should_update_client_name_for_get_access_grants_fn_results(
+        mock_fc_search,
+        mock_fn_return: AdminUserAccessResponse,
+        expected_results_condition,
+):
+    # prepare mocking
+    fcapi_search_return_dict = [
+        {
+            "clientName": f"client_{fc.forest_client_number}",
+            "clientNumber": fc.forest_client_number,
+            "clientStatusCode": "ACT",
+            "clientTypeCode": "C"
+        }
+        for fc in __collect_access_grants_forest_client(mock_fn_return.access)
+    ]
+    if any(fcapi_search_return_dict):
+        mock_fc_search.return_value = fcapi_search_return_dict
+
+    # call decorated function
+    fn_dec_return = dummy_decorated_get_access_grants_fn(some_results=mock_fn_return)
+
+    if expected_results_condition is not TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS:
+        # expect decorated results is the same and no forest client name update.
+        assert fn_dec_return == ADMIN_GET_ACCESS_PRIVILEGE_RESULTS[expected_results_condition]
+        if (expected_results_condition is TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT):
+            assert mock_fc_search.call_count >= 1  # FC API would be called based on each granted app; so it may not be just once.
+        else:
+            assert mock_fc_search.call_count == 0
+
+    else:
+        # expect decorated results contains client name update.
+        results_client_name_set =  {fc.client_name for fc in __collect_access_grants_forest_client(mock_fn_return.access)}
+        expeced_client_name_set = {item["clientName"] for item in fcapi_search_return_dict}
+        results_client_name_set == expeced_client_name_set
+        assert mock_fc_search.call_count >= 1
+
+
+def __collect_access_grants_forest_client(access_grants: List[FamAuthGrantDto]):
+    if not access_grants:
+        return []
+
+    delegatged_admin_grants = next(
+        filter(lambda access_grant: access_grant.auth_key == AdminRoleAuthGroup.DELEGATED_ADMIN , access_grants),
+        None
+    )
+
+    if not delegatged_admin_grants:
+        return []
+
+    search_forest_client_numbers = [
+        forest_client for grant in delegatged_admin_grants.grants for role in grant.roles if role.forest_clients for forest_client in role.forest_clients
+    ]
+
+    return search_forest_client_numbers
--- server/admin_management/tests/test_data/forest_client_mock_data.py
+++ server/admin_management/tests/test_data/forest_client_mock_data.py
@@ -3,15 +3,19 @@ import datetime
 from enum import Enum
 from typing import List
 
-from api.app.constants import AppEnv, RoleType, UserType
-from api.app.schemas.schemas import (FamAccessControlPrivilegeGetResponse,
-                                     FamApplicationBase, FamForestClientBase,
-                                     FamRoleBase, FamRoleWithClientDto,
-                                     FamUserInfoDto, FamUserTypeDto)
+from api.app.constants import AdminRoleAuthGroup, AppEnv, RoleType, UserType
+from api.app.schemas.schemas import (AdminUserAccessResponse,
+                                     FamAccessControlPrivilegeGetResponse,
+                                     FamApplicationBase,
+                                     FamApplicationGrantDto, FamAuthGrantDto,
+                                     FamForestClientBase, FamGrantDetailDto,
+                                     FamRoleBase, FamRoleGrantDto,
+                                     FamRoleWithClientDto, FamUserInfoDto,
+                                     FamUserTypeDto)
 
 # --- mock data for test_forest_client_dec.py
 
-class TestAccessControlResultDictKeys(str, Enum):
+class TestFcDecoratorFnResultConditions(str, Enum):
     NO_RESULT = "no_result"
     NO_FC_IN_RESULTS = "no_fc_results"
     WITH_FC_IN_RESULTS = "with_fc_results",
@@ -22,8 +26,8 @@ Mock data for `get_paged_delegated_admin_assignment_by_application_id()` return 
 Use 'TestAccessControlResultDictKeys' enum to identify different set of data.
 """
 APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META: dict[str, List[FamAccessControlPrivilegeGetResponse]] = {
-    TestAccessControlResultDictKeys.NO_RESULT: [],
-    TestAccessControlResultDictKeys.NO_FC_IN_RESULTS: [
+    TestFcDecoratorFnResultConditions.NO_RESULT: [],
+    TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS: [
         FamAccessControlPrivilegeGetResponse(access_control_privilege_id=900, user_id=900, role_id=900,
             user=FamUserInfoDto(user_name="tu_1", user_type_relation=FamUserTypeDto(user_type_code=UserType.BCEID, description="dummy_desc")),
             role=FamRoleWithClientDto(role_id=900, role_name="r1", role_type_code=RoleType.ROLE_TYPE_CONCRETE, role_purpose="Test dummy role",
@@ -42,7 +46,7 @@ APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META: dict[str, List[FamAccessContr
             ),
             create_date=datetime.datetime(2024, 11, 1, 19, 44, 47)),
     ],
-    TestAccessControlResultDictKeys.WITH_FC_IN_RESULTS: [
+    TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS: [
         FamAccessControlPrivilegeGetResponse(access_control_privilege_id=902, user_id=903, role_id=990,
             user=FamUserInfoDto(user_name="tu_1", user_type_relation=FamUserTypeDto(user_type_code=UserType.BCEID, description="dummy_desc")),
             role=FamRoleWithClientDto(role_id=990, role_name="r2_00001011", role_type_code=RoleType.ROLE_TYPE_CONCRETE, role_purpose="Test dummy role",
@@ -73,7 +77,7 @@ APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META: dict[str, List[FamAccessContr
             ),
             create_date=datetime.datetime(2024, 11, 1, 19, 44, 47)) # no FC
     ],
-    TestAccessControlResultDictKeys.WITH_FC_NOT_ACTIVE_RESULT: [
+    TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT: [
        FamAccessControlPrivilegeGetResponse(access_control_privilege_id=904, user_id=904, role_id=992,
             user=FamUserInfoDto(user_name="tu_1", user_type_relation=FamUserTypeDto(user_type_code=UserType.BCEID, description="dummy_desc")),
             role=FamRoleWithClientDto(role_id=992, role_name="r2_12345678", role_type_code=RoleType.ROLE_TYPE_CONCRETE, role_purpose="Test dummy role",
@@ -85,4 +89,93 @@ APP_DELEGATED_ADMIN_ROLE_GET_RESULTS_NO_PAGE_META: dict[str, List[FamAccessContr
             )),
             create_date=datetime.datetime(2024, 11, 1, 19, 44, 47)), # FC exist in db but not valid (legacy data or not active)
     ]
+}
+
+"""
+Mock data for `AdminUserAccessService.get_access_grants()`.
+Use 'TestAccessControlResultDictKeys' enum to identify different set of data.
+"""
+ADMIN_GET_ACCESS_PRIVILEGE_RESULTS: dict[str, AdminUserAccessResponse] = {
+    TestFcDecoratorFnResultConditions.NO_RESULT: AdminUserAccessResponse(access=[]),
+    TestFcDecoratorFnResultConditions.NO_FC_IN_RESULTS: AdminUserAccessResponse(
+        access=[
+            FamAuthGrantDto(auth_key=AdminRoleAuthGroup.FAM_ADMIN,
+                grants=[
+                    FamGrantDetailDto(application=FamApplicationGrantDto(application_id=1, application_name='FAM', description='Forests Access Management', env=None), roles=None),
+                    FamGrantDetailDto(application=FamApplicationGrantDto(application_id=2, application_name='FOM', description='Forest Operations Map (DEV)', env=AppEnv.APP_ENV_TYPE_DEV), roles=None),
+                    FamGrantDetailDto(application=FamApplicationGrantDto(application_id=3, application_name='FOM', description='Forest Operations Map (TEST)', env=AppEnv.APP_ENV_TYPE_TEST), roles=None),
+                    FamGrantDetailDto(application=FamApplicationGrantDto(application_id=5, application_name='SPAR', description='Seed Planning and Registry Application (DEV)', env=AppEnv.APP_ENV_TYPE_DEV), roles=None),
+                    FamGrantDetailDto(application=FamApplicationGrantDto(application_id=8, application_name='CLIENT', description='Forests Client Management System (DEV)', env=AppEnv.APP_ENV_TYPE_DEV), roles=None),
+                ]
+            ),
+            FamAuthGrantDto(auth_key=AdminRoleAuthGroup.APP_ADMIN,
+                grants=[
+                    FamGrantDetailDto(
+                        application=FamApplicationGrantDto(application_id=2, application_name='FOM', description='Forest Operations Map (DEV)', env=AppEnv.APP_ENV_TYPE_DEV),
+                        roles=[
+                            FamRoleGrantDto(role_id=3, role_name='FOM_SUBMITTER', display_name='Submitter', role_purpose='Provides the privilege to submit a FOM (on behalf of a specific forest client)', role_type_code=RoleType.ROLE_TYPE_ABSTRACT, forest_clients=None),
+                            FamRoleGrantDto(role_id=4, role_name='FOM_REVIEWER', display_name='Reviewer', role_purpose='Provides the privilege to review all FOMs in the system', role_type_code=RoleType.ROLE_TYPE_CONCRETE, forest_clients=None)
+                        ]
+                    )
+                ]
+            )
+        ]
+    ),
+    TestFcDecoratorFnResultConditions.WITH_FC_IN_RESULTS: AdminUserAccessResponse(
+        access=[
+            FamAuthGrantDto(auth_key=AdminRoleAuthGroup.APP_ADMIN,
+                grants=[
+                    FamGrantDetailDto(
+                        application=FamApplicationGrantDto(application_id=2, application_name='FOM', description='Forest Operations Map (DEV)', env=AppEnv.APP_ENV_TYPE_DEV),
+                        roles=[
+                            FamRoleGrantDto(role_id=3, role_name='FOM_SUBMITTER', display_name='Submitter', role_purpose='Provides the privilege to submit a FOM (on behalf of a specific forest client)', role_type_code=RoleType.ROLE_TYPE_ABSTRACT, forest_clients=None),
+                            FamRoleGrantDto(role_id=4, role_name='FOM_REVIEWER', display_name='Reviewer', role_purpose='Provides the privilege to review all FOMs in the system', role_type_code=RoleType.ROLE_TYPE_CONCRETE, forest_clients=None)
+                        ]
+                    )
+                ]
+            ),
+            FamAuthGrantDto(auth_key=AdminRoleAuthGroup.DELEGATED_ADMIN,
+                grants=[
+                    FamGrantDetailDto(
+                        application=FamApplicationGrantDto(application_id=2, application_name='FOM', description='Forest Operations Map (DEV)', env=AppEnv.APP_ENV_TYPE_DEV),
+                        roles=[
+                            FamRoleGrantDto(role_id=3, role_name='FOM_SUBMITTER', display_name='Submitter', role_purpose='Provides the privilege to submit a FOM (on behalf of a specific forest client)', role_type_code=RoleType.ROLE_TYPE_ABSTRACT,
+                                            forest_clients=[
+                                FamForestClientBase(forest_client_number='00001011'),
+                                FamForestClientBase(forest_client_number='00001012')
+                            ]),
+                            FamRoleGrantDto(role_id=4, role_name='FOM_REVIEWER', display_name='Reviewer', role_purpose='Provides the privilege to review all FOMs in the system', role_type_code=RoleType.ROLE_TYPE_CONCRETE, forest_clients=None)
+                        ]
+                    ),
+                    FamGrantDetailDto(
+                        application=FamApplicationGrantDto(application_id=5, application_name='SPAR', description='Seed Planning and Registry Application (DEV)', env=AppEnv.APP_ENV_TYPE_DEV),
+                        roles=[
+                            FamRoleGrantDto(role_id=149, role_name='SPAR_NONMINISTRY_ORCHARD', display_name='Submitter (Non-Ministry)', role_purpose='Allow non ministry users to create, review, update and submit A class registration forms. Non-Ministry users may enter and update lots where their sign in client profile matches the Applicant Client', role_type_code=RoleType.ROLE_TYPE_ABSTRACT,
+                                            forest_clients=[
+                                FamForestClientBase(forest_client_number='00012797')
+                            ]),
+                        ]
+                    )
+                ]
+            )
+        ]
+    ),
+    TestFcDecoratorFnResultConditions.WITH_FC_NOT_ACTIVE_RESULT: AdminUserAccessResponse(
+        access=[
+            FamAuthGrantDto(auth_key=AdminRoleAuthGroup.DELEGATED_ADMIN,
+                grants=[
+                    FamGrantDetailDto(
+                        application=FamApplicationGrantDto(application_id=900, application_name='dummy_app', env=AppEnv.APP_ENV_TYPE_DEV),
+                        roles=[
+                            FamRoleGrantDto(role_id=990, role_name='r1', display_name='Test dummy role', role_type_code=RoleType.ROLE_TYPE_ABSTRACT, role_purpose="Test dummy role",
+                                            forest_clients=[
+                                FamForestClientBase(forest_client_number='12345678'),
+                                FamForestClientBase(forest_client_number='99999999')
+                            ])
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
 }
