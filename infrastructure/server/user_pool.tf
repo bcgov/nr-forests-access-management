0ab9cc9fd4438415e8489d2fa843fcf31012bae4
Ian Liu <ianliuwk1019@gmail.com>
2025-03-05 11:50:45 -0800

    Add additional id_token/access_token attributes to user pool but no need to map.

--- infrastructure/server/user_pool.tf
+++ infrastructure/server/user_pool.tf
@@ -132,6 +132,32 @@ resource "aws_cognito_user_pool" "fam_user_pool" {
     }
   }
 
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = "false"
+    mutable                  = "true"
+    name                     = "id_token"
+    required                 = "false"
+
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = "false"
+    mutable                  = "true"
+    name                     = "access_token"
+    required                 = "false"
+
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+  }
+
   username_configuration {
     case_sensitive = "false"
   }
