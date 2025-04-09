f25afbab220ae3962084cd494cd130781e34559f
Ian Liu <ianliuwk1019@gmail.com>
2025-04-09 11:59:15 -0700

    Adjust display name based on confirmed standard.

--- server/flyway/sql/V63__update_apt_app_role_display_name.sql
+++ server/flyway/sql/V63__update_apt_app_role_display_name.sql
@@ -1,9 +1,9 @@
 -- Update app_fam.fam_role.display_name for application 'APT'
 
 -- APT 'VIEWER' role -> display_name: "View"
-UPDATE app_fam.fam_role SET DISPLAY_NAME = 'View' WHERE APPLICATION_ID in
+UPDATE app_fam.fam_role SET DISPLAY_NAME = 'Viewer' WHERE APPLICATION_ID in
     (select APPLICATION_ID from app_fam.fam_application where APPLICATION_NAME LIKE 'APT_%') AND ROLE_NAME = 'VIEWER';
 
 -- APT 'EDITOR' role -> display_name: "Update"
-UPDATE app_fam.fam_role SET DISPLAY_NAME = 'Update' WHERE APPLICATION_ID in
+UPDATE app_fam.fam_role SET DISPLAY_NAME = 'Editor' WHERE APPLICATION_ID in
     (select APPLICATION_ID from app_fam.fam_application where APPLICATION_NAME LIKE 'APT_%') AND ROLE_NAME = 'EDITOR';
