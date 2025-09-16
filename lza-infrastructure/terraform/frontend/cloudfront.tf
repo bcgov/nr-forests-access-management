d5cd6ce1b5ffd1e27a9af96f12984c645916c347
mcatherine1994 <mcatherine1994@gmail.com>
2025-09-16 14:45:58 -0700

    feat: #1952 update cloudfront configuration for dev,test,tools

--- lza-infrastructure/terraform/frontend/cloudfront.tf
+++ lza-infrastructure/terraform/frontend/cloudfront.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_policy" "web_distribution" {
 }
 
 resource "aws_cloudfront_distribution" "web_distribution" {
-  # aliases             = ["${var.cloudfront_vanity_domain}"]
+  aliases             = ["${var.cloudfront_vanity_domain}"]
   enabled             = true
   is_ipv6_enabled     = true
   wait_for_deployment = false
@@ -37,11 +37,11 @@ resource "aws_cloudfront_distribution" "web_distribution" {
   web_acl_id          = "${aws_wafv2_web_acl.fam_waf_cloudfront.arn}"
 
   viewer_certificate {
-    # acm_certificate_arn = "${var.cloudfront_certificate_arn}"
-    # ssl_support_method = "sni-only"
-    # minimum_protocol_version = "TLSv1.2_2021"
+    acm_certificate_arn = "${var.cloudfront_certificate_arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
 
-    cloudfront_default_certificate = true  # TODO: remove this after certificate is issued and in place and adjust above. Use AWS default for now.
+    # cloudfront_default_certificate = true  # TODO: remove this after certificate is issued and in place and adjust above. Use AWS default for now.
   }
 
   origin {
