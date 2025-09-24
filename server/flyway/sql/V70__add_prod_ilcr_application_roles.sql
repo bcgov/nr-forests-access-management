319ba46f7e1348ae39fd0cce67fc43ab1908e37f
ianliuwk1019 <ianliuwk1019@gmail.com>
2025-09-24 12:19:22 -0700

    Flyway to add new ILCR_PROD and roles.

---
+++ server/flyway/sql/V70__add_prod_ilcr_application_roles.sql
@@ -0,0 +1,35 @@
+-- Create ILCR_PROD applications
+INSERT INTO app_fam.fam_application (
+    application_name,
+    application_description,
+    app_environment,
+    create_user,
+    create_date
+)
+VALUES ('ILCR_PROD', 'Interior Logging Cost Reporting Application (PROD)', 'PROD', CURRENT_USER, CURRENT_DATE)
+;
+
+
+-- Add roles for ILCR_PROD
+INSERT INTO app_fam.fam_role (
+    role_name,
+    display_name,
+    role_purpose,
+    application_id,
+    role_type_code,
+    create_user,
+    create_date
+)
+VALUES ('ILCR_ADMIN', 'Admin', 'Access to all mills, ability to set up access to ILCR for branch staff and licensees, ability to revise mill data, ability to set up new mills in ILCR, ability to update ILCR web pages, requires an IDIR.', (select application_id from app_fam.fam_application where application_name = 'ILCR_PROD'), 'C', CURRENT_USER, CURRENT_DATE),
+       ('ILCR_SUBMITTER', 'Submitter', 'Access to specific mill data as authorized by the ILCR Access Request Form, requires a BCeID', (select application_id from app_fam.fam_application where application_name = 'ILCR_PROD'), 'C', CURRENT_USER, CURRENT_DATE)
+;
+
+-- Create PROD Cognito app clients for ICR
+INSERT INTO app_fam.fam_application_client (
+    cognito_client_id,
+    application_id,
+    create_user,
+    create_date
+)
+VALUES ('${client_id_prod_ilcr_oidc_client}', (select application_id from app_fam.fam_application where application_name = 'ILCR_PROD'), CURRENT_USER, CURRENT_DATE)
+;
