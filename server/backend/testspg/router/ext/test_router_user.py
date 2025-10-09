ac28e63788eb5a38bee28262e0d23f36ecfb58eb
ianliuwk1019 <ianliuwk1019@gmail.com>
2025-10-09 12:49:25 -0700

    Add page params tests.

--- server/backend/testspg/router/ext/test_router_user.py
+++ server/backend/testspg/router/ext/test_router_user.py
@@ -1,9 +1,12 @@
 import logging
 import time
+from contextlib import contextmanager
 from http.client import FORBIDDEN
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, patch
 
-from api.app.constants import ERROR_CODE_INVALID_OPERATION
+import pytest
+from api.app.constants import (ERROR_CODE_INVALID_OPERATION,
+                               EXT_DEFAULT_PAGE_SIZE, EXT_MIN_PAGE)
 from api.app.crud import crud_application, crud_utils
 from api.app.jwt_validation import (ERROR_EXPIRED_TOKEN,
                                     ERROR_PERMISSION_REQUIRED)
@@ -17,6 +20,26 @@ LOGGER = logging.getLogger(__name__)
 endPoint = f"{external_v1_api_prefix}/users"
 client = TestClient(app)
 
+mock_app = MagicMock()
+mock_app.application_id = 123 # fake
+mock_search_empty_result = {
+    "meta": {"total": 0,"pageCount": 0, "page": 1, "size": 10},
+    "users": []}
+mock_search_result = {
+    "meta": {"total": 2, "pageCount": 1, "page": 1, "size": 10},
+    "users": [
+        {"firstName": "Bob", "lastName": "Johnson", "idpUsername": "BOBJOHNSON", "idpUserGuid": "FAKEID002", "idpType": "BCEID", "roles": [{"applicationName": "FOM_DEV", "roleName": "FOM_SUBMITTER", "roleDisplayName": "Submitter", "scopeType": "FOREST_CLIENT", "value": ["00001011", "00001012"]}]},
+        {"firstName": "David", "lastName": "Brown", "idpUsername": "DAVIDB", "idpUserGuid": "FAKEID004", "idpType": "IDIR", "roles": [{"applicationName": "FOM_DEV", "roleName": "FOM_VIEWER", "roleDisplayName": "Viewer", "scopeType": None, "value": []}]}
+    ]
+}
+
+# reusable method to patch the dependencies for user_search tests
+@contextmanager
+def patched_user_search_service(mock_app):
+    with patch.object(crud_application, "get_application_by_app_client_id", return_value=mock_app), \
+         patch.object(crud_utils, "allow_ext_call_api_permission", return_value=True), \
+         patch("api.app.crud.services.ext_app_user_search_service.ExtAppUserSearchService") as MockService:
+        yield MockService
 
 # ------------------ test user_search ----------------------- #
 
@@ -44,11 +67,87 @@ def test_user_search_guards_by_authorize_ext_api_by_app_role(
         response = test_client_fixture.get(f"{endPoint}", headers=auth_headers)
         jwt_utils.assert_error_response(response, FORBIDDEN, ERROR_CODE_INVALID_OPERATION)
 
-    # test when requester does not have permission to access the API
-    mock_app = MagicMock()
-    mock_app.application_id = 123 # fake
+    # test when requester does not have permission (no permitted role) to access the API
     with patch.object(crud_application, "get_application_by_app_client_id", return_value=mock_app), \
         patch.object(crud_utils, "allow_ext_call_api_permission", return_value=False):
         response = test_client_fixture.get(f"{endPoint}", headers=auth_headers)
         jwt_utils.assert_error_response(response, FORBIDDEN, ERROR_PERMISSION_REQUIRED)
         assert "No permission to call" in response.json()["detail"]["description"]
+
+def test_user_search_return_result(
+    test_client_fixture: TestClient,
+    auth_headers,
+    override_depends__get_current_requester
+):
+    # test when no result found
+    override_depends__get_current_requester()
+    with patched_user_search_service(mock_app) as MockService:
+
+        mock_service_instance = MockService.return_value
+        mock_service_instance.search_users.return_value = mock_search_empty_result
+
+        response = test_client_fixture.get(f"{endPoint}", headers=auth_headers)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == mock_search_empty_result
+
+    # test with simple result found
+    with patch.object(crud_application, "get_application_by_app_client_id", return_value=mock_app), \
+        patch.object(crud_utils, "allow_ext_call_api_permission", return_value=True), \
+        patch("api.app.crud.services.ext_app_user_search_service.ExtAppUserSearchService") as MockService:
+        mock_service_instance = MockService.return_value
+        mock_service_instance.search_users.return_value = mock_search_result
+
+        response = test_client_fixture.get(f"{endPoint}", headers=auth_headers)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == mock_search_result
+
+
+# ----------- Tests for page_params parameter schema validation ----------- #
+
+def test_user_search_query_params(
+    test_client_fixture: TestClient,
+    auth_headers,
+    override_depends__get_current_requester,
+):
+    override_depends__get_current_requester()
+    captured_args = {} # to capture the args passed to search_users service method
+
+    # mock method for ExtAppUserSearchService.search_users
+    def capture_args(page_params, filter_params):
+        captured_args['page'] = page_params.page
+        captured_args['size'] = page_params.size
+        return {"meta": {"total": 0,"pageCount": 0, "page": 1, "size": 50}, "users": []} # dummy, not important.
+
+    with patched_user_search_service(mock_app) as MockService:
+        mock_service_instance = MockService.return_value
+        mock_service_instance.search_users.side_effect = capture_args
+
+        # Valid: no params (defaults)
+        response = test_client_fixture.get(endPoint, headers=auth_headers)
+        assert response.status_code == status.HTTP_200_OK
+        assert captured_args['page'] == EXT_MIN_PAGE
+        assert captured_args['size'] == EXT_DEFAULT_PAGE_SIZE
+
+        # Valid: custom params
+        response = test_client_fixture.get(endPoint, headers=auth_headers, params={"page": 2, "size": 20})
+        assert response.status_code == status.HTTP_200_OK
+        assert captured_args['page'] == 2
+        assert captured_args['size'] == 20
+
+    # Invalid scenarios (should return 422)
+    invalid_page_params_list = [
+        {"size": 9},
+        {"size": 101},
+        {"size": 20.5},
+        {"size": "xyz"},
+        {"page": -1},
+        {"page": 1.5},
+        {"page": "abc"},
+    ]
+    for invalid_params in invalid_page_params_list:
+        with patched_user_search_service(mock_app) as MockService:
+            mock_service_instance = MockService.return_value
+            mock_service_instance.search_users.side_effect = capture_args
+            response = test_client_fixture.get(endPoint, headers=auth_headers, params=invalid_params)
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+            assert "detail" in response.json()
