7b58762c767c259e65c3178185338ea107e4b025
Ian Liu <81595625+ianliuwk1019@users.noreply.github.com>
2025-12-10 15:33:23 -0800

    feat: #1882 Oboarding REPT (#2047)

---
+++ lza-infrastructure/terraform/server/oidc_clients_rept.tf
@@ -0,0 +1,116 @@
+resource "aws_cognito_user_pool_client" "dev_rept_oidc_client" {
+  access_token_validity                         = "5"
+  allowed_oauth_flows                           = ["code"]
+  allowed_oauth_flows_user_pool_client          = "true"
+  allowed_oauth_scopes                          = ["openid", "profile", "email"]
+  callback_urls                                 = [
+    var.oidc_sso_playground_url,
+    "http://localhost:3000/dashboard",
+    "http://localhost:3000/int/rept/dashboard",
+    "http://localhost:8080/dashboard",
+    "http://localhost:8080/int/rept/dashboard",
+    "https://dlvrapps.nrs.gov.bc.ca/int/rept/dashboard"
+  ]
+  logout_urls                                   = [
+    var.oidc_sso_playground_url,
+    "${var.cognito_app_client_logout_chain_url.dev}http://localhost:3000",
+    "${var.cognito_app_client_logout_chain_url.dev}http://localhost:3000/int/rept",
+    "${var.cognito_app_client_logout_chain_url.dev}http://localhost:8080",
+    "${var.cognito_app_client_logout_chain_url.dev}http://localhost:8080/int/rept",
+    "${var.cognito_app_client_logout_chain_url.dev}https://dlvrapps.nrs.gov.bc.ca/int/rept"
+  ]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "60"
+  name                                          = "rept_dev"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "24"
+  supported_identity_providers                  = [
+    "${aws_cognito_identity_provider.dev_idir_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "hours"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
+
+resource "aws_cognito_user_pool_client" "test_rept_oidc_client" {
+  access_token_validity                         = "5"
+  allowed_oauth_flows                           = ["code"]
+  allowed_oauth_flows_user_pool_client          = "true"
+  allowed_oauth_scopes                          = ["openid", "profile", "email"]
+  callback_urls                                 = [
+    var.oidc_sso_playground_url,
+    "http://localhost:3000/dashboard",
+    "http://localhost:3000/int/rept/dashboard",
+    "https://testapps.nrs.gov.bc.ca/int/rept/dashboard"
+  ]
+  logout_urls                                   = [
+    var.oidc_sso_playground_url,
+    "${var.cognito_app_client_logout_chain_url.test}http://localhost:3000",
+    "${var.cognito_app_client_logout_chain_url.test}http://localhost:3000/int/rept",
+    "${var.cognito_app_client_logout_chain_url.test}https://testapps.nrs.gov.bc.ca/int/rept"
+  ]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "60"
+  name                                          = "rept_test"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "24"
+  supported_identity_providers                  = [
+    "${aws_cognito_identity_provider.test_idir_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "hours"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
+
+resource "aws_cognito_user_pool_client" "prod_rept_oidc_client" {
+  access_token_validity                         = "5"
+  allowed_oauth_flows                           = ["code"]
+  allowed_oauth_flows_user_pool_client          = "true"
+  allowed_oauth_scopes                          = ["openid", "profile", "email"]
+  callback_urls                                 = [
+    var.oidc_sso_playground_url,
+    "https://apps.nrs.gov.bc.ca/int/rept/dashboard"
+  ]
+  logout_urls                                   = [
+    var.oidc_sso_playground_url,
+    "${var.cognito_app_client_logout_chain_url.prod}https://apps.nrs.gov.bc.ca/int/rept"
+  ]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "60"
+  name                                          = "rept_prod"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "24"
+  supported_identity_providers                  = [
+    "${aws_cognito_identity_provider.prod_idir_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "hours"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
