3ad39aeb8466ba019861527ba0b9abac63d567c4
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2026-05-27 08:16:17 -0700

    feat: onboard LEXIS to FAM

---
+++ lza-infrastructure/terraform/server/oidc_clients_lexis.tf
@@ -0,0 +1,109 @@
+resource "aws_cognito_user_pool_client" "dev_lexis_oidc_client" {
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls = concat(
+    [
+      var.oidc_sso_playground_url,
+      "http://localhost:3000/dashboard",
+    ],
+    [for i in range("${var.dev_pr_url_count}") : "https://nr-lexis-${i}.apps.silver.devops.gov.bc.ca/dashboard"]
+  )
+  logout_urls = concat(
+    [
+      var.oidc_sso_playground_url,
+      "${var.cognito_app_client_logout_chain_url.dev}http://localhost:3000",
+    ],
+    [for i in range("${var.dev_pr_url_count}") : "${var.cognito_app_client_logout_chain_url.dev}https://nr-lexis-${i}.apps.silver.devops.gov.bc.ca"]
+  )
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "5"
+  name                                          = "lexis_dev"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "60"
+  supported_identity_providers = [
+    "${aws_cognito_identity_provider.dev_idir_oidc_provider.provider_name}",
+    "${aws_cognito_identity_provider.dev_bceid_business_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "minutes"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
+
+resource "aws_cognito_user_pool_client" "test_lexis_oidc_client" {
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls = [
+    "https://nr-lexis-test.apps.silver.devops.gov.bc.ca/dashboard"
+  ]
+  logout_urls = [
+    "${var.cognito_app_client_logout_chain_url.test}https://nr-lexis-test.apps.silver.devops.gov.bc.ca"
+  ]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "5"
+  name                                          = "lexis_test"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "60"
+  supported_identity_providers = [
+    "${aws_cognito_identity_provider.test_idir_oidc_provider.provider_name}",
+    "${aws_cognito_identity_provider.test_bceid_business_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "minutes"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
+
+resource "aws_cognito_user_pool_client" "prod_lexis_oidc_client" {
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls = [
+    "https://nr-lexis-prod.apps.silver.devops.gov.bc.ca/dashboard"
+  ]
+  logout_urls = [
+    "${var.cognito_app_client_logout_chain_url.prod}https://nr-lexis-prod.apps.silver.devops.gov.bc.ca"
+  ]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  id_token_validity                             = "5"
+  name                                          = "lexis_prod"
+  prevent_user_existence_errors                 = "ENABLED"
+  read_attributes                               = var.minimum_oidc_attribute_list
+  refresh_token_validity                        = "60"
+  supported_identity_providers = [
+    "${aws_cognito_identity_provider.prod_idir_oidc_provider.provider_name}",
+    "${aws_cognito_identity_provider.prod_bceid_business_oidc_provider.provider_name}"
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "minutes"
+  }
+
+  user_pool_id     = aws_cognito_user_pool.fam_user_pool.id
+  write_attributes = var.minimum_oidc_attribute_list
+}
