845d62ea32c9a5f757672f103aa6ee2eb6f986fd
Greg Pascucci <greg.pascucci@cgi.com>
2026-06-12 09:16:18 -0700

    fix: update to confiugre csp urls and allow for 50 urls in dev (#2215)

--- lza-infrastructure/terraform/server/oidc_clients_csp.tf
+++ lza-infrastructure/terraform/server/oidc_clients_csp.tf
@@ -1,18 +1,39 @@
+locals {
+  csp_dev_host_urls = [
+    for i in range(var.dev_pr_url_count) : "https://nr-csp-${i}.apps.gold.devops.gov.bc.ca"
+  ]
+
+  csp_test_host_urls = [
+    "https://nr-csp-test.apps.gold.devops.gov.bc.ca"
+  ]
+
+  csp_prod_host_urls = [
+    "https://nr-csp-prod.apps.gold.devops.gov.bc.ca"
+  ]
+}
+
 resource "aws_cognito_user_pool_client" "dev_csp_oidc_client" {
-  access_token_validity                         = "5"
-  allowed_oauth_flows                           = ["code"]
-  allowed_oauth_flows_user_pool_client          = "true"
-  allowed_oauth_scopes                          = ["openid", "profile", "email"]
-  callback_urls                                 = [
-    var.oidc_sso_playground_url,
-    "http://localhost:3000/",
-    "https://dlvrapps.nrs.gov.bc.ca/pub/csp"
-  ]
-  logout_urls                                   = [
-    var.oidc_sso_playground_url,
-    "${var.cognito_app_client_logout_chain_url.dev}http://localhost:3000/logout",
-    "${var.cognito_app_client_logout_chain_url.dev}https://dlvrapps.nrs.gov.bc.ca/pub/csp/logout"
-  ]
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls = concat(
+    [
+      var.oidc_sso_playground_url,
+      "http://localhost:3000/"
+    ],
+    local.csp_dev_host_urls
+  )
+  logout_urls = concat(
+    [
+      var.oidc_sso_playground_url,
+      "${var.cognito_app_client_logout_chain_url.dev}http://localhost:3000/logout",
+    ],
+    [
+      for url in local.csp_dev_host_urls :
+      "${var.cognito_app_client_logout_chain_url.dev}${url}/logout"
+    ]
+  )
   enable_propagate_additional_user_context_data = "false"
   enable_token_revocation                       = "true"
   explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
@@ -21,7 +42,7 @@ resource "aws_cognito_user_pool_client" "dev_csp_oidc_client" {
   prevent_user_existence_errors                 = "ENABLED"
   read_attributes                               = var.minimum_oidc_attribute_list
   refresh_token_validity                        = "60"
-  supported_identity_providers                  = [
+  supported_identity_providers = [
     "${aws_cognito_identity_provider.dev_idir_oidc_provider.provider_name}",
     "${aws_cognito_identity_provider.dev_bceid_business_oidc_provider.provider_name}"
   ]
@@ -37,18 +58,15 @@ resource "aws_cognito_user_pool_client" "dev_csp_oidc_client" {
 }
 
 resource "aws_cognito_user_pool_client" "test_csp_oidc_client" {
-  access_token_validity                         = "5"
-  allowed_oauth_flows                           = ["code"]
-  allowed_oauth_flows_user_pool_client          = "true"
-  allowed_oauth_scopes                          = ["openid", "profile", "email"]
-  callback_urls                                 = [
-    var.oidc_sso_playground_url,
-    "https://testapps.nrs.gov.bc.ca/pub/csp"
-  ]
-  logout_urls                                   = [
-    var.oidc_sso_playground_url,
-    "${var.cognito_app_client_logout_chain_url.test}https://testapps.nrs.gov.bc.ca/pub/csp/logout"
-  ]
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls                        = concat([var.oidc_sso_playground_url], local.csp_test_host_urls)
+  logout_urls = concat(
+    [var.oidc_sso_playground_url],
+    [for url in local.csp_test_host_urls : "${var.cognito_app_client_logout_chain_url.test}${url}/logout"]
+  )
   enable_propagate_additional_user_context_data = "false"
   enable_token_revocation                       = "true"
   explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
@@ -57,7 +75,7 @@ resource "aws_cognito_user_pool_client" "test_csp_oidc_client" {
   prevent_user_existence_errors                 = "ENABLED"
   read_attributes                               = var.minimum_oidc_attribute_list
   refresh_token_validity                        = "60"
-  supported_identity_providers                  = [
+  supported_identity_providers = [
     "${aws_cognito_identity_provider.test_idir_oidc_provider.provider_name}",
     "${aws_cognito_identity_provider.test_bceid_business_oidc_provider.provider_name}"
   ]
@@ -73,18 +91,15 @@ resource "aws_cognito_user_pool_client" "test_csp_oidc_client" {
 }
 
 resource "aws_cognito_user_pool_client" "prod_csp_oidc_client" {
-  access_token_validity                         = "5"
-  allowed_oauth_flows                           = ["code"]
-  allowed_oauth_flows_user_pool_client          = "true"
-  allowed_oauth_scopes                          = ["openid", "profile", "email"]
-  callback_urls                                 = [
-    var.oidc_sso_playground_url,
-    "https://apps.nrs.gov.bc.ca/pub/csp"
-  ]
-  logout_urls                                   = [
-    var.oidc_sso_playground_url,
-    "${var.cognito_app_client_logout_chain_url.prod}https://apps.nrs.gov.bc.ca/pub/csp/logout"
-  ]
+  access_token_validity                = "5"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = "true"
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
+  callback_urls                        = concat([var.oidc_sso_playground_url], local.csp_prod_host_urls)
+  logout_urls = concat(
+    [var.oidc_sso_playground_url],
+    [for url in local.csp_prod_host_urls : "${var.cognito_app_client_logout_chain_url.prod}${url}/logout"]
+  )
   enable_propagate_additional_user_context_data = "false"
   enable_token_revocation                       = "true"
   explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH"]
@@ -93,7 +108,7 @@ resource "aws_cognito_user_pool_client" "prod_csp_oidc_client" {
   prevent_user_existence_errors                 = "ENABLED"
   read_attributes                               = var.minimum_oidc_attribute_list
   refresh_token_validity                        = "60"
-  supported_identity_providers                  = [
+  supported_identity_providers = [
     "${aws_cognito_identity_provider.prod_idir_oidc_provider.provider_name}",
     "${aws_cognito_identity_provider.prod_bceid_business_oidc_provider.provider_name}"
   ]
