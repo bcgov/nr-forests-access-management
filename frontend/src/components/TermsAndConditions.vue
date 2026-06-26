99c65dd530a0cc4550f62560d65d6caee8dce4c0
Ian Liu <ianliuwk1019@gmail.com>
2026-06-26 11:19:56 -0700

    Minor term condition header adjustment

--- frontend/src/components/TermsAndConditions.vue
+++ frontend/src/components/TermsAndConditions.vue
@@ -537,6 +537,10 @@ const acceptTermsAndConditions = () => {
 
 <style lang="scss">
 .terms-and-conditions-container {
+    .p-dialog-title.fam-dialog-title {
+        padding-bottom: 1rem;
+    }
+
     a {
         color: var(--link-primary);
     }
